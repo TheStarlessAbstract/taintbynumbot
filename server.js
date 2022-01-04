6b041410426299cf6339e30d17d9b19e5e96e178
TheStarlessAbstract <david.foley@outlook.com>
2022-01-04 18:28:30 +0000

    Fixed tinderquote

--- server.js
+++ server.js
@@ -1,95 +1,124 @@
-require('dotenv').config();
-const tmi = require('tmi.js');
+require("dotenv").config();
+const tmi = require("tmi.js");
 const mongoose = require("mongoose");
 const Tinder = require("./models/tinder");
 const Title = require("./models/title");
+const TINDERTIMEOUT = 30000;
+let time;
+let tinderTimer;
 const uri =
-  "mongodb+srv://" +
-  process.env.USER +
-  ":" +
-  process.env.PASS +
-  "@cluster0.8pokz.mongodb.net/taint_bot?retryWrites=true&w=majority";
+	"mongodb+srv://" +
+	process.env.USER +
+	":" +
+	process.env.PASS +
+	"@cluster0.8pokz.mongodb.net/taint_bot?retryWrites=true&w=majority";
 
 const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 const commands = {
-    upvote: {
-        response: (argument) => `Successfully upvoted ${argument}`
-    },
-    tinderquote: {
-        response: async () => {
-            let tinderEntries = await Tinder.find({});
-            if (tinderEntries.length > 0) {
-                let tinder = getRandom(tinderEntries);
+	upvote: {
+		response: (argument) => `Successfully upvoted ${argument}`,
+	},
+	tinderquote: {
+		response: async () => {
+			time = new Date();
+			let result = [];
 
-                client.say(target, `${tinder.text}`);
-                if (tinder.title != "") {
-                    client.say(
-                    target,
-                    `This Tinder bio was brought to you by the glorious, and taint-filled @${tinder.user}`
-                );
-                }
-            }
-            else {
-                client.say(target, `Tinder bio? What Tinder bio?`)
-            }
-        }
-    },
-    addtinder: {
-        response: () => "addTinder"
-    },
-    titleharassment: {
-        response: () => "titleHarassment"
-    },
-    addtitle: {
-        response: () => "addTitle"
-    },
-    booty: {
-        response: 'Who loves the booty?'
-    }
-}
+			if (
+				!tinderTimer ||
+				(tinderTimer && time.getTime() >= tinderTimer + TINDERTIMEOUT)
+			) {
+				tinderTimer = time.getTime();
 
+				let tinderEntries = await Tinder.find({});
 
+				if (tinderEntries.length > 0) {
+					let tinder = getRandom(tinderEntries);
 
-console.log('hello, twitch');
+					result.push(tinder.text);
+
+					if (tinder.user != "") {
+						result.push(
+							`This Tinder bio was brought to you by the glorious, and taint-filled @${tinder.user}`
+						);
+					}
+				} else {
+					result.push(`Tinder bio? What Tinder bio?`);
+				}
+			}
+
+			return result;
+		},
+	},
+	addtinder: {
+		response: "addTinder",
+	},
+	titleharassment: {
+		response: "titleHarassment",
+	},
+	addtitle: {
+		response: "addTitle",
+	},
+	booty: {
+		response: "Who loves the booty?",
+	},
+};
+
+console.log("hello, twitch");
 
 const client = new tmi.Client({
-    connection: {
-        reconnect: true
-    },
-    channels: [
-        'thestarlessabstract'
-    ],
-    identity: {
-        username: process.env.TWITCH_BOT_USERNAME,
-        password: process.env.TWITCH_OAUTH_TOKEN
-    }
+	connection: {
+		reconnect: true,
+	},
+	channels: ["thestarlessabstract"],
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN,
+	},
 });
 
 client.connect();
 
-client.on('message', async (channel, context, message) => {
-    const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
-    if (!isNotBot) return;
+client.on("connected", onConnectedHandler);
+client.on("message", async (channel, context, message) => {
+	const isNotBot =
+		context.username !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
+	const isNotBuhhs = context.username !== "buhhsbot";
 
-    const [raw, command, argument] = message.match(regexpCommand) || [null, null, null];
+	if (!isNotBot || !isNotBuhhs) {
+		return;
+	} else {
+		const [raw, command, argument] = message.match(regexpCommand) || [
+			null,
+			null,
+			null,
+		];
+		if (!command) return;
 
-    if (!command) return;
+		const { response } = (await commands[command.toLowerCase()]) || {};
 
-    const { response } = await commands[command.toLowerCase()] || {};
-
-    if (typeof response === 'function') {
-        // client.say(channel, await response);
-        // console.log(await response)
-        // client.say(channel, "sdads");
-    }
-    else if (typeof response === 'string') {
-        client.say(channel, response);
-    }
-
+		if (typeof response === "function") {
+			let result = await response();
+			if (result) {
+				for (let i = 0; i < result.length; i++) {
+					client.say(channel, result[i]);
+				}
+			}
+		} else if (typeof response === "string") {
+			client.say(channel, response);
+		}
+	}
 });
+
+function getRandom(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
+function onConnectedHandler(addr, port) {
+	console.log(`* Connected to ${addr}:${port}`);
+}
