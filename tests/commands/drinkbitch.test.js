810b5374d64345d56ea9b4646837c37b0bd8c572
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-01 19:53:31 +0000

    Update drinkbitch.test.js

--- tests/commands/drinkbitch.test.js
+++ tests/commands/drinkbitch.test.js
@@ -1,30 +1,21 @@
 require("dotenv").config();
 
-const drinkBitch = require("../../commands/drinkbitch");
-
 const db = require("../../bot-mongoose.js");
+const drinkBitch = require("../../commands/drinkbitch");
+const LoyaltyPoint = require("../../models/loyaltypoint");
 
 let isBroadcaster;
-let isModUp;
+let isMod = false;
 let userInfo;
 let argument;
 let commandLink = drinkBitch.command;
 const { response } = commandLink.getCommand();
 let currentDateTime = new Date();
 
-describe("drinkBitch", () => {
+describe.skip("drinkBitch", () => {
 	beforeAll(async () => {
-		process.env.JEST_WORKER_ID = 96;
 		db.connectToMongoDB();
 		await drinkBitch.updateAudioLinks();
-	});
-
-	beforeEach(() => {
-		isBroadcaster = true;
-		isModUp = true;
-		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
-		argument = undefined;
-		commandLink.setTimer(currentDateTime - 1000);
 	});
 
 	afterAll(async () => {
@@ -34,28 +25,102 @@ describe("drinkBitch", () => {
 	test("IsBroadcasterIsFalse_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
 		//Assemble
 		isBroadcaster = false;
+		userInfo = {};
+		commandLink.setTimer(currentDateTime - 1000);
 
 		//Act
 		let result = await response({
 			isBroadcaster,
-			isModUp,
+			isMod,
 			userInfo,
 			argument,
 		});
 
 		//Assert
-		expect(result[0]).toBe(undefined);
+		expect(result[0]).toBeUndefined();
 	});
 
-	test("IsBroadcasterIsFalse_AndCoolDownElapsed_ShouldReturnPositiveString", async () => {
+	test("IsBroadcasterIsFalse_AndCoolDownElapsed_AndNoUserInfo_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
+		userInfo = {};
 		commandLink.setTimer(currentDateTime - 6000);
 
 		//Act
 		let result = await response({
 			isBroadcaster,
-			isModUp,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
+	});
+
+	test("IsBroadcasterIsFalse_AndCoolDownElapsed_AndHasUserInfo_AndUserNotInDatabase_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		userInfo = { userId: 12826, displayName: "Twitch" };
+		commandLink.setTimer(currentDateTime - 6000);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"@Twitch It doesn't look like you have been here before, hang around, enjoy the mods abusing Starless, and maybe you too in time can make Starless !drinkbitch"
+		);
+	});
+
+	test("IsBroadcasterIsFalse_AndCoolDownElapsed_AndHasUserInfo_AndUserInDatabase_AndNotEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 6000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 0;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Assert
+		expect(result[0]).toMatch(/You lack the points to make Starless drink/);
+	});
+
+	test("IsBroadcasterIsFalse_AndCoolDownElapsed_AndHasUserInfo_AndUserInDatabase_AndHasEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 6000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
 			userInfo,
 			argument,
 		});
@@ -64,13 +129,176 @@ describe("drinkBitch", () => {
 		expect(result[0]).toBe("@TheStarlessAbstract drink, bitch!");
 	});
 
-	test("IsBroadcasterIsTrue_AndUserIdIsValid_And UserHasValidBalance_ShouldReturnPositiveString", async () => {
+	test("IsBroadcasterIsTrue_AndCoolDownNotElapsed_AndNoUserId_ShouldReturnString", async () => {
 		//Assemble
+		isBroadcaster = true;
+		userInfo = {};
+		commandLink.setTimer(currentDateTime - 1000);
 
 		//Act
 		let result = await response({
 			isBroadcaster,
-			isModUp,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownNotElapsed_AndHasUserId_AndUserNotInDatabase_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 12826, displayName: "Twitch" };
+		commandLink.setTimer(currentDateTime - 1000);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"@Twitch It doesn't look like you have been here before, hang around, enjoy the mods abusing Starless, and maybe you too in time can make Starless !drinkbitch"
+		);
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownNotElapsed_AndHasUserId_AndUserInDatabase_AndNotEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 1000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 0;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Assert
+		expect(result[0]).toMatch(/You lack the points to make Starless drink/);
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownNotElapsed_AndHasUserId_AndUserInDatabase_AndHasEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 1000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe("@TheStarlessAbstract drink, bitch!");
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndNoUserId_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = {};
+		commandLink.setTimer(currentDateTime - 6000);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndHasUserId_AndUserNotInDatabase_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 12826, displayName: "Twitch" };
+		commandLink.setTimer(currentDateTime - 6000);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"@Twitch It doesn't look like you have been here before, hang around, enjoy the mods abusing Starless, and maybe you too in time can make Starless !drinkbitch"
+		);
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndHasUserId_AndUserInDatabase_AndNotEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 6000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 0;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Assert
+		expect(result[0]).toMatch(/You lack the points to make Starless drink/);
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndHasUserId_AndUserInDatabase_AndNotEnoughBalance_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { userId: 100612361, displayName: "TheStarlessAbstract" };
+		commandLink.setTimer(currentDateTime - 6000);
+		let user = await LoyaltyPoint.findOne({
+			userId: userInfo.userId,
+		});
+
+		user.points = 69000;
+		await user.save();
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
 			userInfo,
 			argument,
 		});
