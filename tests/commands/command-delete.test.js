76fef247244299c3b084e3c0f7a9de58382bd5c0
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-01 19:53:19 +0000

    Update command-delete.test.js

--- tests/commands/command-delete.test.js
+++ tests/commands/command-delete.test.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const db = require("../../bot-mongoose.js");
+const BaseCommand = require("../../classes/base-command");
 const Command = require("../../models/command");
 
 const commands = require("../../bot-commands");
@@ -17,16 +18,18 @@ let commandName;
 let commandText;
 
 describe.skip("delComm", () => {
+	let commandNames = [];
 	beforeAll(async () => {
 		db.connectToMongoDB();
 		await commands.setup();
 	});
 
 	afterAll(async () => {
+		await dBCleanUp(commandNames);
 		await db.disconnectFromMongoDB();
 	});
 
-	//0-0-0-0 - //0-0-0-1 - //0-0-1-0 - //0-0-1-1 - //0-0-2-0 - //0-0-2-1
+	//0-0
 	test("IsBroadcasterFalse_AndAndIsModFalse_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
@@ -45,14 +48,12 @@ describe.skip("delComm", () => {
 		expect(result[0]).toBe("!delComm Command is for Mods only");
 	});
 
-	//0-1-0-0 - //0-1-0-1
-	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentNotValid_ShouldReturnString", async () => {
+	//0-1-0
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsUndefined_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
 		isMod = true;
-		commandName = "delCommTest2";
-		commandText = "this is delCommTest2";
-		argument = commandName;
+		argument = undefined;
 
 		//Act
 		let result = await response({
@@ -62,88 +63,16 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
-
 		//Assert
-		expect(result[0]).toBe(
-			"To delete a Command, command name must start with '!' - !delComm ![command name]"
-		);
+		expect(result[0]).toBe("To delete a Command, use !delComm ![command name]");
 	});
 
 	//0-1-1-0
-	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentValid_AndCommandNotInDb_ShouldReturnString", async () => {
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndNoPrefix_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
 		isMod = true;
 		commandName = "delCommTest3";
-		commandText = "this is delCommTest3";
-		argument = "!" + commandName;
-
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isMod,
-			userInfo,
-			argument,
-		});
-
-		//Assert
-		expect(result[0]).toBe(
-			"!delcommtest3 doesn't look to be a command, are you sure you spelt it right, dummy?!"
-		);
-	});
-
-	//0-1-1-1
-	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentValid_AndCommandInDb_ShouldReturnString", async () => {
-		//Assemble
-		isBroadcaster = false;
-		isMod = true;
-		commandName = "delCommTest4";
-		commandText = "this is delCommTest4";
-		argument = "!" + commandName;
-
-		await dbSetup(commandName, commandText);
-
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isMod,
-			userInfo,
-			argument,
-		});
-
-		await dBCleanUp(commandName);
-
-		//Assert
-		expect(result[0]).toBe("!delcommtest4 has been deleted");
-	});
-
-	//0-1-2-0 - //0-1-2-1
-	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentUndefined_ShouldReturnString", async () => {
-		//Assemble
-		isBroadcaster = false;
-		isMod = true;
-		argument = undefined;
-
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isMod,
-			userInfo,
-			argument,
-		});
-
-		//Assert
-		expect(result[0]).toBe("To delete a Command, use !delComm ![command name]");
-	});
-
-	//1-0-0-0 - //1-0-0-1
-	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentNotValid_ShouldReturnString", async () => {
-		//Assemble
-		isBroadcaster = true;
-		isMod = false;
-		commandName = "delCommTest6";
-		commandText = "this is delCommTest6";
 		argument = commandName;
 
 		//Act
@@ -154,46 +83,86 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
-
 		//Assert
 		expect(result[0]).toBe(
 			"To delete a Command, command name must start with '!' - !delComm ![command name]"
 		);
 	});
 
-	//1-0-1-0
-	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentValid_AndCommandNotInDb_ShouldReturnString", async () => {
+	//0-1-1-1-0
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndHasPrefix_AndNoCommandName_ShouldReturnString", async () => {
 		//Assemble
-		isBroadcaster = true;
-		isMod = false;
+		isBroadcaster = false;
+		isMod = true;
+		argument = "!";
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"To delete a Command, you must include the command name - !delComm ![command name]"
+		);
+	});
+
+	//0-1-1-1-1-0
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndNotInCommandList_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		isMod = true;
+		commandName = "delCommTest5";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"!delcommtest5 doesn't look to be a command, are you sure you spelt it right, dummy?!"
+		);
+	});
+
+	//0-1-1-1-1-1-0
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndNotDeletable_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		isMod = true;
+		commandName = "kings";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"!kings is too spicy to be deleted through chat, Starless is going to have to do some work for that, so ask nicely"
+		);
+	});
+
+	//0-1-1-1-1-1-1
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndIsDeletable_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		isMod = true;
 		commandName = "delCommTest7";
 		commandText = "this is delCommTest7";
 		argument = "!" + commandName;
 
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isMod,
-			userInfo,
-			argument,
-		});
-
-		//Assert
-		expect(result[0]).toBe(
-			"!delcommtest7 doesn't look to be a command, are you sure you spelt it right, dummy?!"
-		);
-	});
-
-	//1-0-1-1
-	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentValid_AndCommandInDb_ShouldReturnString", async () => {
-		//Assemble
-		isBroadcaster = true;
-		isMod = false;
-		commandName = "delCommTest8";
-		commandText = "this is delCommTest8";
-		argument = "!" + commandName;
-
 		await dbSetup(commandName, commandText);
 
 		//Act
@@ -204,14 +173,14 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
+		commandNames.push(commandName);
 
 		//Assert
-		expect(result[0]).toBe("!delcommtest8 has been deleted");
+		expect(result[0]).toBe("!delcommtest7 has been deleted");
 	});
 
-	//1-0-2-0 - //1-0-2-1
-	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentUndefined_ShouldReturnString", async () => {
+	//1-0-0
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsUndefined_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
 		isMod = false;
@@ -228,13 +197,13 @@ describe.skip("delComm", () => {
 		//Assert
 		expect(result[0]).toBe("To delete a Command, use !delComm ![command name]");
 	});
-	//1-1-0-0 - //1-1-0-1
-	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentNotValid_ShouldReturnString", async () => {
+
+	//1-0-1-0
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsString_AndNoPrefix_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
-		isMod = true;
-		commandName = "delCommTest10";
-		commandText = "this is delCommTest10";
+		isMod = false;
+		commandName = "delCommTest11";
 		argument = commandName;
 
 		//Act
@@ -245,22 +214,18 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
-
 		//Assert
 		expect(result[0]).toBe(
 			"To delete a Command, command name must start with '!' - !delComm ![command name]"
 		);
 	});
 
-	//1-1-1-0
-	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentValid_AndCommandNotInDb_ShouldReturnString", async () => {
+	//1-0-1-1-0
+	test("IsBroadcasterFalse_AndAndIsModTrue_AndArgumentIsString_AndHasPrefix_AndNoCommandName_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
-		isMod = true;
-		commandName = "delCommTest11";
-		commandText = "this is delCommTest11";
-		argument = "!" + commandName;
+		isMod = false;
+		argument = "!";
 
 		//Act
 		let result = await response({
@@ -270,7 +235,27 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
+		//Assert
+		expect(result[0]).toBe(
+			"To delete a Command, you must include the command name - !delComm ![command name]"
+		);
+	});
+
+	//1-0-1-1-1-0
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndNotInCommandList_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = false;
+		commandName = "delCommTest11";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
 
 		//Assert
 		expect(result[0]).toBe(
@@ -278,13 +263,35 @@ describe.skip("delComm", () => {
 		);
 	});
 
-	//1-1-1-1
-	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentValid_AndCommandInDb_ShouldReturnString", async () => {
+	//1-0-1-1-1-1-0
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndNotDeletable_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
-		isMod = true;
-		commandName = "delCommTest12";
-		commandText = "this is delCommTest12";
+		isMod = false;
+		commandName = "kings";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"!kings is too spicy to be deleted through chat, Starless is going to have to do some work for that, so ask nicely"
+		);
+	});
+
+	//1-0-1-1-1-1-1
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndIsDeletable_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = false;
+		commandName = "delCommTest13";
+		commandText = "this is delCommTest13";
 		argument = "!" + commandName;
 
 		await dbSetup(commandName, commandText);
@@ -297,17 +304,17 @@ describe.skip("delComm", () => {
 			argument,
 		});
 
-		await dBCleanUp(commandName);
+		commandNames.push(commandName);
 
 		//Assert
-		expect(result[0]).toBe("!delcommtest12 has been deleted");
+		expect(result[0]).toBe("!delcommtest13 has been deleted");
 	});
 
-	//1-1-2-0 - //1-1-2-1
-	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentUndefined_ShouldReturnString", async () => {
+	//1-1-0
+	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentIsUndefined_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
-		isMod = true;
+		isMod = false;
 		argument = undefined;
 
 		//Act
@@ -321,18 +328,137 @@ describe.skip("delComm", () => {
 		//Assert
 		expect(result[0]).toBe("To delete a Command, use !delComm ![command name]");
 	});
+
+	//1-1-1-0
+	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentIsString_AndNoPrefix_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		commandName = "delCommTest19";
+		argument = commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"To delete a Command, command name must start with '!' - !delComm ![command name]"
+		);
+	});
+
+	//1-1-1-1-0
+	test("IsBroadcasterTrue_AndIsModFalse_AndArgumentIsString_AndHasPrefix_AndNoCommandName_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = false;
+		argument = "!";
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"To delete a Command, you must include the command name - !delComm ![command name]"
+		);
+	});
+
+	//1-1-1-1-1-0
+	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndNotInCommandList_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		commandName = "delCommTest15";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"!delcommtest15 doesn't look to be a command, are you sure you spelt it right, dummy?!"
+		);
+	});
+
+	//1-1-1-1-1-1-0
+	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndNotDeletable_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		commandName = "kings";
+		argument = "!" + commandName;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBe(
+			"!kings is too spicy to be deleted through chat, Starless is going to have to do some work for that, so ask nicely"
+		);
+	});
+
+	//1-1-1-1-1-1-1
+	test("IsBroadcasterTrue_AndIsModTrue_AndArgumentIsString_AndHasPrefix_AndHasCommandName_AndInCommandList_AndNotDeletable_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		commandName = "delCommTest19";
+		commandText = "this is delCommTest19";
+		argument = "!" + commandName;
+
+		await dbSetup(commandName, commandText);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		commandNames.push(commandName);
+
+		//Assert
+		expect(result[0]).toBe("!delcommtest19 has been deleted");
+	});
 });
 
 async function dbSetup(setupCommand, text) {
 	setupCommand = setupCommand.toLowerCase();
-	commands.list[setupCommand] = {
-		response: text,
-	};
+	commands.list[setupCommand] = new BaseCommand(() => {
+		return {
+			response: text,
+		};
+	});
+
 	await Command.create({ name: setupCommand, text: text });
 }
 
-async function dBCleanUp(cleanUpCommand) {
-	cleanUpCommand = cleanUpCommand.toLowerCase();
-	delete commands.list[cleanUpCommand];
-	await Command.deleteOne({ name: cleanUpCommand });
+async function dBCleanUp(cleanUpCommands) {
+	cleanUpCommands = cleanUpCommands.map((name) => name.toLowerCase());
+
+	cleanUpCommands.forEach((name) => {
+		delete commands.list[name];
+	});
+
+	await Command.deleteMany({ name: { $in: cleanUpCommands } });
 }
