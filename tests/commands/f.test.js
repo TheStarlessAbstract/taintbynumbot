fe605059277bc8033952f702daf5acb6c324bef7
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-05 20:02:34 +0000

    Update f.test.js

--- tests/commands/f.test.js
+++ tests/commands/f.test.js
@@ -12,7 +12,7 @@ let commandLink = f.command;
 const { response } = commandLink.getCommand();
 let currentDateTime = new Date();
 
-describe("f", () => {
+describe.skip("f", () => {
 	beforeAll(async () => {
 		db.connectToMongoDB();
 		await f.setup();
@@ -30,55 +30,51 @@ describe("f", () => {
 		await db.disconnectFromMongoDB();
 	});
 
-	// test("IsBroadcasterIsFalse_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	isBroadcaster = false;
+	test("IsBroadcasterIsFalse_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
+		//Assemble
+		isBroadcaster = false;
 
-	// 	//Act
-	// 	let result = await response({
-	// 		isBroadcaster,
-	// 		isModUp,
-	// 		userInfo,
-	// 		argument,
-	// 	});
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isModUp,
+			userInfo,
+			argument,
+		});
 
-	// 	//Assert
-	// 	expect(result[0]).toBe(undefined);
-	// });
+		//Assert
+		expect(result[0]).toBe(undefined);
+	});
 
-	// test("IsBroadcasterIsFalse_AndCoolDownElapsed_ShouldReturnPositiveString", async () => {
-	// 	//Assemble
-	// 	isBroadcaster = false;
-	// 	commandLink.setTimer(currentDateTime - 11000);
+	test("IsBroadcasterIsFalse_AndCoolDownElapsed_ShouldReturnPositiveString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		commandLink.setTimer(currentDateTime - 11000);
 
-	// 	//Act
-	// 	let result = await response({
-	// 		isBroadcaster,
-	// 		isModUp,
-	// 		userInfo,
-	// 		argument,
-	// 	});
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isModUp,
+			userInfo,
+			argument,
+		});
 
-	// 	//Assert
-	// 	expect(result[0].startsWith("Starless has now died/failed")).toBe(true);
-	// });
-
-	// test("IsBroadcasterIsTrue_ShouldReturnPositiveString", async () => {
-	// 	//Assemble
-
-	// 	//Act
-	// 	let result = await response({
-	// 		isBroadcaster,
-	// 		isModUp,
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0].startsWith("Starless has now died/failed")).toBe(true);
-	// });
+		//Assert
+		expect(result[0].startsWith("Starless has now died/failed")).toBe(true);
+	});
 
 	test("IsBroadcasterIsTrue_ShouldReturnPositiveString", async () => {
-		expect(3).toBe(3);
+		//Assemble
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isModUp,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0].startsWith("Starless has now died/failed")).toBe(true);
 	});
 });
