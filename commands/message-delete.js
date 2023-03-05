6f1136a764d6dd115cea80d30d7d2f67bc25097b
TheStarlessAbstract <david.foley@outlook.com>
2023-03-05 23:34:36 +0000

    Update message-delete.js

--- commands/message-delete.js
+++ commands/message-delete.js
@@ -1,65 +1,38 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const Message = require("../models/message");
+
+const helper = new Helper();
 
 let commandResponse = () => {
 	return {
 		response: async (config) => {
 			let result = [];
 
-			if (config.isModUp && config.argument) {
-				let index = config.argument.split(/\s(.+)/)[0].toLowerCase();
-				let text = config.argument.split(/\s(.+)/)[1];
+			if (
+				helper.isValidModeratorOrStreamer(config) &&
+				helper.isValuePresentAndString(config.argument)
+			) {
+				let index = helper.getCommandArgumentKey(config, 0);
 
-				if (versions[0].active && !isNaN(index)) {
-					let quote = await Message.findOne({ index: index });
-					if (quote) {
-						result.push("Message " + index + " was: " + quote.text);
-						await quote.remove();
+				if (helper.isValuePresentAndNumber(index)) {
+					let message = await Message.findOne({ index: index });
+					if (message) {
+						result.push("Message deleted - " + index + " was: " + message.text);
+						await message.remove();
 					} else {
-						result.push("No message number " + config.argument + " found");
+						result.push("No Message " + config.argument + " found");
 					}
-				} else if (versions[1].active && config.argument && isNaN(index)) {
-					console.log(text);
-					let entries = await Message.find({
-						text: { $regex: text, $options: "i" },
-					});
-
-					if (entries.length > 0) {
-						let pularlity = entries.length > 1 ? "messages" : "message";
-
-						result.push(
-							entries.length + " " + pularlity + " found mentioning: " + text
-						);
-
-						let output;
-						if (entries.length > 1) {
-							output = "Indexes include: ";
-							for (let i = 0; i < 5; i++) {
-								output += entries[i].index + ", ";
-							}
-							result.push(output);
-							if (entries.length > 5) {
-								result.push(
-									"There are more matches, maybe you could be more specific"
-								);
-							}
-						} else {
-							output =
-								entries[0].index +
-								" is the number of the message you are looking to delete. Use !delMessage " +
-								entries[0].index;
-						}
-					} else {
-						result.push("No messages found including '" + text + "'");
-					}
+				} else {
+					result.push(
+						"!delMessage [index] - index is a number - !delMessage 69"
+					);
 				}
-			} else if (!config.isModUp) {
-				result.push(["!delMessage command is for Mods only"]);
-			} else if (!config.argument) {
-				result.push([
-					"To delete a message, you must include the message number like !delMessage 69, or include a search string like !delMessage uwu to find the number of the message",
-				]);
+			} else if (!helper.isValidModeratorOrStreamer(config)) {
+				result.push("!delMessage is for Mods only");
+			} else if (!helper.isValuePresentAndString(config.argument)) {
+				result.push("To delete a Message use !delMessage [index]");
 			}
 
 			return result;
