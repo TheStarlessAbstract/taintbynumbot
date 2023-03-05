7732f760273aa504fa03a6b8ca9c328ebcafb15b
TheStarlessAbstract <david.foley@outlook.com>
2023-03-05 23:34:46 +0000

    Update message-edit.test.js

--- tests/commands/message-edit.test.js
+++ tests/commands/message-edit.test.js
@@ -1,47 +1,33 @@
 require("dotenv").config();
-
 const db = require("../../bot-mongoose.js");
+const Helper = require("../../classes/helper");
 
-// const kings = require("../../commands/kings");
+const Message = require("../../models/message");
+const messageEdit = require("../../commands/message-edit");
+
+const helper = new Helper();
 
 let isBroadcaster;
 let isMod;
 let userInfo;
 let argument;
-// let commandLink = kings.command;
-// const { response } = commandLink.getCommand();
-// let currentDateTime = new Date();
+let commandLink = messageEdit.command;
+const { response } = commandLink.getCommand();
 
 describe.skip("editMessage", () => {
+	let cleanUpList = [];
+
 	beforeAll(async () => {
 		db.connectToMongoDB();
-	});
-
-	afterAll(async () => {
-		await db.disconnectFromMongoDB();
-	});
-
-	test("IsBroadcasterIsFalse_AndCoolDownElapsed_AndHasUserInfo_AndUserInDatabase_ShouldReturnString", async () => {
-		//Assemble
-		isBroadcaster = true;
 		userInfo = {
 			userId: 100612361,
 			displayName: "TheStarlessAbstract",
 		};
-		commandLink.setTimer(currentDateTime - 6000);
+	});
 
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isMod,
-			userInfo,
-			argument,
-		});
-
-		//Assert
-		expect(
-			result[0].startsWith("@TheStarlessAbstract You have drawn the")
-		).toBe(true);
+	afterAll(async () => {
+		await dBCleanUp(cleanUpList);
+		await db.disconnectFromMongoDB();
 	});
 
 	//isbroadcaster
@@ -53,6 +39,23 @@ describe.skip("editMessage", () => {
 	//message unique
 
 	//0-0
+	test("IsBroadcasterIsFalse_AndIsModIsFalse_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		isMod = false;
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toMatch(/!delMessage is for Mods only/);
+	});
+
 	//0-1-0
 	//0-1-1-0
 	//0-1-1-1-0
@@ -72,3 +75,14 @@ describe.skip("editMessage", () => {
 	//1-1-1-1-1-1-0
 	//1-1-1-1-1-1-1
 });
+
+async function dbSetup(index, setupMessage) {
+	await Message.create({
+		index: index,
+		text: setupMessage,
+	});
+}
+
+async function dBCleanUp(cleanUpMessages) {
+	await Message.deleteMany({ text: { $in: cleanUpMessages } });
+}
