6a0a2d1998ff2f4f6818809eac40818ddbc9177d
TheStarlessAbstract <david.foley@outlook.com>
2023-03-05 23:34:40 +0000

    Update message-add.test.js

--- tests/commands/message-add.test.js
+++ tests/commands/message-add.test.js
@@ -1,17 +1,22 @@
 require("dotenv").config();
-
 const db = require("../../bot-mongoose.js");
+const Helper = require("../../classes/helper");
 
-const commandAdd = require("../../commands/message-add");
+const Message = require("../../models/message");
+const messageAdd = require("../../commands/message-add");
+
+const helper = new Helper();
 
 let isBroadcaster;
 let isMod;
 let userInfo;
 let argument;
-let commandLink = commandAdd.command;
+let commandLink = messageAdd.command;
 const { response } = commandLink.getCommand();
 
-describe("addMessage", () => {
+describe.skip("addMessage", () => {
+	let cleanUpList = [];
+
 	beforeAll(async () => {
 		db.connectToMongoDB();
 		userInfo = {
@@ -21,15 +26,11 @@ describe("addMessage", () => {
 	});
 
 	afterAll(async () => {
+		await dBCleanUp(cleanUpList);
 		await db.disconnectFromMongoDB();
 	});
 
-	//isbroadcaster
-	//ismod
-	//argument
-
-	//0-0
-	test.skip("IsBroadcasterIsFalse_AndIsModIsFalse_ShouldReturnString", async () => {
+	test("IsBroadcasterIsFalse_AndIsModIsFalse_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
 		isMod = false;
@@ -43,11 +44,10 @@ describe("addMessage", () => {
 		});
 
 		//Assert
-		expect(result[0]).toMatch(/!addmessage command is for Mods only/);
+		expect(result[0]).toMatch(/!addMessage command is for Mods only/);
 	});
 
-	//0-1-0
-	test.skip("IsBroadcasterIsFalse_AndIsModIsTrue_AndArgumentUndefined_ShouldReturnString", async () => {
+	test("IsBroadcasterIsFalse_AndIsModIsTrue_AndArgumentIsUndefined_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
 		isMod = true;
@@ -61,17 +61,14 @@ describe("addMessage", () => {
 		});
 
 		//Assert
-		expect(result[0]).toMatch(
-			/To add a Message use !addMessage [message output]/
-		);
+		expect(result[0]).toMatch(/To add a Message use !addMessage/);
 	});
 
-	//0-1-1
-	test.skip("IsBroadcasterIsFalse_AndIsModIsTrue_AndArgumentIsString_ShouldReturnString", async () => {
+	test("IsBroadcasterIsFalse_AndIsModIsTrue_AndArgumentIsString_AndNotInDatabase_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = false;
 		isMod = true;
-		argument = "This is a test message1";
+		argument = "This is a test message 3";
 
 		//Act
 		let result = await response({
@@ -81,18 +78,39 @@ describe("addMessage", () => {
 			argument,
 		});
 
+		cleanUpList.push(argument);
+
 		//Assert
-		expect(result[0]).toMatch(
-			/To add a Message use !addMessage [message output]/
-		);
+		expect(result[0]).toMatch(/This is a test message 3/);
 	});
 
-	//1-0-0
-	test.skip("IsBroadcasterIsTrue_AndIsModIsFalse_AndNoUserInfo_ShouldReturnString", async () => {
+	test("IsBroadcasterIsFalse_AndIsModIsTrue_AndArgumentIsString_AndInDatabase_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = false;
+		isMod = true;
+		argument = "This is a test message 4";
+
+		await dbSetup(argument);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		cleanUpList.push(argument);
+
+		//Assert
+		expect(result[0]).toMatch(/This Message has already been added/);
+	});
+
+	test("IsBroadcasterIsTrue_AndIsModIsFalse_AndArgumentIsUndefined_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
 		isMod = false;
-		userInfo = {};
+		argument = {};
 
 		//Act
 		let result = await response({
@@ -103,15 +121,56 @@ describe("addMessage", () => {
 		});
 
 		//Assert
-		expect(result[0]).toMatch(/!addmessage command is for Mods only/);
+		expect(result[0]).toBe("To add a Message use !addMessage [message output]");
 	});
 
-	//1-0-1
-	test.skip("IsBroadcasterIsTrue_AndIsModIsFalse_AndUserInfo_ShouldReturnString", async () => {
+	test("IsBroadcasterIsTrue_AndIsModIsFalse_AndArgumentIsString_AndnOTInDatabase_ShouldReturnString", async () => {
 		//Assemble
 		isBroadcaster = true;
 		isMod = false;
-		userInfo = {};
+		argument = "This is a test message 6";
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		cleanUpList.push(argument);
+
+		//Assert
+		expect(result[0]).toMatch(/This is a test message 6/);
+	});
+
+	test("IsBroadcasterIsTrue_AndIsModIsTrue_AndArgumentIsString_AndInDatabase_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = false;
+		argument = "This is a test message 7";
+
+		await dbSetup(argument);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		cleanUpList.push(argument);
+
+		//Assert
+		expect(result[0]).toMatch(/This Message has already been added/);
+	});
+
+	test("IsBroadcasterIsTrue_AndIsModIsTrue_AndArgumentIsUndefined_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		argument = {};
 
 		//Act
 		let result = await response({
@@ -122,9 +181,64 @@ describe("addMessage", () => {
 		});
 
 		//Assert
-		expect(result[0]).toMatch(/!addmessage command is for Mods only/);
+		expect(result[0]).toBe("To add a Message use !addMessage [message output]");
 	});
 
-	//1-1-0
-	//1-1-1
+	test("IsBroadcasterIsTrue_AndIsModIsTrue_AndArgumentIsString_AndnOTInDatabase_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		argument = "This is a test message 9";
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		cleanUpList.push(argument);
+
+		//Assert
+		expect(result[0]).toMatch(/This is a test message 9/);
+	});
+
+	test("IsBroadcasterIsTrue_AndIsModIsTrue_AndArgumentIsString_AndInDatabase_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		isMod = true;
+		argument = "This is a test message 10";
+
+		await dbSetup(argument);
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		cleanUpList.push(argument);
+
+		//Assert
+		expect(result[0]).toMatch(/This Message has already been added/);
+	});
 });
+
+async function dbSetup(setupMessage) {
+	let message = await Message.findOne({ text: setupMessage });
+
+	if (!message) {
+		let messageList = await Message.find({});
+		await Message.create({
+			index: helper.getNextIndex(messageList),
+			text: setupMessage,
+		});
+	}
+}
+
+async function dBCleanUp(cleanUpMessages) {
+	await Message.deleteMany({ text: { $in: cleanUpMessages } });
+}
