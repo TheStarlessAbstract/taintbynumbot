13f37f251cffe8a25ea8ef2bbd67bb19da4346fd
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-05 20:01:30 +0000

    Update kings-reset.js

--- commands/kings-reset.js
+++ commands/kings-reset.js
@@ -1,13 +1,16 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const kings = require("./kings");
+
+const helper = new Helper();
 
 let commandResponse = () => {
 	return {
 		response: async (config) => {
 			let result = [];
 
-			if (config.isModUp) {
+			if (helper.isValidModeratorOrStreamer(config)) {
 				await kings.resetKings();
 				let cardsToDraw = kings.getCardsToDraw();
 
