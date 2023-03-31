fa88b8ddbe9e914d6a7bbdb239722baf40ec1cca
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-31 19:53:48 +0100

    Update server-io.js

--- server-io.js
+++ server-io.js
@@ -108,7 +108,7 @@ async function setup(newIo) {
 
 			let redirectUri = "http://localhost:5000/test";
 			let scope =
-				"channel:manage:predictions+channel:manage:redemptions+channel:read:predictions+" +
+				"channel:manage:broadcast+channel:manage:predictions+channel:manage:redemptions+channel:read:predictions+" +
 				"channel:read:redemptions+channel:read:subscriptions+channel_subscriptions";
 
 			io.emit("setDetails", { clientId, redirectUri, scope });
