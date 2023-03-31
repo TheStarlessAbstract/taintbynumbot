3882894963a873deea1ebdc520f4b9d25ced19e1
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-31 04:20:21 +0100

    Update lurk.test.js

--- tests/commands/lurk.test.js
+++ tests/commands/lurk.test.js
@@ -3,42 +3,44 @@ require("dotenv").config();
 const lurk = require("../../commands/lurk");
 
 let isBroadcaster;
-let isModUp;
+let isMod;
 let userInfo;
 let argument;
 let commandLink = lurk.command;
 const { response } = commandLink.getCommand();
 
-describe.skip("lurk", () => {
-	test("NoUserInfo_ShouldReturnUndefined", async () => {
+describe("lurk", () => {
+	test("IsBroadcasterFalse_ShouldReturnUndefined", async () => {
 		//Assemble
-		userInfo = {};
-
-		//Act
-		let result = await response({
-			isBroadcaster,
-			isModUp,
-			userInfo,
-			argument,
-		});
-
-		//Assert
-		expect(result[0]).toBeUndefined();
-	});
-
-	test("HasUserInfo_ShouldReturnString", async () => {
-		//Assemble
+		isBroadcaster = false;
 		userInfo = { displayName: "design_by_rose" };
 
 		//Act
 		let result = await response({
 			isBroadcaster,
-			isModUp,
+			isMod,
 			userInfo,
 			argument,
 		});
 
 		//Assert
 		expect(result[0]).toMatch(/@design_by_rose finds a comfortable spot/);
+	});
+
+	test("IsBroadcasterTrue_ShouldReturnString", async () => {
+		//Assemble
+		isBroadcaster = true;
+		userInfo = { displayName: "TheStarlessAbstract" };
+
+		//Act
+		let result = await response({
+			isBroadcaster,
+			isMod,
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
 	});
 });
