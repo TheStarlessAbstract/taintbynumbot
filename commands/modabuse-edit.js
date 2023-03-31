78c8b71f57b7c04436c7c538c347dbeaa1284a15
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-03-31 19:45:29 +0100

    Update modabuse-edit.js

--- commands/modabuse-edit.js
+++ commands/modabuse-edit.js
@@ -1,78 +1,55 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const Title = require("../models/title");
+
+const helper = new Helper();
 
 let commandResponse = () => {
 	return {
 		response: async (config) => {
 			let result = [];
 
-			if (config.isModUp && config.argument) {
-				let index = config.argument.split(/\s(.+)/)[0].toLowerCase();
-				let text;
+			if (
+				helper.isValidModeratorOrStreamer(config) &&
+				helper.isValuePresentAndString(config.argument)
+			) {
+				let index = helper.getCommandArgumentKey(config, 0);
+				let text = helper.getCommandArgumentKey(config, 1);
 
-				if (isNaN(index)) {
-					text = config.argument;
-				} else {
-					text = config.argument.split(/\s(.+)/)[1];
-				}
+				if (
+					helper.isValuePresentAndNumber(index) &&
+					helper.isValuePresentAndString(text)
+				) {
+					let title = await Title.findOne({ index: index });
 
-				if (versions[0].active && !isNaN(index)) {
-					let quote = await Title.findOne({ index: index });
-					if (quote) {
-						result.push("ModAbuse " + index + " was: " + quote.text);
-						quote.text = text;
-						await quote.save();
+					if (title) {
+						if (title.text == text) {
+							result.push("ModAbuse " + index + " already says: " + title.text);
+						} else if (title.text != text) {
+							result.push(
+								"ModAbuse " +
+									index +
+									" has been updated - it previously was: " +
+									title.text
+							);
 
-						result.push(
-							"ModAbuse " + index + " has been updated to: " + quote.text
-						);
-					} else {
-						result.push("No ModAbuse number " + config.argument + " found");
-					}
-				} else if (versions[1].active && config.argument && isNaN(index)) {
-					let entries = await Title.find({
-						text: { $regex: text, $options: "i" },
-					});
-
-					if (entries.length > 0) {
-						let pularlity = entries.length > 1 ? "ModAbuses" : "ModAbuse";
-
-						result.push(
-							entries.length + " " + pularlity + " found mentioning: " + text
-						);
-
-						let output;
-						if (entries.length > 1) {
-							output = "Indexes include: ";
-							for (let i = 0; i < 5; i++) {
-								output += entries[i].index + ", ";
-							}
-
-							result.push(output);
-
-							if (entries.length > 5) {
-								result.push(
-									"There are more matches, maybe you could be more specific"
-								);
-							}
-						} else {
-							output =
-								entries[0].index +
-								" is the number of the ModAbuse you are looking to edit. Use !editmodabuse " +
-								entries[0].index;
-
-							result.push(output);
+							title.text = text;
+							await title.save();
 						}
 					} else {
-						result.push("No ModAbuse found including '" + text + "'");
+						result.push("No ModAbuse " + index + " found");
 					}
+				} else {
+					result.push(
+						"!editModAbuse [index] [updated text] - !editModAbuse 69 It's all about the booty"
+					);
 				}
-			} else if (!config.isModUp) {
-				result.push("!editmodabuse command is for Mods only");
-			} else if (!config.argument) {
+			} else if (!helper.isValidModeratorOrStreamer(config)) {
+				result.push("!editModAbuse is for Mods only");
+			} else if (!helper.isValuePresentAndString(config.argument)) {
 				result.push(
-					"To edit a ModAbuse, you must include the ModAbuse number like !editmodabuse 69, or include a search string like !editmodabuse uwu"
+					"To edit a ModAbuse, use !editModAbuse [index] [updated text]"
 				);
 			}
 
@@ -85,13 +62,6 @@ let versions = [
 	{
 		description: "Edits an existing ModAbuse",
 		usage: "!editmodabuse 69 What in the fuck?",
-		usableBy: "mods",
-		active: true,
-	},
-	{
-		description:
-			"Searches for a ModAbuse with this string, returns the index number of the ModAbuse, or quotes if multiple. Use above version to edit specific ModAbuse",
-		usage: "!editmodabuse sit on my face",
 		usableBy: "mods",
 		active: true,
 	},
