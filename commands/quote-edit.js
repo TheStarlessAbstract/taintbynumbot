43725563e1abd908722b96ccbbf0096f068f44a7
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-04-01 18:31:35 +0100

    Update quote-edit.js

--- commands/quote-edit.js
+++ commands/quote-edit.js
@@ -1,70 +1,53 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const Quote = require("../models/quote");
+
+const helper = new Helper();
 
 let commandResponse = () => {
 	return {
 		response: async (config) => {
 			let result = [];
 
-			if (config.isModUp && config.argument) {
-				let index = config.argument.split(/\s(.+)/)[0].toLowerCase();
-				let text = config.argument.split(/\s(.+)/)[1];
+			if (
+				helper.isValidModeratorOrStreamer(config) &&
+				helper.isValuePresentAndString(config.argument)
+			) {
+				let index = helper.getCommandArgumentKey(config, 0);
+				let text = helper.getCommandArgumentKey(config, 1);
 
-				if (versions[0].active && !isNaN(index)) {
+				if (
+					helper.isValuePresentAndNumber(index) &&
+					helper.isValuePresentAndString(text)
+				) {
 					let quote = await Quote.findOne({ index: index });
+
 					if (quote) {
-						result.push("Quote " + index + " was: " + quote.text);
-						quote.text = text;
-						await quote.save();
-
-						result.push(
-							"Quote " + index + " has been updated to: " + quote.text
-						);
-					} else {
-						result.push("No quote number " + config.argument + " found");
-					}
-				} else if (versions[1].active && config.argument && isNaN(index)) {
-					let entries = await Quote.find({
-						text: { $regex: text, $options: "i" },
-					});
-
-					if (entries.length > 0) {
-						let pularlity = entries.length > 1 ? "quotes" : "quote";
-
-						result.push(
-							entries.length + " " + pularlity + " found mentioning: " + text
-						);
-
-						let output;
-						if (entries.length > 1) {
-							output = "Indexes include: ";
-							for (let i = 0; i < 5; i++) {
-								output += entries[i].index + ", ";
-							}
-
-							result.push(output);
-
-							if (entries.length > 5) {
-								result.push(
-									"There are more matches, maybe you could be more specific"
-								);
-							}
+						if (quote.text == text) {
+							result.push("Quote " + index + " already says: " + quote.text);
 						} else {
-							output =
-								entries[0].index +
-								" is the number of the quote you are looking to edit. Use !editquote " +
-								entries[0].index;
+							result.push("Quote " + index + " was: " + quote.text);
+							quote.text = text;
+							await quote.save();
+
+							result.push(
+								"Quote " + index + " has been updated to: " + quote.text
+							);
 						}
 					} else {
-						result.push("No quotes found including '" + text + "'");
+						result.push("No Quote " + index + " found");
 					}
+				} else {
+					result.push(
+						"!editQuote [quote number] [updated text] - !editModAbuse 69 It's all about the booty"
+					);
 				}
-			} else if (!config.isModUp) {
-				result.push("!editQuote command is for Mods only");
-			} else if (!config.argument) {
+			} else if (!helper.isValidModeratorOrStreamer(config)) {
+				result.push("!editQuote is for Mods only");
+			} else if (!helper.isValuePresentAndString(config.argument)) {
 				result.push(
-					"To edit a quote, you must include the quote number like !editquote 69, or include a search string like !editquote uwu"
+					"To edit a Quote, use !editQuote [quote number] [quote text]"
 				);
 			}
 
@@ -77,13 +60,6 @@ let versions = [
 	{
 		description: "Edits an existing quote",
 		usage: "!editQuote 69 What in the fuck?",
-		usableBy: "mods",
-		active: true,
-	},
-	{
-		description:
-			"Searches for a quote with this string, returns the index number of the quote, or quotes if multiple. Use above version to edit specific quote",
-		usage: "!editQuote sit on my face",
 		usableBy: "mods",
 		active: true,
 	},
