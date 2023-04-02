7341be8f01ecd62375b05f2b13dfc80242ca7c5d
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-04-02 19:45:51 +0100

    Update modabuse-add.js

--- commands/modabuse-add.js
+++ commands/modabuse-add.js
@@ -1,9 +1,11 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const chatClient = require("../bot-chatclient");
 
 const Title = require("../models/title");
 
+const helper = new Helper();
 let twitchId = process.env.TWITCH_USER_ID;
 
 let commandResponse = () => {
@@ -11,61 +13,59 @@ let commandResponse = () => {
 		response: async (config) => {
 			let result = [];
 
-			if (config.isModUp) {
-				let entries = await Title.find({});
-				let index = entries.length ? getNextIndex(entries) : 1;
-				let message;
+			if (helper.isValidModeratorOrStreamer(config)) {
+				let title;
 
-				if (versions[0].active && !config.argument) {
-					try {
-						const apiClient = chatClient.getApiClient();
-						let channel = await apiClient.channels.getChannelInfo(twitchId);
+				if (
+					helper.isVersionActive(versions, 0) &&
+					!helper.isValuePresentAndString(config.argument)
+				) {
+					let apiClient = await chatClient.getApiClient();
+					let channel = await apiClient.channels.getChannelInfo(twitchId);
 
-						if (channel == null) {
-							result.push(
-								"Twitch says no, and Starless should really sort this out some time after stream"
-							);
-						} else {
-							message = channel.title;
-						}
-					} catch (err) {
+					if (channel == null) {
 						result.push(
 							"Twitch says no, and Starless should really sort this out some time after stream"
 						);
-					}
-				} else if (versions[1].active && config.argument) {
-					if (config.argument.includes("@")) {
-						config.argument = config.argument.split("@");
-						message = config.argument[0];
 					} else {
-						message = config.argument;
+						title = channel.title;
 					}
+				} else if (
+					helper.isVersionActive(versions, 1) &&
+					helper.isValuePresentAndString(config.argument)
+				) {
+					title = config.argument;
 				}
 
 				if (result.length == 0) {
-					try {
-						let created = await Title.create({
+					let existingTitle = await Title.findOne({ text: title });
+
+					if (existingTitle != null) {
+						result.push("This Title has already been added");
+					} else {
+						let entry = await Title.findOne()
+							.sort({ index: -1 })
+							.select("index")
+							.exec();
+
+						let index;
+						if (entry) {
+							index = entry.index + 1;
+						} else {
+							index = 1;
+						}
+
+						await Title.create({
 							index: index,
-							text: message,
+							text: title,
 							addedBy: config.userInfo.displayName,
 						});
 
-						if (created._id) {
-							result.push("Title added");
-						}
-					} catch (err) {
-						if (err.code == 11000) {
-							result.push("This title has already been added");
-						} else {
-							console.log(err);
-							result.push(
-								"There was some problem adding the title, and Starless should really sort this shit out."
-							);
-						}
+						result.push("Title added");
 					}
 				}
-			} else if (!config.isModUp) {
-				result.push("!addModAbuse command is for Mods only");
+			} else {
+				result.push("!addModAbuse is for Mods only");
 			}
 
 			return result;
@@ -92,9 +92,5 @@ let versions = [
 ];
 
 const addModAbuse = new BaseCommand(commandResponse, versions);
-
-function getNextIndex(array) {
-	return array[array.length - 1].index + 1;
-}
 
 exports.command = addModAbuse;
