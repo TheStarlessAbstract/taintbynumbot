247cdba0a6d7b192ac8b98780962bf847ac839b8
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-04-02 19:45:56 +0100

    Update tinder-add.js

--- commands/tinder-add.js
+++ commands/tinder-add.js
@@ -1,59 +1,49 @@
 const BaseCommand = require("../classes/base-command");
+const Helper = require("../classes/helper");
 
 const Tinder = require("../models/tinder");
+
+const helper = new Helper();
 
 let commandResponse = () => {
 	return {
 		response: async (config) => {
 			let result = [];
 
-			if (versions[0].active && config.isModUp && config.argument) {
-				let entries = await Tinder.find({});
-				let index = entries.length ? getNextIndex(entries) : 1;
-				let message;
-				let user;
+			if (
+				helper.isValidModeratorOrStreamer(config) &&
+				helper.isValuePresentAndString(config.argument)
+			) {
+				let tinderText = config.argument;
+				let existingTinder = await Tinder.findOne({ text: tinderText });
 
-				if (config.argument.includes("@")) {
-					config.argument = config.argument.split("@");
-					message = config.argument[0];
-					user = config.argument[1];
+				if (existingTinder != null) {
+					result.push("This Tinder bio has already been added");
 				} else {
-					message = config.argument;
-					user = "";
-				}
+					let entry = await Tinder.findOne()
+						.sort({ index: -1 })
+						.select("index")
+						.exec();
 
-				try {
+					let index;
+					if (entry) {
+						index = entry.index + 1;
+					} else {
+						index = 1;
+					}
+
 					await Tinder.create({
 						index: index,
-						user: user,
-						text: message,
+						text: tinderText,
 						addedBy: config.userInfo.displayName,
 					});
-					result.push(["Added new Tinder bio"]);
 
-					if (!user) {
-						result.push([
-							"To add the name of the author of this Tinder bio, use the command: !edittinderauthor " +
-								index +
-								" @USERNAME",
-						]);
-					}
-				} catch (err) {
-					if (err.code == 11000) {
-						result.push("This Tinder bio has already been added");
-					} else {
-						console.log(err);
-						result.push(
-							"There was some problem adding this Tinder bio, and Starless should really sort this shit out."
-						);
-					}
+					result.push("New Tinder bio added: " + tinderText);
 				}
-			} else if (!config.isModUp) {
-				result.push(["!addTinder command is for Mods only"]);
+			} else if (!helper.isValidModeratorOrStreamer(config)) {
+				result.push("!addTinder is for Mods only");
 			} else if (!config.argument) {
-				result.push([
-					"To add a Tinder quote, you must include the quote after the command: '!addtinder Never mind about carpe diem, carpe taint @design_by_rose'",
-				]);
+				result.push("To add a Tinder bio use !addTinder [tinder bio]");
 			}
 
 			return result;
@@ -65,16 +55,12 @@ let versions = [
 	{
 		description: "To save Starless' new Tinder bio",
 		usage:
-			"!addtinder As long as my face is around, you will always have some place to sit",
+			"!addTinder As long as my face is around, you will always have some place to sit",
 		usableBy: "mods",
 		active: true,
 	},
 ];
 
 const addTinder = new BaseCommand(commandResponse, versions);
-
-function getNextIndex(array) {
-	return array[array.length - 1].index + 1;
-}
 
 exports.command = addTinder;
