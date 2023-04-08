74679fab7a09fbafde35b5096df33cfbd5cf5751
TheStarlessAbstract <david.foley@outlook.com>
2023-04-08 21:19:29 +0100

    Update drinkbitch.js

--- commands/drinkbitch.js
+++ commands/drinkbitch.js
@@ -19,7 +19,7 @@ let commandResponse = () => {
 			let currentTime = new Date();
 
 			if (
-				helper.isValuePresentAndNumber(config.userInfo.userId) &&
+				helper.isValuePresentAndString(config.userInfo.userId) &&
 				helper.isValuePresentAndString(config.userInfo.displayName) &&
 				(helper.isCooldownPassed(currentTime, drinkBitch.timer, cooldown) ||
 					helper.isStreamer(config))
