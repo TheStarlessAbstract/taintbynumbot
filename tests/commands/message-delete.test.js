9e5f6b819cd5379e01c93627c858abec1b708f8b
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-06-18 20:33:23 +0100

    Update message-delete.test.js

--- tests/commands/message-delete.test.js
+++ tests/commands/message-delete.test.js
@@ -11,7 +11,7 @@ let argument;
 let commandLink = messageDelete.command;
 const { response } = commandLink.getCommand();
 
-describe("delMessage", () => {
+describe.skip("delMessage", () => {
 	let cleanUpList = [];
 
 	beforeAll(async () => {
