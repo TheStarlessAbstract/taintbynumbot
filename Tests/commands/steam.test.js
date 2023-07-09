947f7111c1adb0c2d6f13df20ddcf19d4c698112
TheStarlessAbstract <david.foley@outlook.com>
2023-07-09 23:14:05 +0100

    Update steam.test.js

--- Tests/commands/steam.test.js
+++ Tests/commands/steam.test.js
@@ -114,351 +114,88 @@ describe("steam", () => {
 		});
 
 		//Assert
+		expect(result[0]).toMatch(/You should play/);
+	}, 30000);
+
+	/////////
+
+	test("IsBroadcasterIsTrue_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
+		//Assemble
+		userInfo.isBroadcaster = true;
+		commandLink.setTimer(currentDateTime - 1000);
+		argument = undefined;
+
+		//Act
+		let result = await response({
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndArgumentIsUndefined_ShouldReturnUndefined", async () => {
+		//Assemble
+		userInfo.isBroadcaster = true;
+		commandLink.setTimer(currentDateTime - 31000);
+		argument = undefined;
+
+		//Act
+		let result = await response({
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toBeUndefined();
+	});
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndArgumentIsString_AndUsernameIsNotFound_ShouldReturnString", async () => {
+		//Assemble
+		userInfo.isBroadcaster = true;
+		commandLink.setTimer(currentDateTime - 31000);
+		argument = "thestarlesssbstract";
+
+		//Act
+		let result = await response({
+			userInfo,
+			argument,
+		});
+
+		//Assert
+		expect(result[0]).toMatch(/Username not found/);
+	}, 10000);
+
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndArgumentIsString_AndUsernameIsInvalidFormat_ShouldReturnString", async () => {
+		//Assemble
+		userInfo.isBroadcaster = true;
+		commandLink.setTimer(currentDateTime - 31000);
+		argument = "a";
+
+		//Act
+		let result = await response({
+			userInfo,
+			argument,
+		});
+
+		//Assert
 		expect(result[0]).toMatch(/invalid format/);
 	}, 10000);
 
-	// test("IsBroadcasterIsFalse_AndIsModIsFalse_AndCoolDownElapsed_AndVersionOneActive_AndArgumentIsString_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest06";
+	test("IsBroadcasterIsTrue_AndCoolDownElapsed_AndArgumentIsString_AndUsernameIsFound_ShouldReturnString", async () => {
+		//Assemble
+		userInfo.isBroadcaster = true;
+		commandLink.setTimer(currentDateTime - 31000);
+		argument = "TheStarlessAbstract";
 
-	// 	commandLink.setVersionActive(0);
+		//Act
+		let result = await response({
+			userInfo,
+			argument,
+		});
 
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsFalse_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsUndefined_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toMatch(/The curent title is: /);
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsFalse_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsString_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest08";
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// ///////
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 1000);
-	// 	argument = undefined;
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndNoVersionsActive_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-	// 	commandLink.setVersionActive(0);
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndVersionZeroActive_AndArgumentIsUndefined_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toMatch(/The curent title is: /);
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndVersionZeroActive_AndArgumentIsString_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest12";
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndVersionOneActive_AndArgumentIsUndefined_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-
-	// 	commandLink.setVersionActive(0);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndVersionOneActive_AndArgumentIsString_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest14";
-
-	// 	commandLink.setVersionActive(0);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBe("Title has been set to " + argument);
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsUndefined_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toMatch(/The curent title is: /);
-	// });
-
-	// test("IsBroadcasterIsFalse_AndIsModIsTrue_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsString_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = false;
-	// 	userInfo.isMod = true;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest16";
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBe("Title has been set to " + argument);
-	// });
-
-	// ////////
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownNotElapsed_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 1000);
-	// 	argument = undefined;
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndNoVersionsActive_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-	// 	commandLink.setVersionActive(0);
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndVersionZeroActive_AndArgumentIsUndefined_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toMatch(/The curent title is: /);
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndVersionZeroActive_AndArgumentIsString_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest20";
-	// 	commandLink.setVersionActive(1);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndVersionOneActive_AndArgumentIsUndefined_ShouldReturnUndefined", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-
-	// 	commandLink.setVersionActive(0);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBeUndefined();
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndVersionOneActive_AndArgumentIsString_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest22";
-
-	// 	commandLink.setVersionActive(0);
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBe("Title has been set to " + argument);
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsUndefined_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = undefined;
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toMatch(/The curent title is: /);
-	// });
-
-	// test("IsBroadcasterIsTrue_AndIsModIsFalse_AndCoolDownElapsed_AndAllVersionsActive_AndArgumentIsString_ShouldReturnString", async () => {
-	// 	//Assemble
-	// 	userInfo.isBroadcaster = true;
-	// 	userInfo.isMod = false;
-	// 	commandLink.setTimer(currentDateTime - 31000);
-	// 	argument = "titleTest24";
-
-	// 	//Act
-	// 	let result = await response({
-	// 		userInfo,
-	// 		argument,
-	// 	});
-
-	// 	//Assert
-	// 	expect(result[0]).toBe("Title has been set to " + argument);
-	// });
+		//Assert
+		expect(result[0]).toMatch(/You should play/);
+	}, 30000);
 });
