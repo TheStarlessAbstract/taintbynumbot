16fb9ecfec4f0c2e42ec7e2daf8e83835f3a9c46
TheStarlessAbstract <david.foley@outlook.com>
2023-07-17 01:05:46 +0100

    Update helper.js

--- classes/helper.js
+++ classes/helper.js
@@ -120,6 +120,13 @@ class Helper {
 
 		return array;
 	}
+
+	startsWithCaseInsensitive(string, subString) {
+		let stringLowercase = string.toLowerCase();
+		let subStringLowercase = subString.toLowerCase();
+
+		return stringLowercase.startsWith(subStringLowercase);
+	}
 }
 
 module.exports = Helper;
