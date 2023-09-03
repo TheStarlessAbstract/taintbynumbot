21c633415e41137b7165e65d1e2531d687d83255
TheStarlessAbstract <david.foley@outlook.com>
2023-09-03 22:40:33 +0100

    Create spotifyServices.js

---
+++ services/spotifyServices.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+const spotify = require("../repos/spotifyRepo");
+
+async function getCurrentPlaying(channelId) {
+	const token = await spotify.getToken(channelId);
+
+	if (token == null) {
+		return "no token";
+	}
+
+	const response = await axios.get(
+		"https://api.spotify.com/v1/me/player/currently-playing",
+		{
+			headers: {
+				Authorization: "Bearer " + token.access_token,
+			},
+		}
+	);
+
+	let result;
+
+	if (response.statusCode == 401) {
+		result = "poop";
+	} else {
+		result =
+			"The song playing is: " +
+			response.data.item.name +
+			" by " +
+			response.data.item.artists[0].name;
+	}
+
+	return result;
+}
+
+exports.getCurrentPlaying = getCurrentPlaying;
