71f98eea6e2e56092ac1a4ce41af1dbaab9ec377
TheStarlessAbstract <david.foley@outlook.com>
2023-09-03 23:54:06 +0100

    Update spotifyServices.js

--- services/spotifyServices.js
+++ services/spotifyServices.js
@@ -5,7 +5,7 @@ const spotify = require("../repos/spotifyRepo");
 async function getCurrentPlaying(channelId) {
 	const token = await spotify.getToken(channelId);
 
-	if (token == null) {
+	if (token === null) {
 		return "no token";
 	}
 
@@ -13,21 +13,24 @@ async function getCurrentPlaying(channelId) {
 		"https://api.spotify.com/v1/me/player/currently-playing",
 		{
 			headers: {
-				Authorization: "Bearer " + token.access_token,
+				Authorization: "Bearer " + token.accessToken,
 			},
 		}
 	);
 
 	let result;
 
-	if (response.statusCode == 401) {
-		result = "poop";
+	if (response.status === 200) {
+		result = `The song playing is: ${response.data.item.name} by ${response.data.item.artists[0].name}. 
+		Link: ${response.data.item.external_urls.spotify}`;
+	} else if (response.statusCode == 401) {
+		result = `Can't access Spotify account, due to bad or expired token. Please rea-authenticate`;
+	} else if (response.statusCode == 403) {
+		result = `Can't access Spotify account, due to a bad OAuth request. Starless, fix it`;
+	} else if (response.statusCode == 429) {
+		result = `Can't access Spotify account, as the application has exceeded rate limits, try again...much later`;
 	} else {
-		result =
-			"The song playing is: " +
-			response.data.item.name +
-			" by " +
-			response.data.item.artists[0].name;
+		result = `Can't access Spotify account, no idea really, error code is: ${response.statusCode}`;
 	}
 
 	return result;
