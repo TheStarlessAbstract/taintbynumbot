4bea308b7c62cede52c1b2b81ff6d40438f0277b
TheStarlessAbstract <david.foley@outlook.com>
2023-09-15 23:12:17 +0100

    Update spotifyServices.js

--- services/spotifyServices.js
+++ services/spotifyServices.js
@@ -9,35 +9,36 @@ async function getCurrentPlaying(channelId) {
 		return "no token";
 	}
 
-	// check if playing
-	axios.get("https://api.spotify.com/v1/me/player/", {
+	const response = await axios.get("https://api.spotify.com/v1/me/player/", {
 		headers: {
 			Authorization: "Bearer " + token.accessToken,
 		},
 	});
 
-	const response = await axios.get(
-		"https://api.spotify.com/v1/me/player/currently-playing",
-		{
-			headers: {
-				Authorization: "Bearer " + token.accessToken,
-			},
-		}
-	);
+	let result = "";
 
-	let result;
+	switch (response.statusCode) {
+		case 401:
+			result = `Can't access Spotify account, due to bad or expired token. Please re-authenticate`;
+			break;
+		case 403:
+			result = `Can't access Spotify account, due to a bad OAuth request. Starless, fix it`;
+			break;
+		case 429:
+			result = `Can't access Spotify account, as the application has exceeded rate limits, try again...much later`;
+			break;
+		default:
+			//if other statusCode
+			if (response?.statusCode) {
+				result = `Can't access Spotify account, no idea really, error code is: ${response.statusCode}`;
+			} else if (!response.data.is_playing) {
+				result = "";
+			} else if (response.data.is_playing) {
+				result = `The song playing is: ${response.data.item.name} by ${response.data.item.artists[0].name}.
+				Link: ${response.data.item.external_urls.spotify}`;
+			}
 
-	if (response.status === 200) {
-		result = `The song playing is: ${response.data.item.name} by ${response.data.item.artists[0].name}. 
-		Link: ${response.data.item.external_urls.spotify}`;
-	} else if (response.statusCode == 401) {
-		result = `Can't access Spotify account, due to bad or expired token. Please rea-authenticate`;
-	} else if (response.statusCode == 403) {
-		result = `Can't access Spotify account, due to a bad OAuth request. Starless, fix it`;
-	} else if (response.statusCode == 429) {
-		result = `Can't access Spotify account, as the application has exceeded rate limits, try again...much later`;
-	} else {
-		result = `Can't access Spotify account, no idea really, error code is: ${response.statusCode}`;
+			break;
 	}
 
 	return result;
