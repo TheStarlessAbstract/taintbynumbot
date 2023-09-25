cb35badc9130f0829d69928dae9f75b9e5f0768a
TheStarlessAbstract <david.foley@outlook.com>
2023-09-25 19:44:39 +0100

    Create quote-me-dirty.js

---
+++ redemptions/quote-me-dirty.js
@@ -0,0 +1,51 @@
+const Helper = require("../classes/helper");
+
+const Quote = require("../models/quote");
+
+const botChatClient = require("../bot-chatclient");
+
+const twitch = require("../services/twitch");
+
+const helper = new Helper();
+
+let twitchId = process.env.TWITCH_USER_ID;
+
+async function index(channelId) {
+	let chatClient = botChatClient.getChatClient();
+	let randomQuote = [];
+	try {
+		randomQuote = await Quote.aggregate([{ $sample: { size: 1 } }]);
+
+		if (randomQuote.length === 0) {
+			console.log("No quotes found.");
+		} else {
+			console.log("Random Document:", randomQuote[0].text);
+		}
+	} catch (err) {
+		randomQuote.push({ text: "" });
+		console.error(err);
+	}
+
+	if (randomQuote[0].text === "") {
+		return;
+	}
+
+	let channel = await twitch.getChannelInfoById(channelId);
+
+	chatClient.say(
+		"#" + channel.displayName,
+		"Time for another round of Quote me Dirty. Have your say in the poll."
+	);
+	chatClient.say("#" + channel.displayName, randomQuote[0].text);
+
+	let data = {
+		channelPointsPerVote: 69,
+		choices: ["Yes", "No"],
+		duration: 69,
+		title: "Was the quote dirty?",
+	};
+
+	await twitch.createPoll(channelId, data);
+}
+
+exports.index = index;
