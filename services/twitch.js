d4d9cde01b872c842706ff5715447b5bf190a81b
TheStarlessAbstract <thestarlessabstract@gmail.com>
2023-09-29 20:15:41 +0100

    Update twitch.js

--- services/twitch.js
+++ services/twitch.js
@@ -12,6 +12,27 @@ async function createPoll(broadcaster, data) {
 	console.log(poll.endDate);
 }
 
+async function createPrediction(broadcaster, data) {
+	let success = true;
+	try {
+		await pubSubApiClient.predictions.createPrediction(broadcaster, data);
+	} catch (err) {
+		success = false;
+
+		const errorMessage = err.message;
+		const messageStart =
+			errorMessage.indexOf('message":"') + 'message":"'.length;
+		const messageEnd = errorMessage.indexOf('"}', messageStart);
+		const extractedMessage = errorMessage.substring(messageStart, messageEnd);
+
+		console.error(
+			`ERROR - status: ${err.statusCode}, url: ${err.url}, function: createPrediction(), message: ${extractedMessage}`
+		);
+	}
+
+	return success;
+}
+
 async function setApiClient(apiClient) {
 	pubSubApiClient = apiClient;
 }
@@ -19,3 +40,4 @@ async function setApiClient(apiClient) {
 exports.setApiClient = setApiClient;
 exports.getChannelInfoById = getChannelInfoById;
 exports.createPoll = createPoll;
+exports.createPrediction = createPrediction;
