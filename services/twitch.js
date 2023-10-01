090a9a3191c6ecc40b15f16eadcf53778997ef00
TheStarlessAbstract <david.foley@outlook.com>
2023-10-01 20:58:53 +0100

    Update twitch.js

--- services/twitch.js
+++ services/twitch.js
@@ -6,19 +6,12 @@ async function getChannelInfoById(user) {
 	return channel;
 }
 
-async function createPoll(broadcaster, data) {
-	const poll = await pubSubApiClient.polls.createPoll(broadcaster, data);
-
-	console.log(poll.endDate);
-}
-
 async function createPrediction(broadcaster, data) {
 	let success = true;
 	try {
 		await pubSubApiClient.predictions.createPrediction(broadcaster, data);
 	} catch (err) {
 		success = false;
-
 		console.error(err);
 	}
 
@@ -31,7 +24,6 @@ async function getPredictions(broadcaster, pagination) {
 		predictions = await pubSubApiClient.predictions.getPredictions(broadcaster);
 	} catch (err) {
 		console.error(err);
-
 		predictions = null;
 	}
 
@@ -78,7 +70,6 @@ async function setApiClient(apiClient) {
 
 exports.setApiClient = setApiClient;
 exports.getChannelInfoById = getChannelInfoById;
-exports.createPoll = createPoll;
 exports.createPrediction = createPrediction;
 exports.getPredictions = getPredictions;
 exports.updateRedemptionStatusByIds = updateRedemptionStatusByIds;
