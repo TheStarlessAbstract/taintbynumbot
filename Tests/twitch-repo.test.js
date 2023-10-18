41383a7573843fc9c2dd5a54ea54950f04287fa6
TheStarlessAbstract <david.foley@outlook.com>
2023-10-18 20:40:06 +0100

    Create twitch-repo.test.js

---
+++ Tests/twitch-repo.test.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const db = require("./../bot-mongoose.js");
+const twitch = require("./../repos/twitch");
+
+describe("twitch", () => {
+	beforeAll(async () => {
+		db.connectToMongoDB();
+	});
+
+	afterAll(async () => {
+		await db.disconnectFromMongoDB();
+	});
+
+	test("twitch", async () => {
+		//Assemble
+
+		//Act
+		let result = await twitch.init();
+
+		//Assert
+		expect(result).toBe(true);
+	});
+});
