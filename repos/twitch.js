fc45ad3f4dfc2b4ce1e0e89ec851d786ff725481
TheStarlessAbstract <david.foley@outlook.com>
2023-11-19 20:17:02 +0000

    Update twitch.js

--- repos/twitch.js
+++ repos/twitch.js
@@ -1,17 +1,22 @@
 const ApiClient = require("@twurple/api").ApiClient;
+const ChatClient = require("@twurple/chat").ChatClient;
 const PubSubClient = require("@twurple/pubsub").PubSubClient;
 const RefreshingAuthProvider = require("@twurple/auth").RefreshingAuthProvider;
+const Helper = require("./../classes/helper");
 
 const User = require("./../models/user");
 
 const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 
-let pubSubClient;
+const helper = new Helper();
+
 let apiClient;
+let chatClient;
+let pubSubClient;
 
 async function init() {
-	let users = await User.find({}, "twitchId twitchToken").exec();
+	let users = await User.find({}, "twitchId role twitchToken").exec();
 
 	if (users.length == 0) {
 		return false;
@@ -23,13 +28,18 @@ async function init() {
 	for (let i = 0; i < users.length; i++) {
 		token = tokenFormat(users[i].twitchToken);
 
-		authProvider.addUser(users[i].twitchId, token, [
-			`pubsub:${users[i].twitchId}`,
-		]);
+		let intent = [`chat`];
+		if (users[i].role != "bot") {
+			intent = [`pubsub:${users[i].twitchId}`];
+		}
+
+		authProvider.addUser(users[i].twitchId, token, intent);
 	}
 
-	pubSubClient = new PubSubClient({ authProvider });
-	apiClient = new ApiClient({ authProvider });
+	apiClient = createApiClient(authProvider);
+	chatClient = await createChatClient(authProvider);
+	pubSubClient = createPubSubClient(authProvider);
+
 	return true;
 }
 
@@ -40,6 +50,7 @@ async function createAuthProvider() {
 	});
 
 	authProvider.onRefresh(async (twitchId, token) => {
+		console.log("refresh");
 		await updateUserTwitchToken(twitchId, token);
 	});
 
@@ -75,14 +86,45 @@ function tokenFormat(token) {
 	};
 }
 
-function getPubSubClient() {
-	return pubSubClient;
+function createApiClient(authProvider) {
+	return new ApiClient({ authProvider });
+}
+
+async function createChatClient(authProvider) {
+	return new ChatClient({
+		authProvider,
+		channels: await getUsernames(),
+		requestMembershipEvents: true,
+	});
+}
+
+function createPubSubClient(authProvider) {
+	new PubSubClient({ authProvider });
+}
+
+async function getUsernames() {
+	let usersFromDb = await User.find({ role: { $ne: "bot" } }, "twitchId");
+	let twitchIds = usersFromDb.map((user) => user.twitchId);
+
+	let usersFromTwitch = await apiClient.users.getUsersByIds(twitchIds);
+	let usernames = usersFromTwitch.map((user) => user.name);
+
+	return usernames;
 }
 
 function getApiClient() {
 	return apiClient;
 }
 
+function getChatClient() {
+	return chatClient;
+}
+
+function getPubSubClient() {
+	return pubSubClient;
+}
+
 exports.init = init;
-exports.getPubSubClient = getPubSubClient;
 exports.getApiClient = getApiClient;
+exports.getChatClient = getChatClient;
+exports.getPubSubClient = getPubSubClient;
