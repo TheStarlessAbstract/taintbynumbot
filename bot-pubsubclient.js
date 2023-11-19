5d4d5c3c07b5851e0a00d9d0e86f810daa04ac7d
TheStarlessAbstract <david.foley@outlook.com>
2023-11-19 20:16:48 +0000

    Update bot-pubsubclient.js

--- bot-pubsubclient.js
+++ bot-pubsubclient.js
@@ -1,14 +1,9 @@
-const twitchRepo = require("./repos/twitch");
-const twitchService = require("./services/twitch");
 const redemptions = require("./bot-redemptions");
-
 const loyalty = require("./bot-loyalty");
 
 let apiClient;
 
 async function init() {
-	let repoResult = await twitchRepo.init();
-	twitchService.init();
 	redemptions.init();
 	loyalty.init();
 }
