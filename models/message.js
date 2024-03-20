6f47bbe0153b484650cd80cdfc3a26d41233396c
TheStarlessAbstract <david.foley@outlook.com>
2024-03-20 16:50:16 +0000

    Update message.js

--- models/message.js
+++ models/message.js
@@ -2,12 +2,30 @@ let mongoose = require("mongoose"),
 	Schema = mongoose.Schema;
 
 let MessageSchema = new Schema({
-	index: { type: Number, unique: true },
+	index: { type: Number },
+	twitchId: { type: String },
 	text: {
 		type: String,
-		unique: true,
 	},
 	addedBy: String,
 });
 
+MessageSchema.index({ twitchId: 1, index: 1 }, { unique: true });
+MessageSchema.index({ twitchId: 1, text: 1 }, { unique: true });
+
 module.exports = mongoose.model("Message", MessageSchema);
+
+// ORIGINAL
+// let mongoose = require("mongoose"),
+// 	Schema = mongoose.Schema;
+
+// let MessageSchema = new Schema({
+// 	index: { type: Number, unique: true },
+// 	text: {
+// 		type: String,
+// 		unique: true,
+// 	},
+// 	addedBy: String,
+// });
+
+// module.exports = mongoose.model("Message", MessageSchema);
