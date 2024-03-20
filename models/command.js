7d35be1f54e36f1d775b851b3e86df2a59b89f01
TheStarlessAbstract <david.foley@outlook.com>
2024-03-20 16:50:00 +0000

    Update command.js

--- models/command.js
+++ models/command.js
@@ -2,12 +2,30 @@ let mongoose = require("mongoose"),
 	Schema = mongoose.Schema;
 
 let CommandSchema = new Schema({
-	name: {
-		type: String,
-		unique: true,
-	},
-	text: String,
-	createdBy: String,
+	streamerId: { type: String, required: true },
+	name: { type: String, required: true },
+	text: { type: String, required: true },
+	createdBy: { type: String, required: true },
+	createdOn: { type: Date, required: true },
+	lastEditedBy: String,
+	lastEditEdOn: String,
 });
 
+CommandSchema.index({ streamerId: 1, name: 1 }, { unique: true });
+
 module.exports = mongoose.model("Command", CommandSchema);
+
+// ORIGINAL
+// let mongoose = require("mongoose"),
+// 	Schema = mongoose.Schema;
+
+// let CommandSchema = new Schema({
+// 	name: {
+// 		type: String,
+// 		unique: true,
+// 	},
+// 	text: String,
+// 	createdBy: String,
+// });
+
+// module.exports = mongoose.model("Command", CommandSchema);
