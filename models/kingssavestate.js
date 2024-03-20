99c60d1acd021fb55b25e650f557a83fe3bdcfc9
TheStarlessAbstract <david.foley@outlook.com>
2024-03-20 16:50:08 +0000

    Update kingssavestate.js

--- models/kingssavestate.js
+++ models/kingssavestate.js
@@ -2,6 +2,7 @@ let mongoose = require("mongoose"),
 	Schema = mongoose.Schema;
 
 let KingsSaveStateSchema = new Schema({
+	twitchId: { type: String, unique: true },
 	cardsToDraw: Schema.Types.Mixed,
 	kingsCount: Number,
 	jagerPrizeCounter: Number,
