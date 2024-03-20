db5c4ad8f2f61a2c017e2503d22d43a9259b8b44
TheStarlessAbstract <david.foley@outlook.com>
2024-03-20 16:49:19 +0000

    Update bot-mongoose.js

--- bot-mongoose.js
+++ bot-mongoose.js
@@ -3,17 +3,43 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URI;
 
-function connectToMongoDB() {
-	mongoose.connect(uri, {
+async function connectToMongoDB() {
+	await mongoose.connect(uri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
 	mongoose.set("strictQuery", false);
+
+	console.log(getReadyState());
 }
 
 async function disconnectFromMongoDB() {
 	await mongoose.disconnect();
 }
 
+function getReadyState() {
+	let readyState;
+
+	switch (mongoose.connection.readyState) {
+		case 0:
+			readyState = "Mongoose disconnected";
+			break;
+		case 1:
+			readyState = "Mongoose connected";
+			break;
+		case 2:
+			readyState = "Mongoose connecting";
+			break;
+		case 3:
+			readyState = "Mongoose disconnecting";
+			break;
+		default:
+			readyState = "Unknown connection state";
+	}
+
+	return readyState;
+}
+
 exports.connectToMongoDB = connectToMongoDB;
 exports.disconnectFromMongoDB = disconnectFromMongoDB;
+exports.getReadyState = getReadyState;
