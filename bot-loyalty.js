5d5a2491000de05f954ed0d6ffbc2c0db2e35965
TheStarlessAbstract <david.foley@outlook.com>
2024-03-20 16:49:12 +0000

    Update bot-loyalty.js

--- bot-loyalty.js
+++ bot-loyalty.js
@@ -1,72 +1,45 @@
-const twitchRepo = require("./repos/twitch");
+const twitchService = require("./services/twitch");
 
 const LoyaltyPoint = require("./models/loyaltypoint");
 
-let twitchUserId = process.env.TWITCH_USER_ID;
+let userLoyalty = [];
 
-let apiClient;
-let existingUsers = [];
-let newUsers;
-let subs;
-let loyaltyInterval;
-
-async function init() {
-	apiClient = twitchRepo.getApiClient();
+function start(twitchId) {
+	userLoyalty.push({ id: twitchId, interval: createInterval(twitchId) });
 }
 
-function chatInterval() {
-	let followingUser;
-	loyaltyInterval = setInterval(async () => {
-		let chatPaginated = await apiClient.chat.getChattersPaginated(twitchUserId);
-		let currentPageUsers = await chatPaginated.getNext();
-		let currentUsersList = [];
+async function createInterval(twitchId) {
+	return setInterval(async () => {
+		let chatters = await getChatters(twitchId);
 
-		while (currentPageUsers.length > 0) {
-			currentUsersList = currentUsersList.concat(currentPageUsers);
-			currentPageUsers = await chatPaginated.getNext();
+		if (chatters.length == 0) {
+			return;
 		}
 
-		// creating list of userIds
 		let userIds = [];
-		for (let i = 0; i < currentUsersList.length; i++) {
-			userIds.push(currentUsersList[i].userId);
+		for (let i = 0; i < chatters.length; i++) {
+			userIds.push(chatters[i].userId);
 		}
 
-		// gets array of subs in chat
-		subs = await apiClient.subscriptions.getSubscriptionsForUsers(
-			twitchUserId,
-			userIds
-		);
+		let subs = await twitchService.getSubscriptionsForUsers(twitchId, userIds);
 
-		existingUsers = await LoyaltyPoint.find({
+		let existingUsers = await LoyaltyPoint.find({
+			twitchId: twitchId,
 			userId: { $in: userIds },
 		}).exec();
 
-		if (existingUsers.length > 0) {
-			newUsers = currentUsersList.filter(
-				(user) =>
-					!!!existingUsers.find((exUser) => exUser.userId == user.userId)
-			);
+		let newUsers = chatters.filter(
+			(user) => !existingUsers.find((exUser) => exUser.userId == user.userId)
+		);
 
-			await createUser(newUsers);
-		} else {
-			await createUser(currentUsersList);
-		}
+		createUser(twitchId, newUsers, subs);
 
-		let channelFollower;
 		for (let i = 0; i < existingUsers.length; i++) {
-			////
-			// not needed, existingUsers created using curreNtUsers IDs??
-			followingUser = currentUsersList.find(
-				(chatUser) => chatUser.userId == existingUsers[i].userId
-			);
-			////
-
 			let newFollowBonus = 0;
 			if (!existingUsers[i].follower) {
-				channelFollower = await apiClient.channels.getChannelFollowers(
-					twitchUserId,
-					followingUser.userId
+				let channelFollower = await twitchService.getChannelFollowers(
+					twitchId,
+					existingUsers[i].userId
 				);
 
 				if (channelFollower.data.length == 1) {
@@ -75,26 +48,25 @@ function chatInterval() {
 				}
 			}
 
-			modifier = getModifier(existingUsers[i]);
+			modifier = getModifier(existingUsers[i], subs);
 			existingUsers[i].points += 10 * modifier + newFollowBonus;
 
 			await existingUsers[i].save();
 		}
-	}, 300000);
+	}, 5 * 60 * 1000);
 }
 
-async function createUser(array) {
+async function createUser(twitchId, users, subs) {
 	// if broadcaster ignore, remove from array earlier
-	let newUser;
-	let newFollowBonus = 0;
+	let newUsers = [];
+	let newFollowBonus;
 	let following;
 	let modifier;
-	let channelFollower;
 
-	for (let i = 0; i < array.length; i++) {
-		channelFollower = await apiClient.channels.getChannelFollowers(
-			twitchUserId,
-			array[i].userId
+	for (let i = 0; i < users.length; i++) {
+		let channelFollower = await twitchService.getChannelFollowers(
+			twitchId,
+			users[i].userId
 		);
 
 		if (channelFollower.data.length == 1) {
@@ -105,28 +77,24 @@ async function createUser(array) {
 			newFollowBonus = 0;
 		}
 
-		modifier = getModifier(array[i]);
-		newUser = new LoyaltyPoint({
-			username: array[i].userName,
-			userId: array[i].userId,
+		modifier = getModifier(users[i], subs);
+
+		newUsers.push({
+			twitchId: twitchId,
+			userId: users[i].userId,
 			points: 10 * modifier + newFollowBonus,
 			follower: following,
 		});
-		await newUser.save();
 	}
+
+	await LoyaltyPoint.insertMany(newUsers);
 }
 
-function getModifier(user) {
+function getModifier(user, subs) {
 	let mod = 1;
 	let sub;
 
-	if (typeof user == "object") {
-		if (user.userId) {
-			sub = subs.find((sub) => sub.userId == user.userId);
-		} else {
-			sub = subs.find((sub) => sub.userId == user.id);
-		}
-	}
+	sub = subs.find((sub) => sub.userId == user.userId);
 
 	if (sub) {
 		mod = sub.tier == 1000 ? 1.2 : sub.tier == 2000 ? 1.4 : 2;
@@ -135,10 +103,26 @@ function getModifier(user) {
 	return mod;
 }
 
-function stopInterval() {
-	clearInterval(loyaltyInterval);
+async function getChatters(twitchId) {
+	let chatters = [];
+	let chattersPaginated = await twitchService.getChattersPaginated(twitchId);
+	let currentPageChatters = await chattersPaginated.getNext();
+
+	while (currentPageChatters.length > 0) {
+		chatters = chatters.concat(currentPageChatters);
+		currentPageChatters = await chattersPaginated.getNext();
+	}
+
+	return chatters;
 }
 
-exports.init = init;
-exports.start = chatInterval;
-exports.stop = stopInterval;
+function stop(twitchId) {
+	let index = userLoyalty.findIndex((el) => el.id == twitchId);
+	if (index != -1) {
+		clearInterval(userLoyalty[index].interval);
+		userLoyalty.splice(index, 1);
+	}
+}
+
+exports.start = start;
+exports.stop = stop;
