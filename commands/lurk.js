ae0fc8e1ee417158e10b9ef19cd40d5600273283
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-22 19:32:06 +0000

    Update lurk.js

--- commands/lurk.js
+++ commands/lurk.js
@@ -1,4 +1,3 @@
-const BaseCommand = require("../classes/base-command");
 const BotCommand = require("../classes/bot-command");
 const Helper = require("../classes/helper");
 
@@ -10,17 +9,32 @@ const users = {};
 let commandResponse = () => {
 	return {
 		response: async (config) => {
-			if (helper.isStreamer(config.userInfo)) return;
+			if (helper.isStreamer(config)) return;
 
+			/////
 			let userCommand = await CommandNew.findOne({
 				streamerId: config.channelId,
 				name: "lurk",
 			});
 
 			users[config.channelId] = { output: userCommand.output };
+			//////
 
-			let output = helper.getOutput(users, config.channelId, "isLurking");
-			output = output.replace("[displayName]", config.userInfo.displayName);
+			let output = helper.getOutput(users[config.channelId], "isLurking");
+
+			// check if matches regex here, if true continue, if false return output
+
+			map = getMap(config);
+			console.log(map);
+			// {
+			// 	displayName: config.displayName,
+			// 	channelId: config.channelId,
+			// 	channelName: config.channelName,
+			// }
+
+			// output = helper.processOutput(output, map);
+
+			output = output.replace("{displayName}", config.displayName);
 
 			return output;
 		},
@@ -38,5 +52,34 @@ let versions = [
 ];
 
 const lurk = new BotCommand(commandResponse, versions, users);
+
+function processOutput(output, map) {
+	const regex = /\{[^}]*\}/g;
+	const matches = output.match(regex);
+	if (!matches) return output;
+
+	const uniqueMatches = [...new Set(matches)];
+	// loop through unique matches and get replacement from map to update string
+
+	return output;
+}
+
+function getMap(config) {
+	const map = new Map([
+		["displayName", ""],
+		["channelId", ""],
+		["isBroadcaster", ""],
+	]);
+
+	for (const key of map.keys()) {
+		map.set(key, config[key]);
+	}
+
+	return map;
+}
+
+// 				displayName: config.displayName,
+// 				channelId: config.channelId,
+// 				channelName: config.channelName,
 
 exports.command = lurk;
