52c188d4d8c5c9f1cee61d6527265d80e71de83b
TheStarlessAbstract <david.foley@outlook.com>
2024-03-23 12:37:46 +0000

    Update commandnew.js

--- models/commandnew.js
+++ models/commandnew.js
@@ -5,18 +5,24 @@ let OutputSchema = new Schema({
 	message: { type: String, required: true },
 	active: { type: Boolean, required: true },
 });
+let VersionSchema = new Schema({
+	description: String,
+	active: Boolean,
+	usableBy: String, // maybe enum for users, vips, mods, streamer
+});
 
 let CommandNewSchema = new Schema({
 	streamerId: { type: String, required: true },
-	chatName: { type: String, required: true },
-	active: Boolean,
-	name: String,
-	text: String,
+	chatName: { type: String, required: true }, // custom command name
+	active: Boolean, //can be removed, is included in versions
+	name: String, // default command name
+	text: String, // only used for chat created commands || could be moved into output [default: text]
 	output: { type: Map, of: OutputSchema },
 	createdBy: String,
 	createdOn: Date,
 	lastEditedBy: String,
 	lastEditedOn: String,
+	versions: [VersionSchema],
 });
 
 CommandNewSchema.index({ streamerId: 1, chatName: 1 }, { unique: true });
