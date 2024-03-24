6d7d4048f56a40a130a06499e6c8409cc5748373
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-24 19:09:12 +0000

    Update lurk.js

--- commands/lurk.js
+++ commands/lurk.js
@@ -3,16 +3,19 @@ const Helper = require("../classes/helper");
 const CommandNew = require("../models/commandnew");
 const helper = new Helper();
 
-let commandResponse = async (config) => {
+const commandResponse = async (config) => {
 	if (helper.isStreamer(config)) return;
 
 	if (!command.getUser[config.channelId]) {
-		let userCommand = await CommandNew.findOne({
+		const userCommand = await CommandNew.findOne({
 			streamerId: config.channelId,
 			name: "lurk",
 		});
 		if (!userCommand) return;
-		command.addUser(config.channelId, { output: userCommand.output });
+		command.addUser(config.channelId, {
+			output: userCommand.output,
+			versions: userCommand.versions,
+		});
 	}
 
 	let isLurkingString = helper.getOutput(
@@ -20,8 +23,13 @@ let commandResponse = async (config) => {
 		"isLurking"
 	);
 
-	const configMap = getConfigMap(config);
-	isLurkingString = processOutputString(isLurkingString, configMap);
+	if (!isLurkingString) return;
+
+	const commandConfigMap = helper.getCommandConfigMap(config);
+	isLurkingString = helper.processOutputString(
+		isLurkingString,
+		commandConfigMap
+	);
 
 	return isLurkingString;
 };
@@ -37,43 +45,5 @@ let versions = [
 ];
 
 const command = new BotCommand(commandResponse, versions);
-
-function processOutputString(outputString, map) {
-	const regex = /\{[^}]*\}/g;
-	const keysInOutputString = outputString.match(regex);
-	if (!keysInOutputString) return outputString;
-
-	const uniqueKeysInOutputString = [...new Set(keysInOutputString)];
-	const cleanedArrayOfKeys = removeCurlyBracesFromStringArray(
-		uniqueKeysInOutputString
-	);
-
-	for (let i = 0; i < cleanedArrayOfKeys.length; i++) {
-		outputString = outputString.replaceAll(
-			`{${cleanedArrayOfKeys[i]}}`,
-			map.get(cleanedArrayOfKeys[i])
-		);
-	}
-
-	return outputString;
-}
-
-function removeCurlyBracesFromStringArray(stringArray) {
-	return stringArray.map((string) => string.substring(1, string.length - 1));
-}
-
-function getConfigMap(config) {
-	const map = new Map([
-		["displayName", ""],
-		["channelId", ""],
-		["isBroadcaster", ""],
-	]);
-
-	for (const key of map.keys()) {
-		map.set(key, config[key]);
-	}
-
-	return map;
-}
 
 module.exports = command;
