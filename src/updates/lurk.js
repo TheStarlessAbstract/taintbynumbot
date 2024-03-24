3e1cdb5a4fec50bfe1ea2a20d8123557d41b3239
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-24 19:09:22 +0000

    Create lurk.js

---
+++ src/updates/lurk.js
@@ -0,0 +1,42 @@
+const Command = require("../../models/command.js"); // swap import with below when doing copy back to Command collection
+const CommandNew = require("../../models/commandnew.js");
+
+let twitchId = 100612361;
+
+async function copyAndUpdate() {
+	let comm = lurkTemplate();
+	await comm.save();
+}
+
+function lurkTemplate() {
+	return new CommandNew({
+		streamerId: twitchId,
+		chatName: "lurk",
+		defaultName: "lurk",
+		createdBy: twitchId,
+		createdOn: new Date(),
+		output: new Map([
+			[
+				"isLurking",
+				{
+					message:
+						"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+					active: true,
+				},
+			],
+		]),
+		versions: new Map([
+			[
+				"noArguement",
+				{
+					description:
+						"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+					active: true,
+					minimumPermissionLevel: "users",
+				},
+			],
+		]),
+	});
+}
+
+module.exports = copyAndUpdate;
