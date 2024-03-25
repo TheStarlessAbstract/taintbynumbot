0046ec4cbf9fbd7ddd63fd3154579c6ccfcbe2ff
TheStarlessAbstract <david.foley@outlook.com>
2024-03-25 01:27:05 +0000

    Update drinkbitch.js

--- commands/drinkbitch.js
+++ commands/drinkbitch.js
@@ -12,83 +12,92 @@ let audioLinks;
 let cooldown = 5000;
 let cost = 500;
 
-let commandResponse = () => {
-	return {
-		response: async (config) => {
-			let result = [];
-			let currentTime = new Date();
+let commandResponse = async (config) => {
+	let currentTime = new Date();
 
-			if (
-				helper.isCooldownPassed(currentTime, drinkBitch.timer, cooldown) ||
-				helper.isStreamer(config.userInfo)
-			) {
-				drinkBitch.setTimer(currentTime);
-				let user = await LoyaltyPoint.findOne({
-					userId: config.userInfo.userId,
-				});
+	if (
+		!helper.isCooldownPassed(currentTime, command.timer, cooldown) &&
+		!helper.isStreamer(config)
+	)
+		return;
+	command.setTimer(currentTime);
 
-				if (user) {
-					let audioLink;
-					if (user.points > cost) {
-						audioLink = helper.getRandomisedAudioFileUrl(audioLinks);
+	let user;
 
-						if (!helper.isTest()) {
-							audio.play(audioLink);
-						}
+	if (!helper.isStreamer(config)) {
+		user = await LoyaltyPoint.findOne({
+			twitchId: config.channelId,
+			userId: config.userId,
+		}).exec();
+	}
 
-						user.points -= cost;
-						await user.save();
-						result.push("@TheStarlessAbstract drink, bitch!");
-					} else {
-						if (helper.getRandomBetweenInclusiveMax(1, 100) == 100) {
-							audioLink = helper.getRandomisedAudioFileUrl(audioLinks);
+	const commandConfigMap = helper.getCommandConfigMap(config);
 
-							if (!helper.isTest()) {
-								audio.play(audioLink);
-							}
+	if (!user && !helper.isStreamer(config)) {
+		return helper.getProcessedOutputString(
+			command.getChannel(config.channelId),
+			"userNotInDatabase",
+			commandConfigMap
+		);
+	}
 
-							result.push(
-								"@" +
-									config.userInfo.displayName +
-									" You lack the points to make Starless drink, but The Church of Latter-Day Taints takes pity on you. @TheStarlessAbstract drink, bitch!"
-							);
-						} else {
-							result.push(
-								"@" +
-									config.userInfo.displayName +
-									" You lack the points to make Starless drink, hang about stream if you have nothing better to do, and maybe you too can make Starless !drinkbitch"
-							);
-						}
-					}
-				} else {
-					result.push(
-						"@" +
-							config.userInfo.displayName +
-							" It doesn't look like you have been here before, hang around, enjoy the mods abusing Starless, and maybe you too in time can make Starless !drinkbitch"
-					);
-				}
-			}
+	let audioLink;
+	if (user.points >= cost || helper.isStreamer(config)) {
+		///////// new audio player file to get url and send to play
+		audioLink = helper.getRandomisedAudioFileUrl(audioLinks);
 
-			return result;
-		},
-	};
+		if (!helper.isTest()) {
+			audio.play(audioLink);
+		}
+		/////////
+		user.points -= cost;
+		await user.save();
+
+		return helper.getProcessedOutputString(
+			command.getChannel(config.channelId),
+			"validBalance",
+			commandConfigMap
+		);
+	}
+	if (helper.getRandomBetweenInclusiveMax(1, 7) == 7) {
+		////////////////
+		audioLink = helper.getRandomisedAudioFileUrl(audioLinks);
+
+		if (!helper.isTest()) {
+			audio.play(audioLink);
+		}
+		////////////
+
+		return helper.getProcessedOutputString(
+			command.getChannel(config.channelId),
+			"luckyShot",
+			commandConfigMap
+		);
+	}
+
+	return helper.getProcessedOutputString(
+		command.getChannel(config.channelId),
+		"invalidBalance",
+		commandConfigMap
+	);
 };
 
-let versions = [
-	{
-		description: "Makes Starless drink booze",
-		usage: "!drinkbitch",
-		usableBy: "users",
-		cost: "500 Tainty Points",
-		active: true,
-	},
-];
+// let versions = [
+// 	{
+// 		description: "Makes Starless drink booze",
+// 		usage: "!drinkbitch",
+// 		usableBy: "users",
+// 		cost: "500 Tainty Points",
+// 		active: true,
+// 	},
+// ];
 
-const drinkBitch = new TimerCommand(commandResponse, versions, cooldown);
+const command = new TimerCommand(commandResponse, cooldown);
 
-async function updateAudioLinks() {
-	audioLinks = await AudioLink.find({ command: "drinkbitch" });
-}
+// async function updateAudioLinks() {
+// 	audioLinks = await AudioLink.find({ command: "drinkbitch" });
+// }
 
-exports.command = drinkBitch;
-exports.updateAudioLinks = updateAudioLinks;
+// exports.updateAudioLinks = updateAudioLinks;
+
+module.exports = command;
