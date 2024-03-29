167cd0e10a38c0fda5f61401ada2de2b50139707
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-29 19:20:30 +0000

    Update helper.js

--- classes/helper.js
+++ classes/helper.js
@@ -18,10 +18,6 @@ class Helper {
 		return "";
 	}
 
-	isStreamer(config) {
-		return config.isBroadcaster;
-	}
-
 	isTest() {
 		return process.env.JEST_WORKER_ID != undefined;
 	}
@@ -158,64 +154,6 @@ class Helper {
 		}
 
 		return userIds;
-	}
-
-	removeFirstAndLastCharacterStringArray(arrayOfStrings) {
-		return arrayOfStrings.map((string) =>
-			string.substring(1, string.length - 1)
-		);
-	}
-
-	getCommandConfigMap(config) {
-		const map = new Map([
-			["displayName", ""],
-			["channelId", ""],
-			["isBroadcaster", ""],
-		]);
-
-		for (const key of map.keys()) {
-			map.set(key, config[key]);
-		}
-
-		return map;
-	}
-
-	getProcessedOutputString(channel, outputReference, configMap) {
-		let a = this.#getOutputString(channel, outputReference);
-		if (!a) return;
-
-		let b = this.#processOutputString(a, configMap);
-		return b;
-	}
-
-	#getOutputString(user, outputReference) {
-		let output = "";
-
-		if (!user.output.get(outputReference).active) return null;
-
-		output = user.output.get(outputReference).message;
-
-		return output;
-	}
-
-	#processOutputString(outputString, map) {
-		const regex = /\{[^}]*\}/g;
-		const keysInOutputString = outputString.match(regex);
-		if (!keysInOutputString) return outputString;
-
-		const uniqueKeysInOutputString = [...new Set(keysInOutputString)];
-		const cleanedArrayOfKeys = this.removeFirstAndLastCharacterStringArray(
-			uniqueKeysInOutputString
-		);
-
-		for (let i = 0; i < cleanedArrayOfKeys.length; i++) {
-			outputString = outputString.replaceAll(
-				`{${cleanedArrayOfKeys[i]}}`,
-				map.get(cleanedArrayOfKeys[i])
-			);
-		}
-
-		return outputString;
 	}
 }
 
