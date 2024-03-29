8c18fac1eb2f851525a61ee7dd992d5716b349b7
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-29 19:20:33 +0000

    Update timer-command.js

--- classes/timer-command.js
+++ classes/timer-command.js
@@ -1,4 +1,4 @@
-const BaseCommand = require("../classes/base-command");
+const BaseCommand = require("../src/classes/base-command");
 
 class TimerCommand extends BaseCommand {
 	constructor(getCommand, cooldown) {
