ac81481c4bf2dd716c3eda55b712ad29002d93dc
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-03-31 20:12:57 +0100

    Update commandnew.js

--- models/commandnew.js
+++ models/commandnew.js
@@ -1,6 +1,19 @@
 let mongoose = require("mongoose"),
 	Schema = mongoose.Schema;
 
+let UsableBySchema = new Schema({
+	broadcaster: Boolean,
+	mods: Boolean,
+	vips: Boolean,
+	artists: Boolean,
+	users: { type: Boolean, default: true },
+});
+
+let CooldownSchema = new Schema({
+	length: { type: Number, required: true },
+	lastUsed: Number,
+});
+
 let OutputSchema = new Schema({
 	message: { type: String, required: true },
 	active: { type: Boolean, required: true },
@@ -8,29 +21,25 @@ let OutputSchema = new Schema({
 let VersionSchema = new Schema({
 	description: { type: String, required: true }, // what is the purpose of this version of the command
 	active: { type: Boolean, required: true }, // can this command be used in chat
-	minimumPermissionLevel: {
-		type: String,
-		required: true,
-		enum: ["users", "artist", "vips", "mods", "streamer"], // maybe not needed, as would prevent custom
-		default: "streamer",
-	}, // minimum user level to use the command
+	usableBy: { type: UsableBySchema },
+	cooldown: { type: CooldownSchema },
+	cost: Number,
 });
 
 let CommandNewSchema = new Schema({
-	streamerId: { type: String, required: true },
+	channelId: { type: String, required: true },
 	chatName: { type: String, required: true }, // custom command name
-	defaultName: String, // default command name, change to defaultName
+	type: String, // type of command
 	text: String, // only used for chat created commands || could be moved into output [default: text]
-	cost: Number,
+	cost: Number, // maybe should be in versions
 	createdBy: String,
 	createdOn: Date,
 	lastEditedBy: String,
 	lastEditedOn: String,
 	output: { type: Map, of: OutputSchema },
-	versions: { type: Map, of: VersionSchema }, // "no arguement", "number argument", "string arguement"
+	versions: { type: Map, of: VersionSchema }, // "no argument", "number argument", "string argument"
 });
 
 CommandNewSchema.index({ streamerId: 1, chatName: 1 }, { unique: true });
-CommandNewSchema.index({ chatName: 1, defaultName: 1 }, { unique: true });
 
 module.exports = mongoose.model("CommandNew", CommandNewSchema);
