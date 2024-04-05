cfb73bbde014aaa0fad3bfd6d48510d466f07b10
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-05 18:17:36 +0100

    Update lurk.js

--- src/commands/lurk.js
+++ src/commands/lurk.js
@@ -1,26 +1,17 @@
 const BotCommand = require("../classes/bot-command");
-const CommandNew = require("../../models/commandnew");
 const {
 	getChatCommandConfigMap,
 	getProcessedOutputString,
-	isCooldownPassed,
-	configRoleStrings,
 } = require("../utils");
 
 const cooldown = 0;
 
 const commandResponse = async (config) => {
-	let channel = await checkChannel(config, "lurk");
-	if (!channel) return;
+	const channel = await command.checkChannel(config, "lurk");
+	if (!channel?.versions || !channel?.output) return;
 
-	let versionKey = getCommandVersionKey(config, channel);
-	if (!versionKey) return;
-
-	let commandRestriction = isCommandRestricted(
-		config,
-		channel.versions.get(versionKey)
-	);
-	if (commandRestriction) return;
+	const isCommandAvailable = await command.checkCommandStatus(config, channel);
+	if (!isCommandAvailable) return;
 
 	const chatCommandConfigMap = getChatCommandConfigMap(config);
 	if (!(chatCommandConfigMap instanceof Map)) return;
@@ -30,7 +21,6 @@ const commandResponse = async (config) => {
 		"isLurking",
 		chatCommandConfigMap
 	);
-
 	if (typeof output !== "string") return;
 
 	return output;
@@ -39,69 +29,3 @@ const commandResponse = async (config) => {
 const command = new BotCommand(commandResponse, cooldown);
 
 module.exports = command;
-
-function isCommandRestricted(config, version) {
-	const userAllowed = hasPermittedRoles(config, version.usableBy);
-	if (!userAllowed) return true;
-
-	const bypass = hasPermittedRoles(config, version.cooldown.bypassRoles);
-	if (bypass) return false;
-
-	return isCooldownPassed(
-		currentTime,
-		version.cooldown.lastUsed,
-		version.cooldown.length
-	);
-}
-
-function hasPermittedRoles(config, permittedRoles) {
-	const roleStrings = configRoleStrings(config);
-	const found = roleStrings.some((r) => permittedRoles.includes(r));
-
-	return found;
-}
-
-async function checkChannel(config, type) {
-	let channel = command.getChannel([config.channelId]);
-
-	if (!channel) {
-		const userCommand = await CommandNew.findOne({
-			channelId: config.channelId,
-			type: type,
-		});
-		if (!userCommand) return;
-
-		channel = {
-			output: userCommand.output,
-			versions: userCommand.versions,
-		};
-		command.addChannel(config.channelId, channel);
-	}
-
-	return channel;
-}
-
-function getCommandVersionKey(config, channel) {
-	let hasArgument = false;
-	let argument = config.argument;
-	if (argument) hasArgument = true;
-
-	let isNumber = false;
-	if (hasArgument) isNumber = isArgumentNumber();
-
-	const iterator = channel.versions.entries();
-
-	let pair;
-	for (let i = 0; i < channel.versions.size; i++) {
-		pair = iterator.next().value;
-		if (
-			!pair[1].active ||
-			(!pair[1].isArgumentOptional && hasArgument != pair[1].hasArgument) ||
-			isNumber != pair[1].isArgumentNumber
-		)
-			continue;
-
-		return pair[0];
-	}
-	return;
-}
