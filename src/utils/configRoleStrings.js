aaf3140945f80bdd0cbefbefb46b657c91954018
TheStarlessAbstract <david.foley@outlook.com>
2024-04-06 21:48:21 +0100

    Update configRoleStrings.js

--- src/utils/configRoleStrings.js
+++ src/utils/configRoleStrings.js
@@ -1,14 +1,15 @@
 const configRoleStrings = (config) => {
 	let array = [];
 
-	if (config.isBroadcaster) array.push("isBroadcaster");
-	if (config.isMod) array.push("isMod");
-	if (config.isSub) array.push("isSub");
-	if (config.isFounder) array.push("isFounder");
-	if (config.isArtist) array.push("isArtist");
-	if (config.isVip) array.push("isVip");
+	if (config.isBroadcaster) array.push("broadcaster");
+	if (config.isMod) array.push("mods");
+	if (config.isSub) array.push("subs");
+	if (config.isFounder) array.push("founders");
+	if (config.isArtist) array.push("artists");
+	if (config.isVip) array.push("vips");
 
-	if (array.length == 0) array.push("isUser");
+	if (array.length == 0) array.push("users");
+	console.log(array);
 
 	return array;
 };
