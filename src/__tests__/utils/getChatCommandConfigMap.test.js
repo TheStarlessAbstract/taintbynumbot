a94fc9bac170a50dc5b79b5b0a42810611ce8544
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-12 19:10:46 +0100

    Update getChatCommandConfigMap.test.js

--- src/__tests__/utils/getChatCommandConfigMap.test.js
+++ src/__tests__/utils/getChatCommandConfigMap.test.js
@@ -5,83 +5,107 @@ jest.mock("../../config", () => ({
 	chatCommandConfigMap: jest.fn(),
 }));
 
-describe("getChatCommandConfigMap", () => {
+describe("getChatCommandConfigMap()", () => {
 	let config;
-
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
-	describe("When chatCommandConfigMap is not an instance of Map", () => {
-		test("Result should be undefined", async () => {
-			//Assemble
-			chatCommandConfigMap.mockReturnValue(undefined);
 
-			config = {
-				channelId: "1",
-				displayName: "TheStarlessAbstract",
-				isBroadcaster: true,
-			};
+	test("should return a map object containing the chat command configuration when provided with a valid config object", () => {
+		// Assemble
+		const map = new Map([
+			["displayName", ""],
+			["channelId", ""],
+			["isBroadcaster", ""],
+		]);
+		chatCommandConfigMap.mockReturnValue(map);
 
-			//Act
-			let result = getChatCommandConfigMap(config);
+		const config = {
+			channelId: "1",
+			displayName: "TheStarlessAbstract",
+			isBroadcaster: "true",
+		};
 
-			//Assert
-			expect(result).toBeUndefined();
-		});
+		// Act
+		const result = getChatCommandConfigMap(config);
+
+		// Assert
+		expect(result).toEqual(
+			new Map([
+				["displayName", "TheStarlessAbstract"],
+				["channelId", "1"],
+				["isBroadcaster", "true"],
+			])
+		);
 	});
-	describe("When chatCommandConfigMap is an instance of Map", () => {
-		describe("And at least one key in chatCommandConfigMap does not match a property in config", () => {
-			test("Result should be undefined", async () => {
-				//Assemble
-				const map = new Map([
-					["a", ""],
-					["channelId", ""],
-					["isBroadcaster", ""],
-				]);
-				chatCommandConfigMap.mockReturnValue(map);
 
-				config = {
-					channelId: "1",
-					displayName: "TheStarlessAbstract",
-					isBroadcaster: true,
-				};
+	test("should return undefined when provided with an empty config object", () => {
+		// Assemble
+		const map = new Map([
+			["displayName", ""],
+			["channelId", ""],
+			["isBroadcaster", ""],
+		]);
+		chatCommandConfigMap.mockReturnValue(map);
 
-				//Act
-				let result = getChatCommandConfigMap(config);
+		const config = {};
 
-				//Assert
-				expect(result).toBeUndefined();
-			});
-		});
+		// Act
+		const result = getChatCommandConfigMap(config);
 
-		describe("And all keys in chatCommandConfigMap match properties in config", () => {
-			test("Result should be undefined", async () => {
-				//Assemble
-				const map = new Map([
-					["displayName", ""],
-					["channelId", ""],
-					["isBroadcaster", ""],
-				]);
-				chatCommandConfigMap.mockReturnValue(map);
+		// Assert
+		expect(result).toBeUndefined();
+	});
 
-				config = {
-					channelId: "1",
-					displayName: "TheStarlessAbstract",
-					isBroadcaster: "true",
-				};
+	test("should return undefined when provided with a null config object", () => {
+		// Assemble
+		const map = new Map([
+			["displayName", ""],
+			["channelId", ""],
+			["isBroadcaster", ""],
+		]);
+		chatCommandConfigMap.mockReturnValue(map);
 
-				//Act
-				let result = getChatCommandConfigMap(config);
+		const config = null;
 
-				//Assert
-				expect(result).toEqual(
-					new Map([
-						["displayName", "TheStarlessAbstract"],
-						["channelId", "1"],
-						["isBroadcaster", "true"],
-					])
-				);
-			});
-		});
+		// Act
+		const result = getChatCommandConfigMap(config);
+
+		// Assert
+		expect(result).toBeUndefined();
+	});
+
+	test("should return undefined when provided with a config object that is not an instance of Object", () => {
+		// Assemble
+		const map = new Map([
+			["displayName", ""],
+			["channelId", ""],
+			["isBroadcaster", ""],
+		]);
+		chatCommandConfigMap.mockReturnValue(map);
+
+		const config = "invalid";
+
+		// Act
+		const result = getChatCommandConfigMap(config);
+
+		// Assert
+		expect(result).toBeUndefined();
+	});
+
+	test("should update the chat command configuration map with values from the config object", () => {
+		// Assemble
+		const mockConfig = { key1: "value1", key2: "value2" };
+		const mockMap = new Map();
+		mockMap.set("key1", "");
+		mockMap.set("key2", "");
+		chatCommandConfigMap.mockReturnValue(mockMap);
+
+		// Act
+		const result = getChatCommandConfigMap(mockConfig);
+
+		// Assert
+		expect(result.get("key1")).toEqual("value1");
+		expect(result.get("key2")).toEqual("value2");
 	});
 });
