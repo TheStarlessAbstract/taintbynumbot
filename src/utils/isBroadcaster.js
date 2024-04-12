9dcf48d14d6db3de5b8a2b2b74814a9c74847e44
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-12 19:11:19 +0100

    Update isBroadcaster.js

--- src/utils/isBroadcaster.js
+++ src/utils/isBroadcaster.js
@@ -1,4 +1,11 @@
+/**
+ * Returns the value of the `isBroadcaster` property from the `config` object.
+ * @param {object} config - The configuration object that contains the `isBroadcaster` property.
+ * @returns {boolean} - The value of the `isBroadcaster` property.
+ */
 const isBroadcaster = (config) => {
+	if (typeof config?.isBroadcaster !== "boolean") return false;
+
 	return config.isBroadcaster;
 };
 
