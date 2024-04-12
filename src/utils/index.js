0ff20617c27a4f557efe6779554c6c93e1105bc6
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-12 19:11:17 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,15 @@
-const getProcessedOutputString = require("./getProcessedOutputString.js");
 const isBroadcaster = require("./isBroadcaster.js");
-const isCooldownPassed = require("./isCooldownPassed.js");
-const getChatCommandConfigMap = require("./getCommandConfigMap.js");
-const configRoleStrings = require("./configRoleStrings.js");
-// const confirmChannelProperties = require("./confirmChannelProperties.js");
 const isValueNumber = require("./isValueNumber.js");
+const configRoleStrings = require("./configRoleStrings.js");
+const processOutputString = require("./processOutputString.js");
+const getChatCommandConfigMap = require("./getChatCommandConfigMap.js");
+const getProcessedOutputString = require("./getProcessedOutputString.js");
 
 module.exports = {
+	isBroadcaster,
+	isValueNumber,
+	configRoleStrings,
+	processOutputString,
 	getChatCommandConfigMap,
 	getProcessedOutputString,
-	isBroadcaster,
-	isCooldownPassed,
-	configRoleStrings,
-	// confirmChannelProperties,
-	isValueNumber,
 };
