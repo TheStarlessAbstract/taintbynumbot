a5849709c9e31576c3bbca808ae37bdfdf3b4e04
TheStarlessAbstract <david.foley@outlook.com>
2024-04-13 00:29:51 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+const diceRoll = require("./diceRoll.js");
 const isBroadcaster = require("./isBroadcaster.js");
 const isValueNumber = require("./isValueNumber.js");
 const configRoleStrings = require("./configRoleStrings.js");
@@ -6,6 +7,7 @@ const getChatCommandConfigMap = require("./getChatCommandConfigMap.js");
 const getProcessedOutputString = require("./getProcessedOutputString.js");
 
 module.exports = {
+	diceRoll,
 	isBroadcaster,
 	isValueNumber,
 	configRoleStrings,
