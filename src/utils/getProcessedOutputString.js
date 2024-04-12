e16ae60a7658dfee7f1b87326de35b5ab4987503
TheStarlessAbstract <david.foley@outlook.com>
2024-04-13 00:29:49 +0100

    Update getProcessedOutputString.js

--- src/utils/getProcessedOutputString.js
+++ src/utils/getProcessedOutputString.js
@@ -5,7 +5,7 @@ const processOutputString = require("./processOutputString.js");
  * @param {object} channel - The channel to retrieve the output string from.
  * @param {string} outputReference - The reference to the specific output.
  * @param {Map} configMap - A map of configuration options.
- * @returns {string} The processed output string, or undefined if the parameter types are invalid or if the configMap is empty.
+ * @returns {string|undefined} The processed output string, or undefined if the parameter types are invalid or if the configMap is empty.
  */
 const getProcessedOutputString = (channel, outputReference, configMap) => {
 	if (
