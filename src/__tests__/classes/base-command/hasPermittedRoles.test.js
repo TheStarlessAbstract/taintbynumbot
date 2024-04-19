e0d3dd9b7ddef1a184e856fe4add48b6cc151a8c
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-19 19:52:30 +0100

    Update hasPermittedRoles.test.js

--- src/__tests__/classes/base-command/hasPermittedRoles.test.js
+++ src/__tests__/classes/base-command/hasPermittedRoles.test.js
@@ -1,66 +1,216 @@
-require("dotenv").config();
-const BaseCommand = require("../../../../classes/base-command.js");
-const { configRoleStrings } = require("../../../utils");
+const BaseCommand = require("../../../classes/base-command.js");
+const { getUserRolesAsStrings, isBroadcaster } = require("../../../utils");
 
 jest.mock("../../../utils", () => ({
-	configRoleStrings: jest.fn(),
+	getUserRolesAsStrings: jest.fn(),
 }));
 
-describe("isCommandRestricted()", () => {
-	let testCommand;
-
-	beforeEach(() => {
-		testCommand = new BaseCommand();
-	});
-
+describe("hasPermittedRoles()", () => {
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
 
-	describe("When configRoleStrings returns an empty array", () => {
-		test("Result should be false", async () => {
-			//Assemble
-			configRoleStrings.mockReturnValue([]);
+	test("should return true when user has at least one of the permitted roles", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {
+			isBroadcaster: true,
+			isFounder: false,
+			isArtist: false,
+			isMod: false,
+			isVip: false,
+			isSub: false,
+		};
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"subs",
+			"viewers",
+		];
 
-			config = {
-				isBroadcaster: false,
-				displayName: "design_by_rose",
-				channelId: 1,
-			};
-			permittedRoles = ["artists", "founders", "mods", "subs", "vips", "users"];
+		getUserRolesAsStrings.mockReturnValue(["broadcaster"]);
 
-			//Act
-			let result = await testCommand.hasPermittedRoles(config, permittedRoles);
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
 
-			//Assert
-			expect(result).toBeFalsy();
-		});
+		// Assert
+		expect(getUserRolesAsStrings).toHaveBeenCalled();
+		expect(result).toBe(true);
 	});
 
-	describe("When configRoleStrings returns an array roles", () => {
-		test("Result should be false", async () => {
-			//Assemble
-			configRoleStrings.mockReturnValue([
-				"artists",
-				"founders",
-				"mods",
-				"subs",
-				"vips",
-				"users",
-			]);
+	test("should return false when user does not have any of the permitted roles", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {
+			isBroadcaster: false,
+			isFounder: false,
+			isArtist: false,
+			isMod: false,
+			isVip: false,
+			isSub: false,
+		};
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"subs",
+		];
 
-			config = {
-				isBroadcaster: false,
-				displayName: "design_by_rose",
-				channelId: 1,
-			};
-			permittedRoles = ["artists", "founders", "mods", "subs", "vips", "users"];
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
 
-			//Act
-			let result = await testCommand.hasPermittedRoles(config, permittedRoles);
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
 
-			//Assert
-			expect(result).toBeTruthy();
-		});
+		// Assert
+		expect(getUserRolesAsStrings).toHaveBeenCalled();
+		expect(result).toBe(false);
+	});
+
+	test("should return true when user is missing some role properties, but has some permitted roles", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {
+			isBroadcaster: true,
+			isFounder: false,
+			isArtist: false,
+		};
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"subs",
+			"viewers",
+		];
+
+		getUserRolesAsStrings.mockReturnValue(["broadcaster"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).toHaveBeenCalled();
+		expect(result).toBe(true);
+	});
+
+	test("should return false when user is missing all role properties, and viewers is not a permitted role", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {};
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"subs",
+		];
+
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).toHaveBeenCalled();
+		expect(result).toBe(false);
+	});
+
+	test("should return true when user is missing all role properties, and viewers is not a permitted role", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {};
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"viewers",
+		];
+
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).toHaveBeenCalled();
+		expect(result).toBe(true);
+	});
+
+	test("should return undefined when userRoles is not an object", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = 1;
+		const permittedRoles = [
+			"broadcaster",
+			"founders",
+			"artists",
+			"mods",
+			"vips",
+			"subs",
+			"viewers",
+		];
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).not.toHaveBeenCalled();
+		expect(result).toBeUndefined();
+	});
+
+	test("should return undefined when permittedRoles is not an array", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {
+			isBroadcaster: true,
+			isFounder: false,
+			isArtist: false,
+			isMod: false,
+			isVip: false,
+			isSub: false,
+		};
+		const permittedRoles = {};
+
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).not.toHaveBeenCalled();
+		expect(result).toBeUndefined();
+	});
+
+	test("should return undefined when permittedRoles is an empty array", () => {
+		// Assemble
+		const testCommand = new BaseCommand();
+		const userRoles = {
+			isBroadcaster: true,
+			isFounder: false,
+			isArtist: false,
+			isMod: false,
+			isVip: false,
+			isSub: false,
+		};
+		const permittedRoles = [];
+
+		getUserRolesAsStrings.mockReturnValue(["viewers"]);
+
+		// Act
+		const result = testCommand.hasPermittedRoles(userRoles, permittedRoles);
+
+		// Assert
+		expect(getUserRolesAsStrings).not.toHaveBeenCalled();
+		expect(result).toBeUndefined();
 	});
 });
