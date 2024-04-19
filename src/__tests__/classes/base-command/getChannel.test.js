223de2e75ca680a29aa634548be920d2477c3add
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-19 19:52:26 +0100

    Update getChannel.test.js

--- src/__tests__/classes/base-command/getChannel.test.js
+++ src/__tests__/classes/base-command/getChannel.test.js
@@ -1,84 +1,74 @@
-const _ = require("lodash");
-const BaseCommand = require("../../../../classes/base-command.js");
+const BaseCommand = require("../../../classes/base-command.js");
 
 describe("getChannel()", () => {
-	let channelId = "1";
-	let testCommand;
+	test("should return the channel object when the channelId exists", () => {
+		// Assemble
+		const baseCommand = new BaseCommand();
+		const channelId = "123";
+		const channel = {
+			versions: new Map([
+				[
+					"noArgument",
+					{
+						description:
+							"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+						active: true,
+					},
+				],
+			]),
+			output: new Map([
+				[
+					"isLurking",
+					{
+						message:
+							"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+						active: true,
+					},
+				],
+			]),
+		};
+		baseCommand.addChannel(channelId, channel);
 
-	beforeEach(() => {
-		testCommand = new BaseCommand();
+		// Act
+		const result = baseCommand.getChannel(channelId);
+
+		// Assert
+		expect(result).toEqual(channel);
 	});
 
-	afterEach(() => {
-		jest.clearAllMocks();
+	test("should return null when the channelId does not exist", () => {
+		// Assemble
+		const baseCommand = new BaseCommand();
+		const channelId = "123";
+
+		// Act
+		const result = baseCommand.getChannel(channelId);
+
+		// Assert
+		expect(result).toBeNull();
 	});
 
-	describe("When channelId is not a property of Channels", () => {
-		test("Result should be false", async () => {
-			//Assemble
+	test("should return null when the channelId is not a string", () => {
+		// Assemble
+		const baseCommand = new BaseCommand();
+		const channelId = 123;
 
-			//Act
-			result = testCommand.getChannel(channelId);
+		// Act
+		const result = baseCommand.getChannel(channelId);
 
-			//Assert
-			expect(result).toBeFalsy();
-		});
+		// Assert
+		expect(result).toBeNull();
 	});
 
-	describe("When channelId is a property of Channels", () => {
-		test("Result should be channel object", async () => {
-			//Assemble
-			channel = {
-				output: new Map([
-					[
-						"isLurking",
-						{
-							message:
-								"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
-							active: true,
-						},
-					],
-				]),
-				versions: new Map([
-					[
-						"noArgument",
-						{
-							description:
-								"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
-							active: true,
-						},
-					],
-				]),
-			};
-			testCommand.addChannel(channelId, channel);
+	test("should return null when the channelId is an empty string", () => {
+		// Assemble
+		const baseCommand = new BaseCommand();
+		const channelId = "";
 
-			//Act
-			let result = testCommand.getChannel(channelId);
+		// Act
+		const result = baseCommand.getChannel(channelId);
 
-			//Assert
-			resultCheck = {
-				output: new Map([
-					[
-						"isLurking",
-						{
-							message:
-								"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
-							active: true,
-						},
-					],
-				]),
-				versions: new Map([
-					[
-						"noArgument",
-						{
-							description:
-								"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
-							active: true,
-						},
-					],
-				]),
-			};
-			expect(_.isEqual(result, resultCheck)).toBe(true);
-		});
+		// Assert
+		expect(result).toBeNull();
 	});
 });
