babdfe1552dd29d24aea83d739e7e12282fae8be
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-19 19:53:16 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 const chatCommandConfigMap = require("./chatCommandConfigMap.js");
+const commandTypes = require("./commandTypes.js");
 
 module.exports = {
 	chatCommandConfigMap,
+	commandTypes,
 };
