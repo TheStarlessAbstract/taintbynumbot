508ba87b34ae1814eb4a35ea1fbd8a1b7f8b37e9
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-19 19:53:08 +0100

    Update lurk.js

--- src/commands/lurk.js
+++ src/commands/lurk.js
@@ -4,9 +4,14 @@ const {
 	getProcessedOutputString,
 } = require("../utils");
 
+/**
+ * Processes the command response based on the provided configuration.
+ * @param {Object} config - The configuration object containing the version and output information of the command.
+ * @returns {Promise<string>} - The processed output string of the command.
+ */
 const commandResponse = async (config) => {
 	//{version: string, output: {map}}
-	const commandDetails = commandType.checkCommandCanRun(config);
+	const commandDetails = await commandType.checkCommandCanRun(config);
 	if (!commandDetails) return;
 
 	const chatCommandConfigMap = getChatCommandConfigMap(config);
