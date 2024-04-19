42856a0cb679628f221adb2b26e1b094526a19e3
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-19 19:52:48 +0100

    Update diceRoll.test.js

--- src/__tests__/utils/diceRoll.test.js
+++ src/__tests__/utils/diceRoll.test.js
@@ -1,19 +1,7 @@
 const { diceRoll } = require("../../utils");
 
 describe("diceRoll()", () => {
-	it("should generate a random number between 1 and the given maximum value", () => {
-		// Assemble
-		const max = 10;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeGreaterThanOrEqual(1);
-		expect(result).toBeLessThanOrEqual(max);
-	});
-
-	it("should return a number between 1 and the given maximum value", () => {
+	test("should return a number between 1 and the given maximum value", () => {
 		// Assemble
 		const max = 10;
 
@@ -26,7 +14,7 @@ describe("diceRoll()", () => {
 		expect(result).toBeLessThanOrEqual(max);
 	});
 
-	it("should return undefined if the maximum value is not a number", () => {
+	test("should return undefined if the maximum value is not a number", () => {
 		// Assemble
 		const max = "invalid";
 
@@ -37,7 +25,7 @@ describe("diceRoll()", () => {
 		expect(result).toBeUndefined();
 	});
 
-	it("should return undefined if the maximum value is less than or equal to 1", () => {
+	test("should return undefined if the maximum value is less than or equal to 1", () => {
 		// Assemble
 		const max = 0;
 
@@ -48,7 +36,7 @@ describe("diceRoll()", () => {
 		expect(result).toBeUndefined();
 	});
 
-	it("should generate a random number between 1 and 1000", () => {
+	test("should generate a random number between 1 and 1000", () => {
 		// Assemble
 		const max = 1000;
 
@@ -60,7 +48,7 @@ describe("diceRoll()", () => {
 		expect(result).toBeLessThanOrEqual(1000);
 	});
 
-	it("should generate a random number between 1 and 2", () => {
+	test("should generate a random number between 1 and 2", () => {
 		// Assemble
 		const max = 2;
 
@@ -72,54 +60,18 @@ describe("diceRoll()", () => {
 		expect(result).toBeLessThanOrEqual(2);
 	});
 
-	it("should generate a random number between 1 and the given maximum value", () => {
-		// Assemble
-		const max = 10;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeGreaterThanOrEqual(1);
-		expect(result).toBeLessThanOrEqual(max);
-	});
-
-	it("should return undefined when the maximum value is -Infinity", () => {
-		// Assemble
-		const max = -Infinity;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeUndefined();
-	});
-
-	it("should return a random number between 1 and Infinity", () => {
-		// Assemble
-		const max = Infinity;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeGreaterThanOrEqual(1);
-		expect(result).toBeLessThanOrEqual(max);
-	});
-
-	it("should generate a random number between 1 and Number.MAX_VALUE", () => {
+	test("should generate a random number between 1 and Number.MAX_VALUE", () => {
 		// Assemble
 		const max = Number.MAX_VALUE;
 
 		// Act
 		const result = diceRoll(max);
-
 		// Assert
 		expect(result).toBeGreaterThanOrEqual(1);
 		expect(result).toBeLessThanOrEqual(max);
 	});
 
-	it("should generate a random number between 1 and Number.MAX_SAFE_INTEGER when the maximum value is Number.MAX_SAFE_INTEGER", () => {
+	test("should generate a random number between 1 and Number.MAX_SAFE_INTEGER when the maximum value is Number.MAX_SAFE_INTEGER", () => {
 		// Assemble
 
 		// Act
@@ -130,19 +82,7 @@ describe("diceRoll()", () => {
 		expect(res).toBeLessThanOrEqual(Number.MAX_SAFE_INTEGER);
 	});
 
-	it("should generate a random number between 1 and Number.MIN_SAFE_INTEGER + 1", () => {
-		// Assemble
-		const max = Number.MIN_SAFE_INTEGER + 1;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeGreaterThanOrEqual(1);
-		expect(result).toBeLessThanOrEqual(max);
-	});
-
-	it("should generate a random number between 1 and Number.MIN_VALUE + 1", () => {
+	test("should generate a random number between 1 and Number.MIN_VALUE + 1", () => {
 		// Assemble
 		const max = Number.MIN_VALUE + 1;
 
@@ -150,11 +90,10 @@ describe("diceRoll()", () => {
 		const result = diceRoll(max);
 
 		// Assert
-		expect(result).toBeGreaterThanOrEqual(1);
-		expect(result).toBeLessThanOrEqual(max);
+		expect(result).toBeUndefined();
 	});
 
-	it("should return undefined when the maximum value is equal to Number.MIN_SAFE_INTEGER", () => {
+	test("should return undefined when the maximum value is equal to Number.MIN_SAFE_INTEGER", () => {
 		// Assemble
 		const max = Number.MIN_SAFE_INTEGER;
 
@@ -165,42 +104,9 @@ describe("diceRoll()", () => {
 		expect(result).toBeUndefined();
 	});
 
-	it("should return undefined when the maximum value is equal to Number.MAX_SAFE_INTEGER + 1", () => {
-		// Assemble
-		const max = Number.MAX_SAFE_INTEGER + 1;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeUndefined();
-	});
-
-	it("should return undefined when the maximum value is equal to NaN", () => {
+	test("should return undefined when the maximum value is equal to NaN", () => {
 		// Assemble
 		const max = NaN;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeUndefined();
-	});
-
-	it("should return undefined when the maximum value is equal to Number.POSITIVE_INFINITY", () => {
-		// Assemble
-		const max = Number.POSITIVE_INFINITY;
-
-		// Act
-		const result = diceRoll(max);
-
-		// Assert
-		expect(result).toBeUndefined();
-	});
-
-	it("should return undefined when the maximum value is equal to Number.NEGATIVE_INFINITY", () => {
-		// Assemble
-		const max = Number.NEGATIVE_INFINITY;
 
 		// Act
 		const result = diceRoll(max);
