346e50c062743ac3b28191bb7c329bd7db8f0bef
TheStarlessAbstract <david.foley@outlook.com>
2024-04-22 20:07:36 +0100

    Update textCommands.js

--- src/updates/textCommands.js
+++ src/updates/textCommands.js
@@ -1,10 +1,13 @@
-const Helper = require("../../classes/helper.js");
-
 const Command = require("../../models/command.js");
 const CommandNew = require("../models/commandnew.js");
 const User = require("../../models/user.js");
-
+const Helper = require("../../classes/helper.js");
 const helper = new Helper();
+
+async function copyAndUpdate() {
+	let comm = template();
+	await comm.save();
+}
 
 async function copyAndUpdate() {
 	let users = await User.find({ role: { $ne: "bot" } }, "twitchId").exec();
@@ -20,21 +23,46 @@ async function copyAndUpdate() {
 			list.push({
 				channelId: activeCommands[j].streamerId,
 				chatName: activeCommands[j].name,
-				text: activeCommands[j].text,
-				createdBy: activeCommands[j].createdBy, // convert to twitchId
+				type: "text",
+				createdBy: activeCommands[j].createdBy,
 				createdOn: activeCommands[j].createdOn,
+				output: new Map([
+					[
+						"text",
+						{
+							message: activeCommands[j].text,
+							active: true,
+						},
+					],
+				]),
 				versions: new Map([
 					[
 						"noArgument",
 						{
+							isArgumentOptional: false,
+							hasArgument: false,
+							isArgumentNumber: false,
 							description: activeCommands[j].text,
 							active: true,
-							usableBy: {
-								broadcaster: false,
-								mods: true,
-								vips: true,
-								artists: true,
-								users: true,
+							usableBy: [
+								"broadcaster",
+								"artists",
+								"founders",
+								"mods",
+								"subs",
+								"vips",
+								"viewers",
+							],
+							cooldown: {
+								length: 0,
+								lastUsed: new Date(),
+								bypassRoles: ["broadcaster"],
+							},
+							cost: { active: false, points: 0, bypassRoles: ["broadcaster"] },
+							hasAudioClip: false,
+							luck: {
+								active: false,
+								odds: 100000,
 							},
 						},
 					],
