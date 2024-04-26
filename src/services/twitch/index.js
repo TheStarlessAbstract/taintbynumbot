a54fa970abaeb00389a1ae0dd6a46af157301c4b
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:47 +0100

    Create index.js

---
+++ src/services/twitch/index.js
@@ -0,0 +1,7 @@
+const channels = require("./channels");
+const users = require("./users");
+const chat = require("./chat");
+const streams = require("./streams");
+const search = require("./search");
+
+module.exports = { channels, users, chat, streams, search };
