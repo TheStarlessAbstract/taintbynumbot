20fe468f82db6cc83ebb94ac56c1dfe5fccd8fe3
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:36 +0100

    Create getChannelInfoById.js

---
+++ src/services/twitch/channels/getChannelInfoById.js
@@ -0,0 +1,17 @@
+const twitchRepo = require("../../../../repos/twitch.js");
+
+const getChannelInfoById = async (id) => {
+	const apiClient = twitchRepo.getApiClient();
+	let channel;
+
+	try {
+		channel = await apiClient.channels.getChannelInfoById(id);
+	} catch (err) {
+		console.error(err);
+		channel = "";
+	}
+
+	return channel;
+};
+
+module.exports = getChannelInfoById;
