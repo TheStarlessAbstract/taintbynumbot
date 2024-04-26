923b9a198a2eb5dc650638138a4e50deb175b070
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:36:13 +0100

    Create index.js

---
+++ src/updates/commands/index.js
@@ -0,0 +1,29 @@
+const db = require("../../../bot-mongoose.js");
+const textcommands = require("./textCommands.js");
+const lurk = require("./lurk.js");
+const drinkBitch = require("./drinkbitch.js");
+const buhhs = require("./buhhs.js");
+const followage = require("./followage.js");
+const shoutout = require("./shoutout.js");
+const game = require("./game.js");
+
+async function init() {
+	const dbStatus = await db.getReadyState();
+	if (dbStatus === "Mongoose disconnected") {
+		await db.connectToMongoDB();
+	}
+
+	await textcommands();
+	await lurk();
+	await drinkBitch();
+	await buhhs();
+	await followage();
+	await shoutout();
+	await game();
+
+	if (dbStatus === "Mongoose disconnected") {
+		await db.disconnectFromMongoDB();
+	}
+}
+
+module.exports = init;
