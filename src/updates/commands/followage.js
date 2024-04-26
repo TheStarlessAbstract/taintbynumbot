bfd0154fe0ec3d662e171cc5c93624770e5a2a04
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:36:07 +0100

    Create followage.js

---
+++ src/updates/commands/followage.js
@@ -0,0 +1,56 @@
+// const Command = require("../../models/command.js");
+const Command = require("../../models/commandnew.js");
+
+let twitchId = "100612361";
+
+async function copyAndUpdate() {
+	let comm = template();
+	await comm.save();
+}
+
+function template() {
+	return new Command({
+		channelId: twitchId,
+		chatName: "followage",
+		type: "followage",
+		createdBy: twitchId,
+		createdOn: new Date(),
+		output: new Map([
+			[
+				"following",
+				{
+					message:
+						"@{displayName} has been staring into the Abstract abyss for {followLength}",
+					active: true,
+				},
+			],
+			[
+				"notFollowing",
+				{
+					message:
+						"@{displayName} hit that follow button, otherwise this command is doing a whole lot of nothing for you",
+					active: true,
+				},
+			],
+		]),
+		versions: new Map([
+			[
+				"noArgument",
+				{
+					isArgumentOptional: false,
+					hasArgument: false,
+					isArgumentNumber: false,
+					description:
+						"How long has it been since you last unfollowed, and then refollowed",
+					active: true,
+					usableBy: ["artists", "founders", "mods", "subs", "vips", "viewers"],
+					cooldown: {
+						length: 0,
+					},
+				},
+			],
+		]),
+	});
+}
+
+module.exports = copyAndUpdate;
