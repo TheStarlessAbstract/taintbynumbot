5db532367532c57312db99619c33eaef3dd7cfba
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:23 +0100

    Update commandTypes.js

--- src/config/commandTypes.js
+++ src/config/commandTypes.js
@@ -2,6 +2,9 @@ const commandTypes = () => {
 	return {
 		hydrate: require("../commands/hydrate"),
 		text: require("../commands/text"),
+		followage: require("../commands/followage"),
+		shoutout: require("../commands/shoutout"),
+		game: require("../commands/game"),
 	};
 };
 
