fa480b8b247aa92a9b592bded34d50df88455baa
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:36:15 +0100

    Create lurk.js

---
+++ src/updates/commands/lurk.js
@@ -0,0 +1,46 @@
+// const Command = require("../../models/command.js");
+const Command = require("../../models/commandnew.js");
+
+let twitchId = "100612361";
+
+async function copyAndUpdate() {
+	let comm = template();
+	await comm.save();
+}
+
+function template() {
+	return new Command({
+		channelId: twitchId,
+		chatName: "lurk",
+		type: "text",
+		createdBy: twitchId,
+		createdOn: new Date(),
+		output: new Map([
+			[
+				"text",
+				{
+					message:
+						"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+					active: true,
+				},
+			],
+		]),
+		versions: new Map([
+			[
+				"noArgument",
+				{
+					isArgumentOptional: false,
+					hasArgument: false,
+					isArgumentNumber: false,
+					description:
+						"@{displayName} finds a comfortable spot behind the bushes to perv on the stream",
+					active: true,
+					usableBy: ["artists", "founders", "mods", "subs", "vips", "viewers"],
+					cooldown: { length: 0 },
+				},
+			],
+		]),
+	});
+}
+
+module.exports = copyAndUpdate;
