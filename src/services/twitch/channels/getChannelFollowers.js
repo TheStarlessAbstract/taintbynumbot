b239caa23230523ad91b65b1ee5ee21d2d9aca59
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:33 +0100

    Create getChannelFollowers.js

---
+++ src/services/twitch/channels/getChannelFollowers.js
@@ -0,0 +1,21 @@
+const twitchRepo = require("../../../../repos/twitch.js");
+
+const getChannelFollowers = async (broadcaster, user, pagination) => {
+	const apiClient = twitchRepo.getApiClient();
+	let followers;
+
+	try {
+		followers = await apiClient.channels.getChannelFollowers(
+			broadcaster,
+			user,
+			pagination
+		);
+	} catch (err) {
+		console.error(err);
+		chatters = "";
+	}
+
+	return followers;
+};
+
+module.exports = getChannelFollowers;
