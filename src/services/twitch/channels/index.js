ddb1cbf61fb3b60588d582e2dcf4e09777752ccb
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:38 +0100

    Create index.js

---
+++ src/services/twitch/channels/index.js
@@ -0,0 +1,5 @@
+const getChannelFollowers = require("./getChannelFollowers.js");
+const getChannelInfoById = require("./getChannelInfoById.js");
+const updateChannelInfo = require("./updateChannelInfo.js");
+
+module.exports = { getChannelFollowers, getChannelInfoById, updateChannelInfo };
