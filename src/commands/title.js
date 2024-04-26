39a6030d94a4ec0218c945785b0ef4bc4b79abd9
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-26 19:29:59 +0100

    Create title.js

---
+++ src/commands/title.js
@@ -0,0 +1,94 @@
+const TimerCommand = require("../classes/timer-command");
+const Helper = require("../classes/helper");
+
+const twitchRepo = require("./../repos/twitch");
+
+let twitchId = process.env.TWITCH_USER_ID;
+
+const helper = new Helper();
+
+let cooldown = 10000;
+let currentTime = new Date();
+
+let commandResponse = () => {
+	return {
+		response: async (config) => {
+			let result = [];
+			currentTime = new Date();
+
+			if (
+				helper.isCooldownPassed(
+					currentTime,
+					title.getTimer(),
+					title.getCooldown()
+				)
+			) {
+				title.setTimer(currentTime);
+				let apiClient;
+				let channel;
+
+				if (
+					helper.isVersionActive(versions, 0) &&
+					!helper.isValuePresentAndString(config.argument)
+				) {
+					apiClient = twitchRepo.getApiClient();
+					channel = await apiClient.channels.getChannelInfoById(twitchId);
+
+					if (channel == null) {
+						result.push(
+							"Twitch says no, and Starless should really sort this out some time after stream"
+						);
+						return result;
+					}
+
+					result.push("The current title is: " + channel.title);
+				} else if (
+					helper.isVersionActive(versions, 1) &&
+					helper.isValuePresentAndString(config.argument) &&
+					helper.isValidModeratorOrStreamer(config.userInfo)
+				) {
+					apiClient = twitchRepo.getApiClient();
+
+					try {
+						await apiClient.channels.updateChannelInfo(twitchId, {
+							title: config.argument,
+						});
+					} catch (e) {
+						if (e.body.includes("banned words")) {
+							result.push(
+								"Twitch says you have used a no-no word - Title not updated"
+							);
+						} else {
+							result.push(
+								"Twitch has not updated the title for reasons - Try again later"
+							);
+						}
+						return result;
+					}
+
+					result.push("Title has been set to " + config.argument);
+				}
+			}
+			return result;
+		},
+	};
+};
+
+let versions = [
+	{
+		description: "Gets current title of the stream",
+		usage: "!title",
+		usableBy: "users",
+		active: true,
+	},
+	{
+		description: "Updates the current title of the stream",
+		usage: "!title My mistakes bring all the bots to the yard",
+		usableBy: "mods",
+		active: true,
+	},
+];
+
+const title = new TimerCommand(commandResponse, versions, cooldown);
+
+exports.command = title;
