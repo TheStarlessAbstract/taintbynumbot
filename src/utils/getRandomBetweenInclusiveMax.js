227d19cce8a4e8f60dc48d2d76788ea8e88efb61
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-04-26 18:36:23 +0100

    Create getRandomBetweenInclusiveMax.js

---
+++ src/utils/getRandomBetweenInclusiveMax.js
@@ -0,0 +1,16 @@
+const isValueNumber = require("./isValueNumber.js");
+
+/**
+ * Generates a random integer between a minimum and maximum value, inclusive.
+ *
+ * @param {number} min - The minimum value for the random number range.
+ * @param {number} max - The maximum value for the random number range.
+ * @returns {number|undefined} - Random integer between `min` and `max`, inclusive.
+ */
+
+const getRandomBetweenInclusiveMax = (min, max) => {
+	if (!isValueNumber(min) || !isValueNumber(max) || !(min < max)) return;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports = getRandomBetweenInclusiveMax;
