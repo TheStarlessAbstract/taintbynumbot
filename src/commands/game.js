53ded589d6fabed9511525df17db5547ed285b0f
TheStarlessAbstract <david.foley@outlook.com>
2024-04-27 00:35:15 +0100

    Update game.js

--- src/commands/game.js
+++ src/commands/game.js
@@ -1,138 +1,102 @@
-const TimerCommand = require("../classes/timer-command");
-const Helper = require("../classes/helper");
+const BaseCommand = require("../classes/base-command");
+const { updateChannelInfo } = require("../services/twitch/channels");
+const { getStreamByUserId } = require("../services/twitch/streams");
+const { searchCategories } = require("../services/twitch/search");
+const { getProcessedOutputString } = require("../utils");
 
-const twitchRepo = require("./../repos/twitch");
+const commandResponse = async (config) => {
+	if (
+		config.versionKey !== "noArgument" &&
+		config.versionKey !== "stringArgument"
+	)
+		return;
 
-const helper = new Helper();
+	let output;
+	let outputType;
 
-let cooldown = 10000;
-let currentTime = new Date();
+	if (config.versionKey == "noArgument") {
+		let stream = await getStreamByUserId(config.channelId);
+		if (!stream) {
+			outputType = "noStream";
+		} else {
+			outputType = "streamIsLive";
+		}
+	}
+	if (config.versionKey == "stringArgument") {
+		const game = await searchCategories(config.argument); // find game
+		if (!game) {
+			outputType = "gameNotFound";
+		} else {
+			outputType = "gameFound";
+			await updateChannelInfo();
+		}
+	}
 
-let commandResponse = () => {
-	return {
-		response: async (config) => {
-			let result = [];
-			let twitchId = config.channelId;
-			currentTime = new Date();
+	output = getProcessedOutputString(
+		config.output.get(outputType),
+		config.configMap
+	);
+	if (!output) return;
 
-			if (
-				helper.isCooldownPassed(
-					currentTime,
-					game.getTimer(),
-					game.getCooldown()
-				)
-			) {
-				game.setTimer(currentTime);
-				let apiClient;
-				let channel;
-
-				if (
-					helper.isVersionActive(versions, 0) &&
-					!helper.isValuePresentAndString(config.argument)
-				) {
-					apiClient = twitchRepo.getApiClient();
-					channel = await apiClient.channels.getChannelInfoById(twitchId);
-
-					if (channel == null) {
-						result.push(
-							"Twitch says no, and Starless should really sort this out some time after stream"
-						);
-						return result;
-					}
-
-					result.push(
-						"@" +
-							config.userInfo.displayName +
-							" -> " +
-							channel.displayName +
-							" is playing " +
-							channel.gameName
-					);
-				} else if (
-					helper.isValuePresentAndString(config.argument) &&
-					config.argument.length > 3 &&
-					helper.isVersionActive(versions, 1) &&
-					helper.isValidModeratorOrStreamer(config.userInfo)
-				) {
-					apiClient = twitchRepo.getApiClient();
-
-					let gamesPaginated = await apiClient.search.searchCategoriesPaginated(
-						config.argument
-					);
-
-					let currentPageGames = await gamesPaginated.getNext();
-					let pages = 0;
-
-					let gameId;
-					while (currentPageGames.length > 0 && pages < 3) {
-						for (let i = 0; i < currentPageGames.length; i++) {
-							if (
-								helper.startsWithCaseInsensitive(
-									currentPageGames[i].name,
-									config.argument
-								)
-							) {
-								gameId = currentPageGames[i].id;
-								break;
-							}
-						}
-
-						if (gameId == undefined) {
-							currentPageGames = await gamesPaginated.getNext();
-							pages++;
-						} else {
-							break;
-						}
-					}
-
-					if (gameId == undefined) {
-						result.push(
-							"@" + config.userInfo.displayName + " no game found by that name."
-						);
-						return result;
-					}
-
-					try {
-						await apiClient.channels.updateChannelInfo(twitchId, {
-							gameId: gameId,
-						});
-					} catch (e) {
-						result.push(
-							"Twitch has not updated the game for reasons - Try again later"
-						);
-						return result;
-					}
-
-					channel = await apiClient.channels.getChannelInfoById(twitchId);
-
-					result.push(
-						"@" +
-							config.userInfo.displayName +
-							" -> The stream game has been updated to: " +
-							channel.gameName
-					);
-				}
-			}
-			return result;
-		},
-	};
+	return output;
 };
 
-let versions = [
-	{
-		description: "Gets current game category for the stream",
-		usage: "!game",
-		usableBy: "users",
-		active: true,
-	},
-	{
-		description: "Sets the currenty game category for the stream",
-		usage: "!game Cyberpunk 2077",
-		usableBy: "mods",
-		active: true,
-	},
-];
+const command = new BaseCommand(commandResponse);
 
-const game = new TimerCommand(commandResponse, versions, cooldown);
+module.exports = command;
 
-exports.command = game;
+////////////////
+
+let gamesPaginated = await apiClient.search.searchCategoriesPaginated(
+	config.argument
+);
+
+let currentPageGames = await gamesPaginated.getNext();
+let pages = 0;
+
+let gameId;
+while (currentPageGames.length > 0 && pages < 3) {
+	for (let i = 0; i < currentPageGames.length; i++) {
+		if (
+			helper.startsWithCaseInsensitive(
+				currentPageGames[i].name,
+				config.argument
+			)
+		) {
+			gameId = currentPageGames[i].id;
+			break;
+		}
+	}
+
+	if (gameId == undefined) {
+		currentPageGames = await gamesPaginated.getNext();
+		pages++;
+	} else {
+		break;
+	}
+}
+
+if (gameId == undefined) {
+	result.push(
+		"@" + config.userInfo.displayName + " no game found by that name."
+	);
+	return result;
+}
+
+try {
+	await apiClient.channels.updateChannelInfo(twitchId, {
+		gameId: gameId,
+	});
+} catch (e) {
+	result.push("Twitch has not updated the game for reasons - Try again later");
+	return result;
+}
+
+channel = await apiClient.channels.getChannelInfoById(twitchId);
+
+result.push(
+	"@" +
+		config.userInfo.displayName +
+		" -> The stream game has been updated to: " +
+		channel.gameName
+);
