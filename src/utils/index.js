e0757170d285b71c437c38120235ce7533a7357a
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-03 18:27:03 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,22 +2,22 @@ const isBroadcaster = require("./isBroadcaster.js");
 const isValueNumber = require("./isValueNumber.js");
 const isNonEmptyMap = require("./isNonEmptyMap.js");
 const isNonEmptyString = require("./isNonEmptyString.js");
-const getUserRolesAsStrings = require("./getUserRolesAsStrings.js");
 const processOutputString = require("./processOutputString.js");
+const getCommandAction = require("./getCommandAction.js");
+const getUserRolesAsStrings = require("./getUserRolesAsStrings.js");
 const getChatCommandConfigMap = require("./getChatCommandConfigMap.js");
-const getProcessedOutputString = require("./getProcessedOutputString.js");
-const getRandomBetweenInclusiveMax = require("./getRandomBetweenInclusiveMax.js");
 const startsWithCaseInsensitive = require("./startsWithCaseInsensitive.js");
+const getRandomBetweenInclusiveMax = require("./getRandomBetweenInclusiveMax.js");
 
 module.exports = {
 	isBroadcaster,
 	isValueNumber,
 	isNonEmptyMap,
 	isNonEmptyString,
-	getUserRolesAsStrings,
 	processOutputString,
+	getCommandAction,
+	getUserRolesAsStrings,
 	getChatCommandConfigMap,
-	getProcessedOutputString,
-	getRandomBetweenInclusiveMax,
 	startsWithCaseInsensitive,
+	getRandomBetweenInclusiveMax,
 };
