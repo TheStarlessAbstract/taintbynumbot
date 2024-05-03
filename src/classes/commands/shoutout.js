9c5103f3c155747296616b5d72b2e4a9106b83ef
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-03 18:26:47 +0100

    Create shoutout.js

---
+++ src/classes/commands/shoutout.js
@@ -0,0 +1,49 @@
+const BaseCommand = require("./baseCommand.js");
+const { getUserByName } = require("../../services/twitch/users");
+const { getChannelInfoById } = require("../../services/twitch/channels");
+const { shoutoutUser } = require("../../services/twitch/chat");
+
+async function action(config) {
+	if (config.versionKey !== "stringArgument") return;
+	let outputType;
+	if (!config?.permitted) {
+		outputType = "modsOnly";
+	}
+
+	const user = await getUserByName(config.username);
+	if (!outputType && !user) {
+		outputType = "notFound";
+	}
+	let channel;
+	if (!outputType) {
+		channel = await getChannelInfoById(user.id);
+	}
+
+	if (!outputType && (!channel || !channel.gameName)) {
+		outputType = "shoutoutNoStreams";
+	}
+
+	if (!outputType) {
+		outputType = "shoutoutAndStreams";
+	}
+
+	const output = this.getProcessedOutputString(
+		this.getOutput(outputType),
+		config.configMap
+	);
+
+	if (user) {
+		await shoutoutUser(config.channelId, user.id);
+	}
+	return output;
+}
+
+class Shoutout extends BaseCommand {
+	constructor(channelId, name, { type, output, versions }) {
+		super(channelId, name, { type, output, versions });
+	}
+
+	action = action.bind(this);
+}
+
+module.exports = Shoutout;
