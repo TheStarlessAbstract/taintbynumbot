83fb25114ee214ab9c67f9556d7b833e8dbade6b
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-03 18:26:55 +0100

    Update findOne.js

--- src/queries/commands/findOne.js
+++ src/queries/commands/findOne.js
@@ -1,7 +1,7 @@
 const Command = require("../../models/commandnew.js");
 
 async function findOne(query, projection, options) {
-	return Command.findOne(query, projection, options).exec();
+	return await Command.findOne(query, projection, options).exec();
 }
 
 module.exports = { findOne };
