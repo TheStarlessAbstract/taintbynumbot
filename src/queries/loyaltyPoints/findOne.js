76a328567596538b75a24595b4d45673f2a40081
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-04 20:23:18 +0100

    Create findOne.js

---
+++ src/queries/loyaltyPoints/findOne.js
@@ -0,0 +1,7 @@
+const LoyaltyPoints = require("../../models/loyaltypointnew.js");
+
+async function findOne(query, projection, options) {
+	return LoyaltyPoints.findOne(query, projection, options).exec();
+}
+
+module.exports = { findOne };
