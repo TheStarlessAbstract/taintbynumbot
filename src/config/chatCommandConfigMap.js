b9ece067cbfe1f9a174657df85e4a59563c6e81c
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-04 20:23:11 +0100

    Update chatCommandConfigMap.js

--- src/config/chatCommandConfigMap.js
+++ src/config/chatCommandConfigMap.js
@@ -1,15 +1,12 @@
-/**
- * Returns a new Map object with default configuration properties.
- * @returns {Map} The Map object with default configuration properties.
- */
 const chatCommandConfigMap = () => {
-	// Create a new Map object and initialize it with default configuration properties
 	return new Map([
+		["channelId", ""],
 		["channelName", ""],
 		["displayName", ""],
 		["gameName", ""],
-		["channelId", ""],
+		["giftAmount", ""],
 		["isBroadcaster", ""],
+		["points", ""],
 		["title", ""],
 	]);
 };
