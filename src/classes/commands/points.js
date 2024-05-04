851360322099e32fa59169dd6225ef24a456f98a
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-04 20:22:58 +0100

    Update points.js

--- src/classes/commands/points.js
+++ src/classes/commands/points.js
@@ -1,13 +1,50 @@
 const BaseCommand = require("./baseCommand.js");
+const { findOne } = require("../../queries/loyaltyPoints");
+const isValueNumber = require("../../utils/valueChecks/isValueNumber.js");
 
 async function action(config) {
-	if (config.versionKey !== "noArgument") return;
+	if (
+		config.versionKey !== "noArgument" &&
+		config.versionKey !== "stringArgument"
+	)
+		return;
 
-	const output = this.getProcessedOutputString(
-		this.getOutput("text"),
+	let output;
+	let outputType;
+	const user = await findOne(
+		{
+			channelId: config.channelId,
+			viewerId: config.userId,
+		},
+		{ points: 1 }
+	);
+
+	outputType = "userNotFound";
+
+	if (config.versionKey == "noArgument" && user) {
+		outputType = "userPoints";
+		if (user) config.configMap.set("points", user.pointspoints);
+	}
+	if (config.versionKey == "stringArgument" && user) {
+		const { a: giftTo, b: giftAmount } = this.getArgumentParams(
+			config.argument
+		);
+		outputType = "noParams";
+
+		if (isNonEmptyString(giftTo) && isValueNumber(giftAmount)) {
+			outputType = "gifted";
+			user.points += giftAmount;
+			config.configMap.set("giftAmount", giftAmount);
+		}
+	}
+
+	output = this.getProcessedOutputString(
+		this.getOutput(outputType),
 		config.configMap
 	);
-	if (!output) return;
+	if (!output) outputType == "error";
+
+	if (outputType == "gifted") await user.save();
 
 	return output;
 }
