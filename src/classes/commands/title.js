838d5e6ad685c3e60005410e4ad6ee09c4deda18
TheStarlessAbstract <david.foley@outlook.com>
2024-05-04 01:16:56 +0100

    Update title.js

--- src/classes/commands/title.js
+++ src/classes/commands/title.js
@@ -1,13 +1,40 @@
 const BaseCommand = require("./baseCommand.js");
+const { getStreamByUserId } = require("../../services/twitch/streams");
+const { updateChannelInfo } = require("../../services/twitch/channels");
 
 async function action(config) {
-	if (config.versionKey !== "noArgument") return;
+	if (
+		config.versionKey !== "noArgument" &&
+		config.versionKey !== "stringArgument"
+	)
+		return;
 
-	const output = this.getProcessedOutputString(
-		this.getOutput("text"),
+	let output;
+	let outputType;
+	const stream = await getStreamByUserId(config.channelId);
+	outputType = "noStream";
+
+	if (config.versionKey == "noArgument" && stream) {
+		outputType = "streamIsLive";
+		config.configMap.set("title", stream.title);
+	}
+	if (config.versionKey == "stringArgument" && stream) {
+		outputType = "updateTitle";
+		config.configMap.set("newTitle", config.argument);
+
+		if (stream.title === config.argument) outputType = "existingTitle";
+	}
+
+	if (outputType === "updateTitle") {
+		const data = { title: config.configMap.get("newTitle") };
+		const res = await updateChannelInfo(config.channelId, data);
+		if (!res.success) outputType = "error";
+	}
+
+	output = this.getProcessedOutputString(
+		this.getOutput(outputType),
 		config.configMap
 	);
-	if (!output) return;
 
 	return output;
 }
