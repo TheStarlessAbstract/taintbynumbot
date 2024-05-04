044a351d532729d73cc274a2104e58aa7ab06d4c
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-04 20:23:28 +0100

    Update index.js

--- src/updates/commands/index.js
+++ src/updates/commands/index.js
@@ -1,12 +1,13 @@
 const db = require("../../../bot-mongoose.js");
-const textcommands = require("./textCommands.js");
-const lurk = require("./lurk.js");
-const drinkBitch = require("./drinkbitch.js");
-const buhhs = require("./buhhs.js");
-const followage = require("./followage.js");
-const shoutout = require("./shoutout.js");
 const game = require("./game.js");
+const lurk = require("./lurk.js");
+const buhhs = require("./buhhs.js");
 const title = require("./title.js");
+const points = require("./points.js");
+const shoutout = require("./shoutout.js");
+const followage = require("./followage.js");
+const drinkBitch = require("./drinkbitch.js");
+const textcommands = require("./textCommands.js");
 
 async function init() {
 	const dbStatus = await db.getReadyState();
@@ -14,14 +15,15 @@ async function init() {
 		await db.connectToMongoDB();
 	}
 
-	await textcommands();
-	await lurk();
-	await drinkBitch();
-	await buhhs();
-	await followage();
-	await shoutout();
 	await game();
+	await lurk();
+	await buhhs();
 	await title();
+	await points();
+	await shoutout();
+	await followage();
+	await drinkBitch();
+	await textcommands();
 
 	if (dbStatus === "Mongoose disconnected") {
 		await db.disconnectFromMongoDB();
