cf88c060e8bfd328ef940ad887abac3c31cc9a1f
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-05-04 20:23:20 +0100

    Delete findUserPoints.js

--- src/queries/loyaltyPoints/findUserPoints.js
+++
@@ -1,7 +0,0 @@
-const LoyaltyPoints = require("../../models/loyaltypointnew.js");
-
-async function findUserPoints(query, projection, options) {
-	return LoyaltyPoints.findOne(query, projection, options).exec();
-}
-
-module.exports = { findUserPoints };
