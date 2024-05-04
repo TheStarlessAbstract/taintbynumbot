48fc41a642c76fc2a7be89a3cf59aee3787a4494
TheStarlessAbstract <david.foley@outlook.com>
2024-05-04 01:17:24 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,21 @@
 const isBroadcaster = require("./isBroadcaster.js");
-const isValueNumber = require("./isValueNumber.js");
 const isNonEmptyMap = require("./isNonEmptyMap.js");
 const isNonEmptyString = require("./isNonEmptyString.js");
 const processOutputString = require("./processOutputString.js");
-const getCommandAction = require("./getCommandAction.js");
 const getUserRolesAsStrings = require("./getUserRolesAsStrings.js");
 const getChatCommandConfigMap = require("./getChatCommandConfigMap.js");
 const startsWithCaseInsensitive = require("./startsWithCaseInsensitive.js");
 const getRandomBetweenInclusiveMax = require("./getRandomBetweenInclusiveMax.js");
+const inputCheck = require("./inputCheck");
+const messageHandler = require("./messageHandler");
 
 module.exports = {
+	inputCheck,
+	messageHandler,
 	isBroadcaster,
-	isValueNumber,
 	isNonEmptyMap,
 	isNonEmptyString,
 	processOutputString,
-	getCommandAction,
 	getUserRolesAsStrings,
 	getChatCommandConfigMap,
 	startsWithCaseInsensitive,
