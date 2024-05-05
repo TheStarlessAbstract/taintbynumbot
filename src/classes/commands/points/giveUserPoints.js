f13e1fa2fc9d470b4f8e58f258c540a7f05bead3
TheStarlessAbstract <david.foley@outlook.com>
2024-05-06 00:54:20 +0100

    Update giveUserPoints.js

--- src/classes/commands/points/giveUserPoints.js
+++ src/classes/commands/points/giveUserPoints.js
@@ -1,3 +1,41 @@
-const number = "";
+const { findOne } = require("../../../queries/loyaltyPoints");
+const {
+	isValueNumber,
+	isNonEmptyString,
+} = require("../../../utils/valueChecks");
 
-module.exports = number;
+const giveUserPoints = async function (config) {
+	if (config.versionKey !== "giveUserPoints") return;
+
+	let output;
+	let outputType = "userPoints";
+	const user = await findOne(
+		{
+			channelId: config.channelId,
+			viewerId: config.userId,
+		},
+		{ points: 1 }
+	);
+	if (!user) outputType = "userNotFound";
+	let { a: giftTo, b: giftAmount } = this.getArgumentParams(config.argument);
+	outputType = "noParams";
+	if (isNonEmptyString(giftTo) && isValueNumber(giftAmount)) {
+		if (giftTo.startsWith("@")) {
+			giftTo = giftTo.substring(1);
+		}
+		outputType = "gifted";
+		user.points += giftAmount;
+		config.configMap.set("giftAmount", giftAmount);
+		config.configMap.set("giftTo", giftTo);
+	}
+
+	output = this.getProcessedOutputString(
+		this.getOutput(outputType),
+		config.configMap
+	);
+	if (outputType == "gifted") await user.save();
+
+	return output;
+};
+
+module.exports = giveUserPoints;
