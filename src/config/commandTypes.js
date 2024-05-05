2c575739bb06d155db769ee6f646f67c75bfdd35
TheStarlessAbstract <david.foley@outlook.com>
2024-05-06 00:54:47 +0100

    Update commandTypes.js

--- src/config/commandTypes.js
+++ src/config/commandTypes.js
@@ -1,15 +1,13 @@
-const points = require("../classes/commands/points");
-
 const commandTypes = () => {
 	return {
-		game: require("../classes/commands/game.js"),
-		text: require("../classes/commands/text.js"),
-		title: require("../classes/commands/title.js"),
-		points: points.class,
+		// game: require("../classes/commands/game.js"),
+		// text: require("../classes/commands/text.js"),
+		// title: require("../classes/commands/title.js"),
+		points: require("../classes/commands/points/points"),
 		// points: require("../classes/commands/points.js"),
-		hydrate: require("../classes/commands/hydrate.js"),
-		shoutout: require("../classes/commands/shoutout.js"),
-		followage: require("../classes/commands/followage.js"),
+		// hydrate: require("../classes/commands/hydrate.js"),
+		shoutout: require("../classes/commands/shoutout/shoutout.js"),
+		followage: require("../classes/commands/followage/followage"),
 	};
 };
 
