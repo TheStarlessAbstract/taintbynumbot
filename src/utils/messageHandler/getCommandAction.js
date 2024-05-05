aea5d441f483270b3af5e926e9efeb7816bf7792
TheStarlessAbstract <david.foley@outlook.com>
2024-05-06 00:55:28 +0100

    Delete getCommandAction.js

--- src/utils/messageHandler/getCommandAction.js
+++
@@ -1,8 +0,0 @@
-const commandTypes = require("../../config/commandTypes.js");
-const types = commandTypes();
-
-const getCommandAction = (type) => {
-	return types[type];
-};
-
-module.exports = getCommandAction;
