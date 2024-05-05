f84572392d4d7e17e403c63458b7695caf88e27b
TheStarlessAbstract <david.foley@outlook.com>
2024-05-06 00:54:50 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 const chatCommandConfigMap = require("./chatCommandConfigMap.js");
 const commandTypes = require("./commandTypes.js");
+const commandActions = require("./commandActions.js");
 
 module.exports = {
 	chatCommandConfigMap,
 	commandTypes,
+	commandActions,
 };
