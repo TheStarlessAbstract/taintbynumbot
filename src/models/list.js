e39bb4876f00ad1aeadfdf5e88f033a7e1e01ffe
TheStarlessAbstract <david.foley@outlook.com>
2024-05-11 01:09:22 +0100

    Create list.js

---
+++ src/models/list.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose"),
+	Schema = mongoose.Schema;
+
+const ListSchema = new Schema({
+	channelId: { type: String, required: true },
+	name: { type: String, required: true }, // quote/tinder - has to be the command name
+	index: { type: Number, required: true },
+	text: String,
+	createdBy: String,
+	createdOn: Date,
+	lastEditedBy: String,
+	lastEditedOn: Date,
+});
+
+ListSchema.index({ channelId: 1, name: 1, index: 1 }, { unique: true });
+
+module.exports = mongoose.model("List", ListSchema);
