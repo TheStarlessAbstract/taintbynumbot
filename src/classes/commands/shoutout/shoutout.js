0e5f6a92db030ad47ae95658fd519cb986210d5f
TheStarlessAbstract <david.foley@outlook.com>
2024-05-11 01:08:50 +0100

    Delete shoutout.js

--- src/classes/commands/shoutout/shoutout.js
+++
@@ -1,9 +0,0 @@
-const BaseCommand = require("../baseCommand.js");
-
-class Shoutout extends BaseCommand {
-	constructor(channelId, name, { type, output, versions }) {
-		super(channelId, name, { type, output, versions });
-	}
-}
-
-module.exports = Shoutout;
