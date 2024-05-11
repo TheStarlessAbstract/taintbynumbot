c3f321f462538a18de5c26ecc7207b192ac0d315
TheStarlessAbstract <david.foley@outlook.com>
2024-05-11 01:09:16 +0100

    Update commandActions.js

--- src/config/commandActions.js
+++ src/config/commandActions.js
@@ -1,11 +1,12 @@
 const commandActions = () => {
 	return {
-		// game: require("../classes/commands/game.js"),
-		// text: require("../classes/commands/text.js"),
-		// title: require("../classes/commands/title.js"),
+		game: require("../classes/commands/game"),
+		// song: require("../classes/commands/song"),
+		list: require("../classes/commands/list"),
+		text: require("../classes/commands/text"),
+		title: require("../classes/commands/title"),
 		points: require("../classes/commands/points"),
-		// points: require("../classes/commands/points.js"),
-		// hydrate: require("../classes/commands/hydrate.js"),
+		hydrate: require("../classes/commands/hydrate"),
 		shoutout: require("../classes/commands/shoutout"),
 		followage: require("../classes/commands/followage"),
 	};
