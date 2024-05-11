872fdfda72d6c4bbdb681b624ddc0ca8ebeedc15
TheStarlessAbstract <david.foley@outlook.com>
2024-05-11 01:09:53 +0100

    Update index.js

--- src/updates/index.js
+++ src/updates/index.js
@@ -1,12 +1,16 @@
 const db = require("../../bot-mongoose.js");
 const commands = require("./commands");
 const models = require("./models");
+const quote = require("./quoteToList.js");
+const tinder = require("./tinderToList.js");
 
 init();
 
 async function init() {
 	await db.connectToMongoDB();
 	await commands();
+	// await quote();
+	// await tinder();
 	// await models();
 	await db.disconnectFromMongoDB();
 }
