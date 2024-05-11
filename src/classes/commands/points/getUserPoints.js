1448e9be1769de12e4e6915fc17117a1b2c78cd3
TheStarlessAbstract <david.foley@outlook.com>
2024-05-11 01:10:03 +0100

    Update getUserPoints.js

--- src/classes/commands/points/getUserPoints.js
+++ src/classes/commands/points/getUserPoints.js
@@ -2,9 +2,8 @@ const { findOne } = require("../../../queries/loyaltyPoints");
 
 const getUserPoints = async function (config) {
 	if (config.versionKey !== "getUserPoints") return;
-
 	let output;
-	let outputType = "userPoints";
+
 	const user = await findOne(
 		{
 			channelId: config.channelId,
@@ -12,14 +11,17 @@ const getUserPoints = async function (config) {
 		},
 		{ points: 1 }
 	);
-	if (!user) outputType = "userNotFound";
-	if (user) config.configMap.set("points", user.points);
+
+	let outputType = "userNotFound";
+	if (user) {
+		config.configMap.set("points", user.points);
+		outputType = "userPoints";
+	}
 
 	output = this.getProcessedOutputString(
 		this.getOutput(outputType),
 		config.configMap
 	);
-	if (!output) outputType == "error";
 
 	return output;
 };
