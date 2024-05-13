4a14746c328e1881b79868830d159a7fddd094db
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:15:35 +0100

    Update index.js

--- src/queries/list/index.js
+++ src/queries/list/index.js
@@ -1,7 +1,11 @@
 const findOne = require("./findOne");
 const aggregate = require("./aggregate");
+const create = require("./create");
+const deleteOne = require("./deleteOne");
 
 module.exports = {
 	findOne,
 	aggregate,
+	create,
+	deleteOne,
 };
