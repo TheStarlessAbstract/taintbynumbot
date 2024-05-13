e06bd0b54bc785ec067abe0c167b58be67a89014
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:15:00 +0100

    Update class.js

--- src/classes/commands/hydrate/class.js
+++ src/classes/commands/hydrate/class.js
@@ -4,6 +4,10 @@ class Hydrate extends BaseCommand {
 	constructor(channelId, name, { type, output, versions }) {
 		super(channelId, name, { type, output, versions });
 	}
+
+	versionHasAudioClip(key) {
+		return this.versions.get(key).hasAudioClip;
+	}
 }
 
 module.exports = Hydrate;
