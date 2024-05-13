fd147ba49b19629c08907c33f3236fae2226e6aa
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:16:12 +0100

    Create tinderDelete.js

---
+++ src/updates/commands/tinderDelete.js
@@ -0,0 +1,63 @@
+// const Command = require("../../models/command.js");
+const Command = require("../../models/commandnew.js");
+
+let twitchId = "100612361";
+
+async function copyAndUpdate() {
+	let comm = template();
+	await comm.save();
+}
+
+function template() {
+	return new Command({
+		channelId: twitchId,
+		chatName: "deltinder",
+		type: "listdelete",
+		createdBy: twitchId,
+		createdOn: new Date(),
+		listTypeName: "tinder",
+		output: new Map([
+			[
+				"notPermitted",
+				{
+					message: "Not permitted",
+					active: false,
+				},
+			],
+			[
+				"notDeleted",
+				{
+					message: "{item} was not deleted",
+					active: true,
+				},
+			],
+			[
+				"deleted",
+				{
+					message: "{item} number {index} has been deleted ",
+					active: true,
+				},
+			],
+		]),
+		versions: new Map([
+			[
+				"deleteItem",
+				{
+					isArgumentOptional: false,
+					hasArgument: true,
+					isArgumentNumber: true,
+					description: "Deletes a Tinder bio",
+					active: true,
+					usableBy: ["broadcaster", "mods"],
+					cooldown: {
+						length: 10000,
+						lastUsed: new Date(),
+						bypassRoles: ["broadcaster"],
+					},
+				},
+			],
+		]),
+	});
+}
+
+module.exports = copyAndUpdate;
