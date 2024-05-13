4f839f538d1c60fa769a4bfd4f9e709a8ed5e003
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:15:38 +0100

    Update play.js

--- src/services/audio/play.js
+++ src/services/audio/play.js
@@ -1,9 +1,11 @@
-const play = (input) => {
-	// check database audiolinks
-	// get random url
-	// send to browser source
+const axios = require("axios");
 
-	return true;
+const play = async (channelId, url) => {
+	// send to browser source
+	let resp = await axios.post("http://localhost:5000" + "/playaudio", {
+		channelId,
+		url,
+	});
 };
 
 module.exports = play;
