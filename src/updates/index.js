6e3c989ac96f6b40ff69c5a52b2169f1f52ed765
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:16:16 +0100

    Update index.js

--- src/updates/index.js
+++ src/updates/index.js
@@ -3,12 +3,14 @@ const commands = require("./commands");
 const models = require("./models");
 const quote = require("./quoteToList.js");
 const tinder = require("./tinderToList.js");
+const audio = require("./audiolinks.js");
 
 init();
 
 async function init() {
 	await db.connectToMongoDB();
 	await commands();
+	// await audio();
 	// await quote();
 	// await tinder();
 	// await models();
