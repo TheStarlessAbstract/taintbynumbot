abd741647fa75888edaf4a2b5ad19b1666295b97
TheStarlessAbstract <david.foley@outlook.com>
2024-05-13 04:15:57 +0100

    Create quoteEdit.js

---
+++ src/updates/commands/quoteEdit.js
@@ -0,0 +1,98 @@
+// const Command = require("../../models/command.js");
+const Command = require("../../models/commandnew.js");
+
+let twitchId = "100612361";
+
+async function copyAndUpdate() {
+	let comm = template();
+	await comm.save();
+}
+
+function template() {
+	return new Command({
+		channelId: twitchId,
+		chatName: "editquote",
+		type: "listedit",
+		createdBy: twitchId,
+		createdOn: new Date(),
+		listTypeName: "quote",
+		output: new Map([
+			[
+				"alreadyExists",
+				{
+					message: "This {item} has already been added",
+					active: true,
+				},
+			],
+			[
+				"added",
+				{
+					message: "{item} added: ",
+					active: true,
+				},
+			],
+			[
+				"notPermitted",
+				{
+					message: "User not premitted to add {item}",
+					active: false,
+				},
+			],
+			[
+				"invalidIndex",
+				{
+					message: "Invalid index provided",
+					active: true,
+				},
+			],
+			[
+				"invalidText",
+				{
+					message: "Invalid {item} text provided",
+					active: true,
+				},
+			],
+			[
+				"notFound",
+				{
+					message: "{item} number {index} not found",
+					active: true,
+				},
+			],
+			[
+				"currentText",
+				{
+					message: "{item} already says {updateText}",
+					active: true,
+				},
+			],
+			[
+				"updated",
+				{
+					message: "{item} updated",
+					active: true,
+				},
+			],
+		]),
+		versions: new Map([
+			[
+				"editItem",
+				{
+					isArgumentOptional: false,
+					hasArgument: true,
+					isArgumentNumber: false,
+					description: "Edits a Quote",
+					active: true,
+					usableBy: ["broadcaster", "mods"],
+					cooldown: {
+						length: 10000,
+						lastUsed: new Date(),
+						bypassRoles: ["broadcaster"],
+					},
+				},
+			],
+		]),
+	});
+}
+
+module.exports = copyAndUpdate;
