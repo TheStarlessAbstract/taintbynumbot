24004700c31c75c643ac4135b3e5f518abd53ceb
TheStarlessAbstract <david.foley@outlook.com>
2024-05-18 13:39:40 +0100

    Update deleteItem.js

--- src/classes/commands/listDelete/deleteItem.js
+++ src/classes/commands/listDelete/deleteItem.js
@@ -2,33 +2,32 @@ const { deleteOne } = require("../../../queries/list");
 
 const deleteItem = async function (config) {
 	if (config.versionKey !== "deleteItem") return;
-	let output;
-
 	if (!config?.permitted) {
-		output = this.getProcessedOutputString(
+		return this.getProcessedOutputString(
 			this.getOutput("notPermitted"),
 			config.configMap
 		);
-		return output;
 	}
+
+	config.configMap.set(index, config.argument);
 
 	const item = await deleteOne({
 		channelId: config.channelId,
 		name: this.listTypeName,
 		index: config.argument,
 	});
+
 	let outputType;
 	if (item.deletedCount === 0) {
 		outputType = "notDeleted";
 	} else {
 		outputType = "deleted";
 	}
-	output = this.getProcessedOutputString(
+
+	return this.getProcessedOutputString(
 		this.getOutput(outputType),
 		config.configMap
 	);
-
-	return output;
 };
 
 module.exports = deleteItem;
