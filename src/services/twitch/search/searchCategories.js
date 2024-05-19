c037f5c6e124cd1d6d2dd21d9f468021dfdc60c8
TheStarlessAbstract <david.foley@outlook.com>
2024-05-20 00:18:20 +0100

    Update searchCategories.js

--- src/services/twitch/search/searchCategories.js
+++ src/services/twitch/search/searchCategories.js
@@ -1,4 +1,4 @@
-const twitchRepo = require("../../../../repos/twitch.js");
+const twitchRepo = require("../../../repos/twitch.js");
 
 const searchCategories = async (query, pagination) => {
 	const apiClient = twitchRepo.getApiClient();
