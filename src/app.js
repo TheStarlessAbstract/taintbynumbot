df26a3eab0f407c7646ea82630c63a9e4e98ed21
TheStarlessAbstract <david.foley@outlook.com>
2024-05-20 00:17:53 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const db = require("../bot-mongoose.js");
+const twitchRepo = require("./repos/twitch");
+// const chatClient = require("./bot-chatclient");
+// const pubSubClient = require("./bot-pubsubclient");
+// const discord = require("./bot-discord");
+
+init();
+
+async function init() {
+	setupSignalHandlers();
+
+	await db.connectToMongoDB();
+	if (!(await twitchRepo.init())) db.disconnectFromMongoDB();
+	// await chatClient.init();
+	// await pubSubClient.init();
+	// await discord.setup();
+}
+
+function setupSignalHandlers() {
+	// Handle SIGTERM and SIGINT signals
+	process.on("SIGTERM", handleSignal);
+	process.on("SIGINT", handleSignal);
+}
+
+async function handleSignal(signal) {
+	if (signal === "SIGTERM") {
+		// await kings.saveKingsState();
+		process.exit(0);
+	} else if (signal === "SIGINT") {
+		// await kings.saveKingsState();
+		process.exit(0);
+	}
+}
