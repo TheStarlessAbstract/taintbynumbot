cdb575e9cb0e55f0ccd06cf1332ca19eae6a85b5
TheStarlessAbstract <david.foley@outlook.com>
2024-05-20 00:18:22 +0100

    Update getStreamByUserId.js

--- src/services/twitch/streams/getStreamByUserId.js
+++ src/services/twitch/streams/getStreamByUserId.js
@@ -1,4 +1,4 @@
-const twitchRepo = require("../../../../repos/twitch.js");
+const twitchRepo = require("../../../repos/twitch.js");
 
 const getStreamByUserId = async (user) => {
 	const apiClient = twitchRepo.getApiClient();
