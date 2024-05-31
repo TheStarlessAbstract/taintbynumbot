4e6d8be3a6903c8d2ce43c0109c24860b18c254d
TheStarlessAbstract <david.foley@outlook.com>
2024-06-01 00:50:40 +0100

    Create channel.js

---
+++ src/services/channel/channel.js
@@ -0,0 +1,32 @@
+const Channel = require("../../classes/channel");
+
+function createChannel(
+	channelId,
+	displayName,
+	dbMessages,
+	messageCountTrigger,
+	messageIntervalLength
+) {
+	const messages = [];
+
+	for (let i = 0; i < dbMessages.length; i++) {
+		messages.push({
+			index: dbMessages[i].index,
+			text: dbMessages[i].text,
+			addedBy: dbMessages[i].addedBy,
+			sentToggle: false,
+		});
+	}
+
+	return new Channel(
+		channelId,
+		displayName,
+		messages,
+		messageCountTrigger,
+		messageIntervalLength
+	);
+}
+
+module.exports = {
+	createChannel,
+};
