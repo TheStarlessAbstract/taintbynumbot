398ce6e953cde2fc48b3c9cb098d9a2b791de257
TheStarlessAbstract <david.foley@outlook.com>
2024-06-01 00:50:33 +0100

    Update channels.js

--- src/controllers/channels.js
+++ src/controllers/channels.js
@@ -1,1 +1,29 @@
-// replaces channelslist
+const channels = new Map();
+
+function getChannel(id) {
+	if (!channels.has(id)) return false;
+	return channels.get(id);
+}
+
+function getAllChannels() {
+	return channels;
+}
+
+function addChannel(id, channel) {
+	if (channels.has(id)) return false;
+	channels.set(id, channel);
+	return true;
+}
+
+function removeChannel(id) {
+	if (!channels.has(id)) return false;
+	channels.delete(id);
+	return true;
+}
+
+module.exports = {
+	getChannel,
+	getAllChannels,
+	addChannel,
+	removeChannel,
+};
