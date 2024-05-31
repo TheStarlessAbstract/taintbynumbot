88b390db44ad5b8e05d966eb5d11ad317da6ff46
TheStarlessAbstract <david.foley@outlook.com>
2024-06-01 00:50:42 +0100

    Create channels.js

---
+++ src/services/channels/channels.js
@@ -0,0 +1,39 @@
+const channelsController = require("../../controllers/channels");
+
+function getChannel(id) {
+	const channel = channelsController.getChannel(id);
+	return channel;
+}
+
+function getAllChannels() {
+	const channels = channelsController.getAllChannels();
+	return channels;
+}
+
+function addChannel(id, channel) {
+	const channels = channelsController.getAllChannels();
+	if (channels.has(id)) return false;
+	const res = channelsController.addChannel(id, channel);
+	return res;
+}
+
+function removeChannel(id) {
+	const channels = channelsController.getAllChannels();
+	if (channels.has(id)) return false;
+	const res = channelsController.removeChannel(id);
+	return res;
+}
+
+function getChannelMessageCount(id) {
+	const channel = channelsController.getChannel(id);
+	if (!channel) return false;
+	return channel.getMessageCount();
+}
+
+module.exports = {
+	getChannel,
+	getAllChannels,
+	addChannel,
+	removeChannel,
+	getChannelMessageCount,
+};
