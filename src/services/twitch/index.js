9e2ab2d4d07685a66271d6c11cfcb9fd5007f6ac
TheStarlessAbstract <david.foley@outlook.com>
2024-06-01 00:50:48 +0100

    Update index.js

--- src/services/twitch/index.js
+++ src/services/twitch/index.js
@@ -3,5 +3,6 @@ const users = require("./users");
 const chat = require("./chat");
 const streams = require("./streams");
 const search = require("./search");
+const chatClient = require("./search");
 
-module.exports = { channels, users, chat, streams, search };
+module.exports = { channels, users, chat, streams, search, chatClient };
