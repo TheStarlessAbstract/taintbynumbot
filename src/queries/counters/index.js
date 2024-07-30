7ff9212fda41627bfb3149164cf4e71200d34adc
TheStarlessAbstract <david.foley@outlook.com>
2024-07-30 17:11:15 +0100

    Create index.js

---
+++ src/queries/counters/index.js
@@ -0,0 +1,11 @@
+const findOne = require("./findOne");
+const aggregate = require("./aggregate");
+const updateOne = require("./updateOne");
+const create = require("./create");
+
+module.exports = {
+	findOne,
+	aggregate,
+	updateOne,
+	create,
+};
