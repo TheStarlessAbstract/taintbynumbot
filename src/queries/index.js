840e09ce1edfb4800d833dd941542d0775ed2193
TheStarlessAbstract <david.foley@outlook.com>
2024-07-30 17:11:19 +0100

    Update index.js

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,9 +1,11 @@
 const command = require("./commands");
 const loyaltyPoints = require("./loyaltyPoints");
 const list = require("./list");
+const counters = require("./counters");
 
 module.exports = {
 	command,
 	loyaltyPoints,
 	list,
+	counters,
 };
