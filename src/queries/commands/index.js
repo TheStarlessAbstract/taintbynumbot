a4202253ac1a5083b86a07712767a68ee3e1907d
TheStarlessAbstract <david.foley@outlook.com>
2024-07-30 17:10:46 +0100

    Update index.js

--- src/queries/commands/index.js
+++ src/queries/commands/index.js
@@ -1,5 +1,9 @@
-const findOne = require("./findOne.js");
+const findOne = require("./findOne");
+const create = require("./create");
+const deleteOne = require("./deleteOne");
 
 module.exports = {
 	findOne,
+	create,
+	deleteOne,
 };
