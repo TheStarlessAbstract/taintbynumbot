b15b338e370e45a11ccbd2bfd4c0b201084bf8cf
TheStarlessAbstract <david.foley@outlook.com>
2024-07-30 17:10:26 +0100

    Update editMessage.js

--- src/classes/commands/messageEdit/editMessage.js
+++ src/classes/commands/messageEdit/editMessage.js
@@ -1,5 +1,6 @@
-const { findOne } = require("../../../queries/loyaltyPoints");
+const { getChannel } = require("../../../services/channels/channels");
 const { splitArgs } = require("../../../utils/modify");
+const { updateOne } = require("../../../queries/users");
 
 const editMessage = async function (config) {
 	if (config.versionKey !== "editMessage") return;
@@ -7,15 +8,39 @@ const editMessage = async function (config) {
 	let outputType = "";
 	if (!config?.permitted) outputType = "notPermitted";
 	if (!outputType && !config.argument) outputType = "noArgument";
+	if (outputType)
+		return this.getProcessedOutputString(
+			this.getOutput(outputType),
+			config.configMap
+		);
+
 	const { first: index, second: textUpdate } = splitArgs(config.argument, 0);
+	config.configMap.set("messageIndex", index);
+	const channel = getChannel(config.channelId);
+	if (!channel) return;
+	const message = channel.getMessageByIndex(index);
+	if (!message)
+		return this.getProcessedOutputString(
+			this.getOutput("doesNotExist"),
+			config.configMap
+		);
+	const existingMessage = channel.checkMessageExists(textUpdate);
+	if (existingMessage?.index === index) outputType = "currentMessageText";
+	else if (existingMessage) outputType = "messageExists";
+	if (outputType)
+		return this.getProcessedOutputString(
+			this.getOutput(outputType),
+			config.configMap
+		);
 
-	// get the message with this index
-	// if any message has this text
+	const res = await updateOne(
+		{ channelId: config.channelId, "messages.index": index },
+		{ $set: { "messages.$.text": textUpdate } }
+	);
 
-	if (user) {
-		config.configMap.set("points", user.points);
-		outputType = "userPoints";
-	}
+	outputType = "updated";
+	if (!res.acknowledged) outputType = "error";
+	else message.text = textUpdate;
 
 	output = this.getProcessedOutputString(
 		this.getOutput(outputType),
