906db63f1b14fec4da2bebb5dc300f63c4ea5a7e
TheStarlessAbstract <david.foley@outlook.com>
2024-07-30 17:10:13 +0100

    Update class.js

--- src/classes/commands/listDelete/class.js
+++ src/classes/commands/listDelete/class.js
@@ -1,9 +1,9 @@
 const BaseCommand = require("../baseCommand.js");
 
 class ListDelete extends BaseCommand {
-	constructor(channelId, name, { type, output, versions, listTypeName }) {
+	constructor(channelId, name, { type, output, versions, commandGroup }) {
 		super(channelId, name, { type, output, versions });
-		this.listTypeName = listTypeName;
+		this.commandGroup = commandGroup;
 	}
 }
 
