9b3ab83e047bd9c4c6229b7dcda58ce2a11a4578
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-08-19 12:25:08 +0100

    Update drawACard.js

--- src/classes/commands/cardGameNew/drawACard.js
+++ src/classes/commands/cardGameNew/drawACard.js
@@ -7,26 +7,29 @@ const CardGame = require("./../../cardGame");
 const Channel = require("../../channel");
 
 const drawACard = async function (config) {
-	if (config.versionKey !== "drawACard") return;
+	// config properties used { configMap, permitted, userId }
 
 	const stream = await getStreamByUserId(this.channelId);
+	// check if stream live
 	if (!stream) return this.getOutputString("noStream", config.configMap);
 
+	// check if permitted
 	if (!config?.permitted || typeof config.permitted !== "boolean") {
 		return this.getOutputString("notPermitted", config.configMap);
 	}
 
 	const channel = getChannel(this.channelId);
-
+	// check if channel valid instance
 	if (!(channel instanceof Channel)) return;
-
 	let game = channel.getCardGame(this.name);
 
+	//check if game valid instance
 	if (!(game instanceof CardGame)) {
 		dbCardGame = await findOne({
 			channelId: this.channelId,
 			name: this.name,
 		});
+		// check if cardGame in database
 		if (!dbCardGame) return this.getOutputString("noGame", config.configMap);
 
 		const { suits, values, bonus } = dbCardGame;
@@ -34,8 +37,8 @@ const drawACard = async function (config) {
 		channel.addCardGame(this.name, game);
 	}
 
-	const drawn = await game.drawCard(); // bonus = [{audioLink, selector}]
-
+	// drawn.bonus = [ { audioLink, selector } ]
+	const drawn = await game.drawCard();
 	if (!this.validateCard(drawn)) return;
 	const { card, reset, bonus } = drawn;
 
@@ -51,9 +54,12 @@ const drawACard = async function (config) {
 
 	const audioLinkUrls = [];
 	let audioUrl;
+	// check if card has audio alert
 	if (card.audioName) audioUrl = await this.getAudioUrl(card.audioName);
+	// check if audio alert found add us array
 	if (audioUrl) audioLinkUrls.push(audioUrl);
 
+	// query database for loyatlyPoints for user
 	const dbUser = await loyaltyPoints.findOne(
 		{
 			channelId: this.chanelId,
@@ -62,9 +68,12 @@ const drawACard = async function (config) {
 		{ points: 1 }
 	);
 
+	// if card has at least 1 bonus
 	for (let i = 0; i < bonus.length; i++) {
+		// check if bonus has audio alert, add to array if so
 		if (bonus[i]?.audioLink) audioLinkUrls.push(bonus[i].audioLink);
 
+		// check if bonus has a reward
 		if (bonus[i]?.reward && dbUser) {
 			config.configMap.set("prize", bonus[i].reward);
 			config.configMap.set("total", dbUser.points);
@@ -77,11 +86,9 @@ const drawACard = async function (config) {
 	}
 
 	if (dbUser) await dbUser.save();
-
 	if (reset) {
 		output.push(this.getOutputString("newGame", config.configMap));
 	}
-
 	if (audioLinkUrls.length > 0) play(this.channelId, audioLinkUrls); // update function for array of URLs
 
 	return output;
