5e83b7c0896a835136458dd41d5ba54ad293818e
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-08-23 23:58:38 +0100

    Update loyalty.js

--- src/controllers/loyalty.js
+++ src/controllers/loyalty.js
@@ -1,7 +1,32 @@
-const channelIntervals = new Map();
+const { getChattersPaginated } = require("../services/twitch/chat");
 
-function start(channelId) {}
-function stop(channelId) {}
+const intervals = new Map();
+
+function start(channelId) {
+	intervals.set(channelId, createInterval(channelId));
+}
+function stop(channelId) {
+	const interval = intervals.get(channelId);
+	interval.stop();
+}
+
+function createInterval(channelId) {
+	return setInterval(async () => {
+		let chat = await getChattersPaginated(channelId);
+		const chatters = await listChatters(chat);
+	}, 5 * 60 * 1000);
+}
+
+async function listChatters(chat) {
+	let chatPage = await chat.getNext();
+	if (chatPage.length == 0) return;
+	const chatters = [];
+
+	while (chatPage.length > 0) {
+		chatters = chatters.concat(currentPageChatters);
+		chatPage = await chat.getNext();
+	}
+}
 
 module.exports = {
 	start,
