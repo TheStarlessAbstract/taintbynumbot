0aad9c82eac6c654a44b2aed17b0cf4be1da2c16
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-08-28 20:21:05 +0100

    Update commandTypes.js

--- src/config/commandTypes.js
+++ src/config/commandTypes.js
@@ -1,32 +1,35 @@
-const commandTypes = () => {
-	return {
-		commandadd: require("../classes/commands/commandAdd/class"),
-		commanddelete: require("../classes/commands/commandDelete/class"),
-		commandedit: require("../classes/commands/commandEdit/class"),
-		counter: require("../classes/commands/counter/class"),
-		counterdecrease: require("../classes/commands/counterDecrease/class"),
-		counterincrease: require("../classes/commands/counterIncrease/class"),
-		counterset: require("../classes/commands/counterSet/class"),
-		followage: require("../classes/commands/followage/class"),
-		game: require("../classes/commands/game/class"),
-		hydrate: require("../classes/commands/hydrate/class"),
-		list: require("../classes/commands/list/class"),
-		listadd: require("../classes/commands/listAdd/class"),
-		listdelete: require("../classes/commands/listDelete/class"),
-		listedit: require("../classes/commands/listEdit/class"),
-		messageadd: require("../classes/commands/messageAdd/class"),
-		messagedelete: require("../classes/commands/messageDelete/class"),
-		messageedit: require("../classes/commands/messageEdit/class"),
-		points: require("../classes/commands/points/class"),
-		shoutout: require("../classes/commands/shoutout/class"),
-		song: require("../classes/commands/song/class"),
-		steam: require("../classes/commands/steam/class"),
-		text: require("../classes/commands/text/class"),
-		title: require("../classes/commands/title/class"),
-		cardgame: require("../classes/commands/cardGameNew/class"),
-		cardgamereset: require("../classes/commands/cardGameReset/class"),
-		cardgameremain: require("../classes/commands/cardGameRemain/class"),
-	};
+const commandTypes = {
+	commandadd: "../classes/commands/commandAdd/class",
+	commanddelete: "../classes/commands/commandDelete/class",
+	commandedit: "../classes/commands/commandEdit/class",
+	counter: "../classes/commands/counter/class",
+	counterdecrease: "../classes/commands/counterDecrease/class",
+	counterincrease: "../classes/commands/counterIncrease/class",
+	counterset: "../classes/commands/counterSet/class",
+	followage: "../classes/commands/followage/class",
+	game: "../classes/commands/game/class",
+	hydrate: "../classes/commands/hydrate/class",
+	list: "../classes/commands/list/class",
+	listadd: "../classes/commands/listAdd/class",
+	listdelete: "../classes/commands/listDelete/class",
+	listedit: "../classes/commands/listEdit/class",
+	messageadd: "../classes/commands/messageAdd/class",
+	messagedelete: "../classes/commands/messageDelete/class",
+	messageedit: "../classes/commands/messageEdit/class",
+	points: "../classes/commands/points/class",
+	shoutout: "../classes/commands/shoutout/class",
+	song: "../classes/commands/song/class",
+	steam: "../classes/commands/steam/class",
+	text: "../classes/commands/text/class",
+	title: "../classes/commands/title/class",
+	cardgame: "../classes/commands/cardGameNew/class",
+	cardgamereset: "../classes/commands/cardGameReset/class",
+	cardgameremain: "../classes/commands/cardGameRemain/class",
 };
 
-module.exports = commandTypes;
+function getCommandType(name) {
+	const Type = require(commandTypes[name]);
+	return Type;
+}
+
+module.exports = { getCommandType };
