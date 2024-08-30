0118a052ed3f7f232b4c49352f6110aaf8a4b940
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-08-31 00:46:54 +0100

    Update loyalty.js

--- src/controllers/loyalty.js
+++ src/controllers/loyalty.js
@@ -1,4 +1,6 @@
 const { getChattersPaginated } = require("../services/twitch/chat");
+const { getChannelFollowersPaginated } = require("../services/twitch/channels");
+const { find, insertMany } = require("../queries/loyaltyPoints");
 
 const intervals = new Map();
 
@@ -12,8 +14,30 @@ function stop(channelId) {
 
 function createInterval(channelId) {
 	return setInterval(async () => {
-		let chat = await getChattersPaginated(channelId);
+		const chat = await getChattersPaginated(channelId);
 		const chatters = await listChatters(chat);
+		if (chatters.length == 0) return;
+
+		const userIds = [];
+		for (let i = 0; i < chatters.length; i++) {
+			userIds.push(chatters[i].userId);
+		}
+
+		const subs = await twitchService.getSubscriptionsForUsers(
+			channelId,
+			userIds
+		);
+
+		const existingUsers = await find({
+			channelId,
+			userId: { $in: userIds },
+		}).exec();
+
+		let newUsers = chatters.filter(
+			(user) => !existingUsers.find((exUser) => exUser.userId == user.userId)
+		);
+
+		await createUser(channelId, newUsers, subs);
 	}, 5 * 60 * 1000);
 }
 
@@ -23,9 +47,71 @@ async function listChatters(chat) {
 	const chatters = [];
 
 	while (chatPage.length > 0) {
-		chatters = chatters.concat(currentPageChatters);
+		chatters = chatters.concat(chatPage);
 		chatPage = await chat.getNext();
 	}
+	return chatters;
+}
+
+async function createUser(channelId, users, subs) {
+	const newUsers = [];
+	let newFollowBonus;
+	let following;
+	let modifier;
+
+	const followers = await getChannelFollowersPaginated(channelId);
+
+	users.forEach((value, key) => {
+		followers.some((follower) => {
+			let following = false;
+			let newfollowBonus = 0;
+			if (follower.userId === value.userId) {
+				following = true;
+				newfollowBonus = 2000;
+			}
+
+			modifier = getModifier(users[i], subs);
+		});
+	});
+
+	for (let i = 0; i < users.length; i++) {
+		let channelFollower = await twitchService.getChannelFollowers(
+			channelId,
+			users[i].userId
+		);
+
+		if (channelFollower.data.length == 1) {
+			following = true;
+			newFollowBonus = 2000;
+		} else {
+			following = false;
+			newFollowBonus = 0;
+		}
+
+		modifier = getModifier(users[i], subs);
+
+		newUsers.push({
+			channelId,
+			userId: users[i].userId,
+			points: 10 * modifier + newFollowBonus,
+			follower: following,
+		});
+	}
+
+	await insertMany(newUsers);
+}
+
+function getModifier(user, subs) {
+	let mod = 1;
+	let sub;
+
+	sub = subs.find((sub) => sub.userId === user.userId);
+
+	if (!sub) return mod;
+
+	mod = sub.tier === 1000 ? 1.2 : sub.tier === 2000 ? 1.4 : 2;
+
+	return mod;
 }
 
 module.exports = {
