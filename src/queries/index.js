35a198d54558d708b6f7d23911009e6f83d0ecab
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-11-27 21:54:27 +0000

    Update index.js

--- src/queries/index.js
+++ src/queries/index.js
@@ -2,10 +2,12 @@ const command = require("./commands");
 const loyaltyPoints = require("./loyaltyPoints");
 const list = require("./list");
 const counters = require("./counters");
+const redemption = require("./redemptions");
 
 module.exports = {
 	command,
 	loyaltyPoints,
 	list,
 	counters,
+	redemption,
 };
