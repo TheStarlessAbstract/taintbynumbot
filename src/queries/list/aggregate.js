25655931c93135d45d19741bb1f5147cf378336a
TheStarlessAbstract <thestarlessabstract@gmail.com>
2024-12-16 20:28:12 +0000

    Update aggregate.js

--- src/queries/list/aggregate.js
+++ src/queries/list/aggregate.js
@@ -1,7 +1,7 @@
 const List = require("../../models/list.js");
 
-async function aggregate(pipline) {
-	return await List.aggregate(pipline).exec();
+async function aggregate(pipeline) {
+	return await List.aggregate(pipeline).exec();
 }
 
 module.exports = aggregate;
