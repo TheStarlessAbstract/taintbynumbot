fd738a2e649996fb872e634d3ec962c80a40351a
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-01-11 01:42:19 +0000

    Update index.js

--- src/updates/models/index.js
+++ src/updates/models/index.js
@@ -1,23 +1,13 @@
-const db = require("../../../bot-mongoose");
 const loyaltyPoints = require("./loyaltyPoints");
 const users = require("./users");
 const counter = require("./counter");
 const cardGame = require("./cardGame");
 
 async function init() {
-	const dbStatus = await db.getReadyState();
-	if (dbStatus === "Mongoose disconnected") {
-		await db.connectToMongoDB();
-	}
-
 	// await loyaltyPoints();
 	await users();
 	// await counter();
 	// await cardGame();
-
-	if (dbStatus === "Mongoose disconnected") {
-		await db.disconnectFromMongoDB();
-	}
 }
 
 module.exports = init;
