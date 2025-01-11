8a5ff5f769888a3b6cb1c8222ea108e77def840a
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-01-11 01:41:44 +0000

    Update index.js

--- src/queries/commands/index.js
+++ src/queries/commands/index.js
@@ -1,8 +1,10 @@
+const find = require("./find");
 const findOne = require("./findOne");
 const create = require("./create");
 const deleteOne = require("./deleteOne");
 
 module.exports = {
+	find,
 	findOne,
 	create,
 	deleteOne,
