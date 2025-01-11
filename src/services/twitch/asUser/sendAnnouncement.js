1152decfcd3f6a54238c3822811f6d1cb3e95620
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-01-11 01:42:12 +0000

    Update sendAnnouncement.js

--- src/services/twitch/asUser/sendAnnouncement.js
+++ src/services/twitch/asUser/sendAnnouncement.js
@@ -1,7 +1,14 @@
-const twitchRepo = require("../../../repos/twitch.js");
+const twitchRepo = require("../../../repos/twitch");
 
 const sendAnnouncement = async (broadcaster, moderator, announcement) => {
 	const apiClient = twitchRepo.getApiClient();
+
+	if (!announcement?.message) {
+		console.error(
+			"sendAnnoucement asUser - no message included in anncounement"
+		);
+		return;
+	}
 
 	try {
 		await apiClient.asUser(moderator, async (ctx) => {
