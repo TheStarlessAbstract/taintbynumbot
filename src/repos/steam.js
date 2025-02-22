d3badb426efc93ac8a5f7a118932f36a56eb8058
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-02-22 15:20:39 +0000

    Create steam.js

---
+++ src/repos/steam.js
@@ -0,0 +1,9 @@
+const SteamAPI = require("steamapi");
+const steamApiKey = process.env.STEAM_WEB_API_KEY;
+const steam = new SteamAPI(steamApiKey);
+
+function getApi() {
+	return steam;
+}
+
+exports.getApi = getApi;
