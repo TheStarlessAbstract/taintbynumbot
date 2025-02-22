32ab91bc975fff6e96b8b2665b1fa1e64a426ad8
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-02-22 15:20:06 +0000

    Create steam.js

---
+++ src/classes/commands/steam.js
@@ -0,0 +1,47 @@
+const BaseCommand = require("./baseCommand");
+const steam = require("../../repos/steam");
+
+class Steam extends BaseCommand {
+	constructor(channelId, name, { type, output, versions }) {
+		super(channelId, name, { type, output, versions });
+		this.steamApi = steam.getApi();
+	}
+
+	async achievementsCompleted(userId, games, percent) {
+		if (games.length > 100) {
+			const shuffledGames = this.shuffle(games);
+			games = shuffledGames.slice(0, 100);
+		}
+
+		let percentMatchedGames = [];
+
+		for (let i = 0; i < games.length; i++) {
+			let steamGame;
+			try {
+				steamGame = await this.steamApi.getUserAchievements(
+					userId,
+					games[i].appID
+				);
+			} catch (err) {
+				continue;
+			}
+
+			const countCompleted = steamGame.achievements.filter(
+				(achievement) => achievement.achieved
+			).length;
+
+			const percentComplete =
+				(countCompleted / steamGame.achievements.length) * 100;
+
+			if (percentComplete <= percent)
+				percentMatchedGames.push({
+					name: games[i].name,
+					percentage: percentComplete,
+				});
+		}
+
+		return percentMatchedGames;
+	}
+}
+
+module.exports = Steam;
