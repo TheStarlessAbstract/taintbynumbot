f315a30f118e862c904494dda37576040dee91a2
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-10 22:13:32 +0000

    Create deleteCommand.js

---
+++ src/commandActions/commandDelete/deleteCommand.js
@@ -0,0 +1,26 @@
+const { deleteOne } = require("../../queries/commands");
+const { getChannel } = require("../../controllers/channels");
+
+const deleteCommand = async function (config) {
+	if (!config?.permitted)
+		return this.getOutputString("notPermitted", config.configMap);
+
+	if (!config.argument.startsWith("!"))
+		return this.getOutputString("noPrefix", config.configMap);
+	const name = config.argument.slice(1);
+
+	const command = await deleteOne({
+		channelId: this.channelId,
+		chatName: name,
+	});
+
+	if (command.deletedCount === 0)
+		return this.getOutputString("notDeleted", config.configMap);
+
+	const channel = getChannel(this.channelId);
+	channel.deleteCommand(name);
+
+	return this.getOutputString("deleted", config.configMap);
+};
+
+module.exports = deleteCommand;
