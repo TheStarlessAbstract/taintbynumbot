fb34d1064f41053cebcb898b63c36ceb95d84ef9
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-10 22:14:35 +0000

    Create getRandom.js

---
+++ src/commandActions/list/getRandom.js
@@ -0,0 +1,28 @@
+const { aggregate } = require("../../queries/list");
+
+const getRandom = async function (config) {
+	if (!config?.permitted)
+		return this.getOutputString("notPermitted", config.configMap);
+
+	const pipeline = [
+		{
+			$match: {
+				channelId: config.channelId,
+				name: config.chatName,
+				index: { $exists: true },
+				text: { $ne: "" },
+			},
+		},
+		{ $sample: { size: 1 } },
+	];
+
+	const listItems = await aggregate(pipeline);
+	if (listItems.length === 0)
+		return this.getOutputString("randomNotFound", config.configMap);
+
+	config.configMap.set("index", listItems[0].index);
+	config.configMap.set("text", listItems[0].text);
+	return this.getOutputString("found", config.configMap);
+};
+
+module.exports = getRandom;
