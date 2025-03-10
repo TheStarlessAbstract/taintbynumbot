a4035353bc111c2fbe89216f5b533e4ba357d768
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-10 22:14:28 +0000

    Create index.js

---
+++ src/commandActions/list/index.js
@@ -0,0 +1,9 @@
+const getRandom = require("./getRandom");
+const getRandomByString = require("./getRandomByString");
+const getByIndex = require("./getByIndex");
+
+module.exports = {
+	getRandom,
+	getRandomByString,
+	getByIndex,
+};
