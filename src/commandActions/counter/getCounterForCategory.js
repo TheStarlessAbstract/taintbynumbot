19279b1527a55f2d465e74496017dc1eb52e1fa6
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-10 22:13:43 +0000

    Create getCounterForCategory.js

---
+++ src/commandActions/counter/getCounterForCategory.js
@@ -0,0 +1,22 @@
+const { getStreamByUserId } = require("../../services/twitch/streams");
+
+const getCounterForCategory = async function (config) {
+	if (!config?.permitted)
+		return this.getOutputString("notPermitted", config.configMap);
+
+	let game = config.argument;
+
+	if (!config.argument) {
+		const stream = await getStreamByUserId(config.channelId);
+		if (!stream)
+			return this.getOutputString("channelNotLive", config.configMap);
+		game = stream.gameName;
+	}
+
+	const count = await this.count(game);
+	config.configMap.set("total", count);
+
+	return this.getOutputString("total", config.configMap);
+};
+
+module.exports = getCounterForCategory;
