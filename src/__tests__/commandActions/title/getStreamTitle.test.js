b3f7a927022dc62427a7b05543ecd2984d6423df
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-19 16:00:38 +0000

    Create getStreamTitle.test.js

---
+++ src/__tests__/commandActions/title/getStreamTitle.test.js
@@ -0,0 +1,100 @@
+const {
+	getStreamTitle: commandAction,
+} = require("../../../commandActions/title");
+const Command = require("../../../classes/commands/title");
+const { getStreamByUserId } = require("../../../services/twitch/streams");
+
+jest.mock("../../../services/twitch/streams", () => ({
+	getStreamByUserId: jest.fn(),
+}));
+jest.mock("../../../classes/commands/title");
+
+let mockCommand;
+let action;
+
+describe("get stream title", () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+		mockCommand = new Command();
+		mockCommand.channelId = "100612361";
+		action = commandAction.bind(mockCommand);
+	});
+
+	// test id 1
+	test("should return notPermitted output if config.permitted false", async () => {
+		// Assemble
+		const config = { permitted: false };
+		getStreamByUserId.mockResolvedValue({ id: "100612361" });
+
+		jest.spyOn(mockCommand, "isPermitted").mockImplementation(() => false);
+		jest
+			.spyOn(mockCommand, "getOutputString")
+			.mockImplementation(
+				() => "@TaintByNumBot - You are not permitted to use this command"
+			);
+		// Act
+		const result = await action(config);
+
+		// Assert
+		expect(result).toBe(
+			"@TaintByNumBot - You are not permitted to use this command"
+		);
+		expect(mockCommand.isPermitted).toHaveBeenCalledTimes(1);
+		expect(mockCommand.getOutputString).toHaveBeenCalledTimes(1);
+		expect(getStreamByUserId).toHaveBeenCalledTimes(0);
+	});
+
+	// test id 2
+	test("should return noStream output if no stream found", async () => {
+		// Assemble
+		const config = { permitted: true };
+
+		getStreamByUserId.mockResolvedValue(null);
+
+		jest.spyOn(mockCommand, "isPermitted").mockImplementation(() => true);
+		jest
+			.spyOn(mockCommand, "getOutputString")
+			.mockImplementation(
+				() =>
+					"@TaintByNumBot - TheStarlessAbstract doesn't seem to be streaming right now"
+			);
+		// Act
+		const result = await action(config);
+
+		// Assert
+		expect(result).toBe(
+			"@TaintByNumBot - TheStarlessAbstract doesn't seem to be streaming right now"
+		);
+		expect(mockCommand.isPermitted).toHaveBeenCalledTimes(1);
+		expect(getStreamByUserId).toHaveBeenCalledTimes(1);
+		expect(mockCommand.getOutputString).toHaveBeenCalledTimes(1);
+	});
+
+	// test id 3
+	test("should return streamIsLive output if stream found", async () => {
+		// Assemble
+		const config = { permitted: true, configMap: new Map() };
+
+		getStreamByUserId.mockResolvedValue({
+			title: "ChatGPT said this was a good idea",
+		});
+
+		jest.spyOn(mockCommand, "isPermitted").mockImplementation(() => true);
+		jest
+			.spyOn(mockCommand, "getOutputString")
+			.mockImplementation(
+				() =>
+					"@TaintByNumBot - The current stream title is: ChatGPT said this was a good idea"
+			);
+		// Act
+		const result = await action(config);
+
+		// Assert
+		expect(result).toBe(
+			"@TaintByNumBot - The current stream title is: ChatGPT said this was a good idea"
+		);
+		expect(mockCommand.isPermitted).toHaveBeenCalledTimes(1);
+		expect(getStreamByUserId).toHaveBeenCalledTimes(1);
+		expect(mockCommand.getOutputString).toHaveBeenCalledTimes(1);
+	});
+});
