750b41747033344b0c5f272bdc9bce54c5e31c2c
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-19 16:00:41 +0000

    Update getStreamTitle.js

--- src/commandActions/title/getStreamTitle.js
+++ src/commandActions/title/getStreamTitle.js
@@ -1,6 +1,9 @@
 const { getStreamByUserId } = require("../../services/twitch/streams");
 
 const getStreamTitle = async function (config) {
+	if (!this.isPermitted(config.permitted))
+		return this.getOutputString("notPermitted", config.configMap);
+
 	const stream = await getStreamByUserId(config.channelId);
 	if (!stream) return this.getOutputString("noStream", config.configMap);
 
