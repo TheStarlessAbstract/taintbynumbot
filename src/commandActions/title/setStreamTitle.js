3beb9cd090f08681e86d51e613c784ae3055b934
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-03-23 13:53:48 +0000

    Update setStreamTitle.js

--- src/commandActions/title/setStreamTitle.js
+++ src/commandActions/title/setStreamTitle.js
@@ -2,23 +2,26 @@ const { getStreamByUserId } = require("../../services/twitch/streams");
 const { updateChannelInfo } = require("../../services/twitch/channels");
 
 const setStreamTitle = async function (config) {
+	if (!config.configMap) {
+		console.error("no configMap for setStreamTitle");
+		return;
+	}
+
 	if (!this.isPermitted(config.permitted))
 		return this.getOutputString("notPermitted", config.configMap);
 
-	let outputType;
 	const stream = await getStreamByUserId(config.channelId);
 	if (!stream || !stream?.title)
 		return this.getOutputString("noStream", config.configMap);
 	if (stream.title === config.argument)
 		return this.getOutputString("existingTitle", config.configMap);
 
-	outputType = "updateTitle";
 	config.configMap.set("newTitle", config.argument);
 	const data = { title: config.argument };
 	const res = await updateChannelInfo(config.channelId, data);
-	if (!res.success) outputType = "error";
+	if (!res.success) return this.getOutputString("error", config.configMap);
 
-	return this.getOutputString(outputType, config.configMap);
+	return this.getOutputString("updateTitle", config.configMap);
 };
 
 module.exports = setStreamTitle;
