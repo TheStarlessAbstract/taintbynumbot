232a0cf539350cd8050f8c158d3a757e4c2eb3c9
TheStarlessAbstract <thestarlessabstract@gmail.com>
2025-04-25 23:35:59 +0100

    Update processOutputString.js

--- src/utils/modify/processOutputString.js
+++ src/utils/modify/processOutputString.js
@@ -1,6 +1,8 @@
 const processOutputString = (output, variableMap) => {
 	const regex = /\{([^}]+)\}/g;
 	if (!regex.test(output)) return output;
+	console.log(output);
+	console.log(variableMap);
 	const processedOutput = output.replace(regex, (match) =>
 		variableMap.has(match.slice(1, -1))
 			? variableMap.get(match.slice(1, -1))
